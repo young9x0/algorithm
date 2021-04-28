9432c812b19d343011b96f56d6e09232a4d51be4
maliethy <maliethy@gmail.com>
2021-04-28 09:15:05 +0900

    fix: message from chuck norris 해결하기

--- codingame/chucknorris.js
+++ codingame/chucknorris.js
@@ -1,48 +1,32 @@
+//문제:
 //https://www.codingame.com/ide/puzzle/chuck-norris
-const MESSAGE = 'C';
-let binary = '';
+//참고코드:
+//https://gist.github.com/dukeDanjou/73600a15e4977ef48c8d
 
-for (single_msg of MESSAGE) {
-  const num = single_msg.charCodeAt(0);
+function textToBin(text) {
+  const length = text.length;
+  let output = '';
+  for (let i = 0; i < length; i++) {
+    let bin = text.charCodeAt(i).toString(2);
 
-  binary += num.toString(2);
-}
-if (binary.length < 7) {
-  binary = '0' + binary;
-}
-
-let answer = '';
-let prev = '';
-
-for (bit of binary) {
-  console.log(bit === prev);
-  if (prev !== bit) {
-    if (bit === '1') {
-      answer += ' 0 ';
-    } else {
-      answer += ' 00 ';
+    if (bin.length < 7) {
+      bin = '0' + bin;
     }
+    output += bin;
   }
-  answer += '0';
-  prev = bit;
+  return output;
 }
-console.log(answer.slice(1));
-// let answer = '';
-// let arr = binary.split('');
-// let fir = '0';
-// let sec = '0';
-// arr.map((n, i) => {
-//   if (n === arr[i + 1]) {
-//     //반복될 때
-//     sec += '0';
-//   } else {
-//     //반복 안 될 때
-//     if (n === '0') {
-//       fir = '00';
-//     }
-//     answer += fir + ' ' + sec + ' ';
-//     fir = '0';
-//     sec = '0';
-//   }
-// });
-// console.log(answer.slice(0, -1));
+const encode = {
+  0: '00',
+  1: '0',
+};
+// const binaryText = textToBin('CC');
+const binaryText = textToBin('%');
+let currentSign = binaryText[0];
+let output = encode[currentSign] + ' ' + '0';
+for (let i = 1; i < binaryText.length; i++) {
+  let nextBit = binaryText[i];
+  output += nextBit !== currentSign ? ' ' + encode[nextBit] + ' ' + '0' : '0';
+  currentSign = nextBit;
+}
+console.log(output);
