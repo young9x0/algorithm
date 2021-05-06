d3eb901a2ed04659878ea4047e3c4a84c5f28839
maliethy <maliethy@gmail.com>
2021-05-06 09:07:42 +0900

    refactor: battle의  index매개변수값으로 무승부일 경우와 아닐 경우 동시에 처리하기

--- codingame/war.js
+++ codingame/war.js
@@ -1,6 +1,6 @@
 //https://www.codingame.com/ide/puzzle/winamax-battle
-// const n = parseInt(readline());
-// let game = {p1:[], p2:[]}; // the number of cards for player 1
+// const n = parseInt(readline());// the number of cards for player 1
+// let game = { p1: [], p2: [] };
 // for (let i = 0; i < n; i++) {
 //     const cardp1 = readline();
 //      game.p1.push(cardp1);// the n cards of player 1
@@ -13,7 +13,7 @@
 
 const card = ['2', '3', '4', '5', '6', '7', '8', '9', '1', 'J', 'Q', 'K', 'A'];
 
-//case1) just battle
+// case1) just battle
 
 // const n = 5,
 //   m = 5;
@@ -22,7 +22,7 @@ const card = ['2', '3', '4', '5', '6', '7', '8', '9', '1', 'J', 'Q', 'K', 'A'];
 //   p2: ['8D', '2D', '3H', '4D', '3S'],
 // };
 
-//case2-1) war
+// // case2-1) war
 
 // const n = 5,
 //   m = 5;
@@ -31,156 +31,154 @@ const card = ['2', '3', '4', '5', '6', '7', '8', '9', '1', 'J', 'Q', 'K', 'A'];
 //   p2: ['8D', '2D', '3H', '4D', '3S'],
 // };
 
-//case2-2) war
+// // case2-2) war
 
 // const n = 5,
 //   m = 5;
 // const game = {
-//   p1: ['8C', 'KD', 'AH', 'QH', '3S', '5C', 'JD', '3H', '2H'],
+//   p1: ['8C', 'KD', 'AH', 'QH', '3S', '5C', 'indexD', '3H', '2H'],
 //   p2: ['8D', '2D', '3H', '4D', '3S', '1S', 'KD', 'AH', 'QH'],
 // };
 
-//case3) one battle, one war
-
-// const n = 26,
-//   m = 26;
-// const game = {
-//   p1: [
-//     '10H',
-//     'KD',
-//     '6C',
-//     '10S',
-//     '8S',
-//     'AD',
-//     'QS',
-//     '3D',
-//     '7H',
-//     'KH',
-//     '9D',
-//     '2D',
-//     'JC',
-//     'KS',
-//     '3S',
-//     '2S',
-//     'QC',
-//     'AC',
-//     'JH',
-//     '7D',
-//     'KC',
-//     '10D',
-//     '4C',
-//     'AS',
-//     '5D',
-//     '5S',
-//   ],
-//   p2: [
-//     '2H',
-//     '9C',
-//     '8C',
-//     '4S',
-//     '5C',
-//     'AH',
-//     'JD',
-//     'QH',
-//     '7C',
-//     '5H',
-//     '4H',
-//     '6H',
-//     '6S',
-//     'QD',
-//     '9H',
-//     '10C',
-//     '4D',
-//     'JS',
-//     '6D',
-//     '3H',
-//     '8H',
-//     '3C',
-//     '7S',
-//     '9S',
-//     '8D',
-//     '2C',
-//   ],
-// };
-
-//case4) long game
+// case3) one battle, one war
 
 const n = 26,
   m = 26;
 const game = {
   p1: [
-    'AH',
-    '4H',
-    '5D',
-    '6D',
-    'QC',
-    'JS',
-    '8S',
-    '2D',
-    '7D',
-    'JD',
-    'JC',
-    '6C',
-    'KS',
-    'QS',
-    '9D',
-    '2C',
-    '5S',
-    '9S',
-    '6S',
-    '8H',
-    'AD',
-    '4D',
-    '2H',
-    '2S',
-    '7S',
-    '8C',
-  ],
-  p2: [
     '10H',
-    '4C',
-    '6H',
-    '3C',
-    'KC',
-    'JH',
-    '10C',
-    'AS',
-    '5H',
-    'KH',
-    '10S',
-    '9H',
-    '9C',
-    '8D',
-    '5C',
-    'AC',
-    '3H',
-    '4S',
     'KD',
-    '7C',
-    '3S',
-    'QH',
-    '10D',
+    '6C',
+    '10S',
+    '8S',
+    'AD',
+    'QS',
     '3D',
     '7H',
+    'KH',
+    '9D',
+    '2D',
+    'JC',
+    'KS',
+    '3S',
+    '2S',
+    'QC',
+    'AC',
+    'JH',
+    '7D',
+    'KC',
+    '10D',
+    '4C',
+    'AS',
+    '5D',
+    '5S',
+  ],
+  p2: [
+    '2H',
+    '9C',
+    '8C',
+    '4S',
+    '5C',
+    'AH',
+    'JD',
+    'QH',
+    '7C',
+    '5H',
+    '4H',
+    '6H',
+    '6S',
     'QD',
+    '9H',
+    '10C',
+    '4D',
+    'JS',
+    '6D',
+    '3H',
+    '8H',
+    '3C',
+    '7S',
+    '9S',
+    '8D',
+    '2C',
   ],
 };
 
+// //case4) long game
+
+// const n = 26,
+//   m = 26;
+// const game = {
+//   p1: [
+//     'AH',
+//     '4H',
+//     '5D',
+//     '6D',
+//     'QC',
+//     'JS',
+//     '8S',
+//     '2D',
+//     '7D',
+//     'JD',
+//     'JC',
+//     '6C',
+//     'KS',
+//     'QS',
+//     '9D',
+//     '2C',
+//     '5S',
+//     '9S',
+//     '6S',
+//     '8H',
+//     'AD',
+//     '4D',
+//     '2H',
+//     '2S',
+//     '7S',
+//     '8C',
+//   ],
+//   p2: [
+//     '10H',
+//     '4C',
+//     '6H',
+//     '3C',
+//     'KC',
+//     'JH',
+//     '10C',
+//     'AS',
+//     '5H',
+//     'KH',
+//     '10S',
+//     '9H',
+//     '9C',
+//     '8D',
+//     '5C',
+//     'AC',
+//     '3H',
+//     '4S',
+//     'KD',
+//     '7C',
+//     '3S',
+//     'QH',
+//     '10D',
+//     '3D',
+//     '7H',
+//     'QD',
+//   ],
+// };
+
 let count = 0,
   answer = '',
-  temp1 = '',
-  temp2 = '',
   used_cards = [];
 
-function save_used_card(i, j) {
+function push_used_cards(index) {
   let a = 0,
     b = 0;
 
-  while (a <= i) {
+  while (a <= index) {
     used_cards.push(game.p1[a]);
     a++;
   }
-  while (b <= j) {
+  while (b <= index) {
     used_cards.push(game.p2[b]);
     b++;
   }
@@ -189,8 +187,8 @@ function save_used_card(i, j) {
   return used_cards;
 }
 
-function battle(i, j, war_turn) {
-  if (game.p1[i] !== undefined && game.p2[j] !== undefined) {
+function battle(index) {
+  if (game.p1[index] !== undefined && game.p2[index] !== undefined) {
     // console.log(
     //   'p1:',
     //   game.p1,
@@ -203,9 +201,9 @@ function battle(i, j, war_turn) {
     //   'p2:',
     //   game.p2,
     //   '\n',
-    //   'game.p2[j]:',
-    //   game.p2[j],
-    //   j,
+    //   'game.p2[index]:',
+    //   game.p2[index],
+    //   index,
     //   '\n',
     //   'count:',
     //   count,
@@ -213,38 +211,25 @@ function battle(i, j, war_turn) {
     //   '----------------------------------------------------------------------------',
     //   '\n',
     // );
-    if (card.indexOf(game.p1[i][0]) > card.indexOf(game.p2[j][0])) {
-      if (war_turn) {
-        save_used_card(i, j);
-        game.p1.splice(0, war_turn * 4 + 1);
-        game.p2.splice(0, war_turn * 4 + 1);
-        game.p1.push(...used_cards);
-        used_cards = [];
-      } else {
-        game.p1.push(game.p1[i], game.p2[j]);
-        game.p1.splice(0, 1);
-        game.p2.splice(0, 1);
-      }
+    if (card.indexOf(game.p1[index][0]) > card.indexOf(game.p2[index][0])) {
+      push_used_cards(index);
+      game.p1.splice(0, index + 1);
+      game.p2.splice(0, index + 1);
+      game.p1.push(...used_cards);
+      used_cards = [];
       count++;
-      battle(0, 0, 0);
-    } else if (card.indexOf(game.p1[i][0]) < card.indexOf(game.p2[j][0])) {
-      if (war_turn) {
-        save_used_card(i, j);
-        game.p1.splice(0, war_turn * 4 + 1);
-        game.p2.splice(0, war_turn * 4 + 1);
-        game.p2.push(...used_cards);
-        used_cards = [];
-      } else {
-        game.p2.push(game.p1[i], game.p2[j]);
-        game.p1.splice(0, 1);
-        game.p2.splice(0, 1);
-      }
+      battle(0);
+    } else if (card.indexOf(game.p1[index][0]) < card.indexOf(game.p2[index][0])) {
+      push_used_cards(index);
+      game.p1.splice(0, index + 1);
+      game.p2.splice(0, index + 1);
+      game.p2.push(...used_cards);
+      used_cards = [];
       count++;
-      battle(0, 0, 0);
-    } else if (card.indexOf(game.p1[i][0]) === card.indexOf(game.p2[j][0])) {
-      if (i + 4 < game.p1.length && j + 4 < game.p2.length) {
-        war_turn++;
-        battle(i + 4, j + 4, war_turn);
+      battle(0);
+    } else if (card.indexOf(game.p1[index][0]) === card.indexOf(game.p2[index][0])) {
+      if (index + 4 < game.p1.length && index + 4 < game.p2.length) {
+        battle(index + 4);
       } else {
         return (answer = 'PAT');
       }
@@ -260,5 +245,5 @@ function battle(i, j, war_turn) {
   }
 }
 
-battle(0, 0, 0);
+battle(0);
 console.log(answer);
