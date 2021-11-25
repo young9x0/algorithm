85b9a5715208df40f9e83a173b3792d4d6b0efd9
maliethy <maliethy@gmail.com>
2021-11-25 21:44:59 +0900

    fix: https://ninejy.tistory.com/30 정답 확인하기

--- baekjoon/9093.js
+++ baekjoon/9093.js
@@ -2,24 +2,16 @@ const array = require("fs")
   .readFileSync("/dev/stdin", "utf8")
   .trim()
   .split("\n");
-array.shift();
+
 // const array = [2, "I am happy today", "We want to win the first prize"];
-let result = "",
-  answer = "",
-  arr = [];
 
 for (let i = 1; i <= array[0]; i++) {
-  array[i].split(" ").map((str, idx) => {
+  let arr = [];
+  array[i].split(" ").map((str) => {
     let strTemp = "";
-    str.split("").map((word, index) => {
-      arr[idx] = [];
-      strTemp += str[str.length - (index + 1)];
-
-      arr[idx].push(strTemp);
-    });
+    strTemp = str.split("").reverse().join("");
+    arr.push(strTemp);
   });
-
-  result = arr.flat().reduce((acc, val) => acc + val + " ", "");
-  answer += result + "\n";
+  // console.log(arr);
+  console.log(arr.join(" "));
 }
-console.log(answer);
