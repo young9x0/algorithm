dae5905ef4a2c9f93c078261f296cda7b025fe66
maliethy <maliethy@gmail.com>
2021-11-28 00:09:31 +0900

    fix: 1874  스택수열 틀림

--- baekjoon/1874.js
+++ baekjoon/1874.js
@@ -1,45 +1,38 @@
-//스택수열
-const result = require("fs")
-  .readFileSync("/dev/stdin", "utf8")
-  .trim()
-  .split("\n");
-// const result = [8, 4, 3, 6, 8, 7, 5, 2, 1];
+// //스택수열
+// const result = require("fs")
+//   .readFileSync("/dev/stdin", "utf8")
+//   .trim()
+//   .split("\n");
+const result = [8, 4, 3, 6, 8, 7, 5, 2, 1];
 // const result = [5, 1, 2, 5, 3, 4];
+// const result = [5, 1, 2, 5, 4, 3];
+// const result = [5, 3, 5, 4, 2, 1];
 
-let answer = "";
-
-let stack = [],
-  temp = [],
+let answer = "",
+  stack = [],
+  resultTemp = [],
   lastIdx = 0;
 
-for (let i = 1; i <= result[0]; i++) {
-  stack.push(i);
-}
 result.shift();
-// console.log(stack);
+
 result.map((res) => {
-  stack.map((val) => {
-    // console.log(res, val);
-    //res = 4,3,6
-    if (res > val && lastIdx < val) {
-      lastIdx = val;
-      //lastIdx =1,2,3,4,5
+  //res =
+  if (res > lastIdx) {
+    for (let i = lastIdx + 1; i <= res; i++) {
       answer += "+\n";
+      stack.push(i);
+      // stack=
     }
-    if (res === val) {
-      //res,val = 4,3,6
-      if (lastIdx < val) {
-        lastIdx = val;
-        //lastIdx = 6
-        answer += "+\n";
-      }
-      if (lastIdx === val || temp[temp.length - 1] > val) {
-        temp.push(val); //4,3,6
-        answer += "-\n";
-      }
-    }
-  });
+    lastIdx = res;
+    //lastIdx =
+  }
+  if (res <= lastIdx && stack[stack.length - 1] <= res) {
+    resultTemp.push(res); //
+    stack.pop();
+    //stack=
+    answer += "-\n";
+  }
 });
-// console.log(result, temp);
-result.some((res, idx) => res !== temp[idx]) && (answer = "No");
+// console.log(result, resultTemp);
+resultTemp.some((res, idx) => res !== result[idx]) && (answer = "No");
 console.log(answer);
