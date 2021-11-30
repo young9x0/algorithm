da6f92e4c9cf131f2421739a67408a4cb3887c65
maliethy <maliethy@gmail.com>
2021-11-30 13:02:16 +0900

    fix: https://gobae.tistory.com/23 정답 확인하기

--- baekjoon/1406.js
+++ baekjoon/1406.js
@@ -1,43 +1,53 @@
-// 에디터
+// // 에디터
 // const input = require("fs")
 //   .readFileSync("/dev/stdin", "utf8")
 //   .trim()
 //   .split("\n");
 
-// const input = ["abcd", "3", "P x", "L", "P y"]; //abcdyx
-// const input = ["abc", "9", "L", "L", "L", "L", "L", "P x", "L",  "B","P y",];//yxabc
-// const input = ["dmih", "11", "B", "B", "P x", "L", "B","B", "B",  "P y", "D",  "D","P z",];//yxz
+// //L= left, D= right, B= delete, P $ = plus $
+const input = ["abcd", "3", "P x", "L", "P y"]; //abcdyx
+// // const input = ["abc", "9", "L", "L", "L", "L", "L", "P x", "L", "B", "P y"]; //yxabc
+// // const input = [
+// //   "dmih",
+// //   "11",
+// //   "B",
+// //   "B",
+// //   "P x",
+// //   "L",
+// //   "B",
+// //   "B",
+// //   "B",
+// //   "P y",
+// //   "D",
+// //   "D",
+// //   "P z",
+// // ]; //yxz
 
-let str = input.shift();
-const len = +input.shift();
-let strIdx = 0;
-let answer = "";
+let lStack = input[0].split(""),
+  rStack = [];
 
 input.map((order, idx) => {
-  const word = order.split(" ");
-  console.log("word", word);
-  switch (word[0]) {
-    case "P":
-      answer = !!answer ? answer + word[1] : str + word[1];
-      strIdx = str.length - 1;
-      console.log(answer, strIdx);
-      break;
-    case "L":
-      strIdx = strIdx > 0 ? strIdx - 1 : 0;
-      console.log(answer, strIdx);
-      break;
-    case "D":
-      strIdx = strIdx === str.length - 1 ? str.length - 1 : strIdx - 1;
-      console.log(answer, strIdx);
-      break;
-    case "B":
-      if (strIdx > 0) {
-        answer = str.substring(1);
-        strIdx -= 1;
-        console.log(answer, strIdx);
-      }
-      break;
+  if (idx > 1) {
+    const [cmd, word] = order.split(" ");
+    // console.log("word", word);
+    switch (cmd) {
+      case "P":
+        lStack.push(word);
+        // console.log(lStack, rStack);
+        break;
+      case "L":
+        lStack.length && rStack.push(lStack.pop());
+        // console.log(lStack, rStack);
+        break;
+      case "D":
+        rStack.length && lStack.push(rStack.pop());
+        // console.log(lStack, rStack);
+        break;
+      case "B":
+        lStack.pop();
+        break;
+    }
   }
 });
 
-console.log(answer);
+console.log(lStack.join("") + rStack.reverse().join(""));
