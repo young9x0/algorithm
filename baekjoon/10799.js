563bbf1545ec2480657c4f56acc3c0ba2d3a7a68
maliethy <maliethy@gmail.com>
2021-12-07 22:13:33 +0900

    fix: 10799 맞았습니다!

--- baekjoon/10799.js
+++ baekjoon/10799.js
@@ -1,27 +1,34 @@
 // 쇠막대기
-// const input = require("fs")
-//   .readFileSync("/dev/stdin", "utf8")
-//   .trim()
-//   .split("\n");
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n");
 
 // const input = ["()(((()())(())()))(())"]; // 17
-const input = ["(((()(()()))(())()))(()())"]; // 24
-
+// const input = ["(((()(()()))(())()))(()())"]; // 24
+// console.time("test");
 let stack = [],
   result = 0,
-  splitBrace = [];
+  splitBrace = [],
+  before = "";
 
 splitBrace = input[0].split("");
 
 splitBrace.map((brace, idx) => {
+  if (idx > 0) {
+    before = splitBrace[idx - 1];
+  }
   if (brace === "(") {
     stack.push(brace);
   } else if (brace === ")") {
     stack.pop();
-    if (stack.length > 0) {
+    if (before === "(") {
       result += 1 * stack.length;
+    } else {
+      result += 1;
     }
   }
-  console.log(idx, stack, result);
+  // console.log(idx, stack, before, result);
 });
 console.log(result);
+// console.timeEnd("test");
