456954e7303ca37a86c5e4a03e6c0fef9dce04f9
maliethy <maliethy@gmail.com>
2021-12-09 23:46:36 +0900

    fix: 17299 https://gigibean.tistory.com/21 정답 확인하기

--- baekjoon/17299.js
+++ baekjoon/17299.js
@@ -1,45 +1,62 @@
 // 오등큰수  NGF
-// const input = require("fs")
-//   .readFileSync("/dev/stdin", "utf8")
-//   .trim()
-//   .split("\n");
-
-const input = [
-  `7
-1 1 2 3 4 2 1
-`,
-]; // -1 -1 1 2 2 1 -1
+const fs = require("fs");
+const input = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `7
+1 1 2 3 4 2 1`
+)
+  .split("\n")[1]
+  .split(" ")
+  .map((a) => +a);
 
 // console.time("test");
-let inputStack = [],
+let cInfo = {},
   result = [],
-  splitInput = [];
-
-splitInput = input[0].split("\n")[1].split(" ");
-
-console.log("splitInput", splitInput);
-splitInput.map((list, idx) => {
-  let existCount = 0,
-    index = 0;
-  while (splitInput.length > index) {
-    if (list === splitInput[index]) {
-      existCount++;
-    }
-
-    index++;
-  }
-  inputStack[list] = existCount;
+  stack = [];
+// console.log("input", input);
+input.map((num) => {
+  cInfo[num] = (cInfo[num] || 0) + 1;
 });
-inputStack.map((count, idx) => {
-  if (idx > 0) {
-    let index = 1;
-    while (inputStack.length > index) {
-      if (count < inputStack[index]) {
-        result[idx] = splitInput[idx];
-      }
-      index++;
-    }
+
+// console.log("cInfo", cInfo);
+//{ '0': 4, '1': 3, '2': 2, '3': 1, '4': 1 }
+result = new Array(input.length).fill(-1);
+
+for (let i = 0; i < input.length; i++) {
+  // console.log(i, stack);
+  while (
+    stack.length &&
+    cInfo[input[stack[stack.length - 1]]] < cInfo[input[i]]
+  ) {
+    // console.log("while", i, stack, result);
+    result[stack.pop()] = input[i];
   }
-});
-console.log("result", result);
+  stack.push(i);
+  // console.log(i, stack);
+}
+
+//시간초과
+// input.map((list) => {
+//   let existCount = 0,
+//     index = 0;
+//   while (input.length > index) {
+//     if (list === input[index]) {
+//       existCount++;
+//     }
+
+//     index++;
+//   }
+//   cStack[list] = existCount;
+// });
+// // console.log("cStack", cStack);
+// input.map((list, idx) => {
+//   let index = idx;
+//   while (cStack[input[index]] <= cStack[list]) {
+//     ++index;
+//   }
+//   // console.log(idx, "index", index);
+//   index < input.length && (result[idx] = input[index]);
+// });
+console.log(result.join(" ")); // -1 -1 1 2 2 1 -1
 // console.timeEnd("test");
