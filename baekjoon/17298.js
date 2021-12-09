57d8e9367026f25619cc485085dfacf1d369ca2a
maliethy <maliethy@gmail.com>
2021-12-09 20:59:03 +0900

    fix: 17298 https://junghyeonsu.tistory.com/218 정답 확인하기

--- baekjoon/17298.js
+++ baekjoon/17298.js
@@ -4,7 +4,7 @@ const input = (
   process.platform === "linux"
     ? fs.readFileSync("/dev/stdin").toString()
     : `4
-9 5 4 8`
+3 5 2 7`
 )
   .split("\n")[1]
   .split(" ")
@@ -12,7 +12,6 @@ const input = (
 
 let result = new Array(input.length).fill(-1),
   stack = [];
-
 input.map((num, idx) => {
   while (stack.length && input[stack[stack.length - 1]] < num) {
     result[stack.pop()] = num;
@@ -21,3 +20,15 @@ input.map((num, idx) => {
 });
 
 console.log(result.join(" "));
+
+//메모리초과
+
+// input.map((num, idx) => {
+//   input.map((stack, index) => {
+//     if (result[idx] === -1 && index > idx && stack > num) {
+//       result[idx] = stack;
+//     }
+//   });
+// });
+
+// console.log(result.join(" "));
