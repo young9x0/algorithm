d0975a4a52a7b80945a79c2ce0e03a30ccef1d78
maliethy <maliethy@gmail.com>
2021-12-11 13:34:25 +0900

    fix: 1935 틀렸습니다

--- baekjoon/1935.js
+++ baekjoon/1935.js
@@ -1,18 +1,58 @@
 // 후위 표기식2
 const fs = require("fs");
-const input = (
-  process.platform === "linux"
-    ? fs.readFileSync("/dev/stdin").toString()
-    : `5
+// const question = `1
+// AA+A+
+// 1`;
+const question = `5
 ABC*+DE/-
 1
 2
 3
 4
-5`
+5`;
+const input = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : question
 ).split("\n");
+// console.log(3 * 2 + 1 - 4 / 5);
+//[A,B,C,*,+,D,E,/,-]
+// B * C + A - D / E
 
-let result = 0;
-result = 14.7778777855445;
-console.log(Math.floor(result * 100) / 100);
+let idx = 0,
+  nStack = [];
+const strArr = input[1].split("");
+const numArr = input.slice(2).map((n) => +n);
+
+// console.log("strArr", strArr);
+// console.log("numArr", numArr);
+
+strArr.map((str) => {
+  let signal = "";
+  if (str === "+" || str === "-" || str === "*" || str === "/") {
+    signal = str;
+  } else {
+    nStack.push(numArr[idx] ? numArr[idx++] : numArr[--idx]);
+  }
+
+  // console.log(idx, nStack, signal);
+  if (signal && nStack.length > 1) {
+    let second = nStack.pop(),
+      first = nStack.pop();
+
+    if (signal === "+") {
+      nStack.push(first + second);
+    } else if (signal === "-") {
+      nStack.push(first - second);
+    } else if (signal === "*") {
+      nStack.push(first * second);
+    } else {
+      nStack.push(first / second);
+    }
+    signal = "";
+  }
+});
+// console.log("nStack", nStack);
+
+console.log(Math.floor(nStack[0] * 100) / 100);
 // console.timeEnd("test");
