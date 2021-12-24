93b02e3469c5ae90cc9717aa893c11ae77d71f4b
maliethy <maliethy@gmail.com>
2021-12-24 21:25:54 +0900

    feat: 2609 최대공약수와 최소공배수 맞았습니다!!

---
+++ baekjoon/2609.js
@@ -0,0 +1,36 @@
+//최대공약수와 최대공배수
+//최대공약수, greatest common factors, GCF
+//최대공배수, Least Common Multiple, LCM
+const question = "24 18"; //24 = 2³ * 3  , 18 = 2* 3²
+//6
+//72
+//2³ * 3²
+const fs = require("fs");
+const input = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin", "utf8").toString()
+    : question
+).split("\n");
+
+const factors = [],
+  [fir, sec] = input[0].split(" ");
+
+input[0].split(" ").map((num, idx) => {
+  factors[idx] = [];
+  for (let i = 1; i <= num; i++) {
+    num % i === 0 && factors[idx].push(i);
+  }
+});
+// console.log("factors", factors);
+let largeFactorIdx = 0,
+  GCF = "";
+if (factors[0].length < factors[1].length) {
+  largeFactorIdx = 1;
+}
+factors[largeFactorIdx].map((el, idx) => {
+  factors[!largeFactorIdx ? 1 : 0].map((other) => {
+    if (el === other) GCF = el;
+  });
+});
+// console.log("GCF", GCF);
+console.log(GCF + "\n" + (fir * sec) / GCF);
