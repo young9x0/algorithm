76e4662fe908b7495a92db0b65d53e034d1cde20
maliethy <maliethy@gmail.com>
2022-01-05 00:01:51 +0900

    fix: 9613 시간초과

--- baekjoon/9613.js
+++ baekjoon/9613.js
@@ -17,31 +17,45 @@ const input = (
     : question
 ).split("\n");
 
-let result = 0,
-  GCD = 0;
+let answer = [];
+
 input.map((str, idx) => {
   if (idx > 0) {
+    let result = 0;
     const splited = str.split(" ");
-    splited.shift();
+
+    console.log("splited", splited);
+
     splited.map((nums, index) => {
-      for (let i = 0; i < splited.length; i++) {
-        splited[i + index] && (result += FindGCD(nums, splited[i + index]));
+      if (index > 0) {
+        for (let i = 1; i < splited.length; i++) {
+          if (splited[i + index]) {
+            result += FindGCD(nums, splited[i + index]);
+          }
+        }
       }
     });
+    !!result && answer.push(result);
   }
 });
 
 function FindGCD(fir, sec) {
-  let limit = 0;
+  // console.log("fir", fir);
+  // console.log("sec", sec);
+  let GCD = 1,
+    limit = fir;
+
   if (fir > sec) {
     limit = sec;
-  } else {
-    limit = fir;
   }
-  for (let i = 1; i <= limit; i++) {
+
+  for (let i = 2; i <= limit; i++) {
     if (fir % i === 0 && sec % i === 0) {
-      !GCD || (GCD < i && (GCD = i));
+      console.log(fir, sec, i, GCD);
+      (!GCD || GCD < i) && (GCD = i);
     }
   }
   return GCD;
 }
+
+console.log(answer.join("\n"));
