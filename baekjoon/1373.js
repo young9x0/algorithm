86e393e8011bb8934b2f527851065eed1c474686
maliethy <maliethy@gmail.com>
2022-01-08 16:58:22 +0900

    fix: 1373 https://bonggang.tistory.com/344 정답 확인하기

--- baekjoon/1373.js
+++ baekjoon/1373.js
@@ -8,8 +8,27 @@ const question = "11001100"; //314
 const fs = require("fs");
 const input = (
   process.platform === "linux"
-    ? fs.readFileSync("/dev/stdin", "utf8").toString().trim()
+    ? fs.readFileSync("/dev/stdin", "utf8").toString()
     : question
 ).split("\n");
 
-console.log(parseInt(input[0], 2).toString(8));
+let temp = [],
+  result = 0;
+
+input[0]
+  .split("")
+  .reverse()
+  .map((str, idx) => {
+    const num = Number(str);
+    // console.log(num, idx % 3, result, temp);
+    if (!(idx % 3)) {
+      result += num;
+    } else {
+      result += num * (2 * (idx % 3));
+    }
+    if ((idx + 1) % 3 === 0 || idx === input[0].length - 1) {
+      temp.push(result);
+      result = 0;
+    }
+  });
+console.log(temp.reverse().join(""));
