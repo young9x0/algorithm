b604f667cc274b520db2390c1d3d0f506dd1a739
maliethy <maliethy@gmail.com>
2022-01-11 16:50:59 +0900

    fix: 2089 진행중

--- baekjoon/2089.js
+++ baekjoon/2089.js
@@ -9,11 +9,29 @@ const input = (
     : question
 ).split("\n");
 
-console.log(input[0]);
+let difference = 0,
+  plus = [1],
+  minus = [-2];
 
-if (input[0] < 0) {
-  //짝수번째
-  // 2^4+2^2- 2^3+2^1
+let i = 2;
+if (input[0] < -1) {
+  while (input[0] < minus[minus.length - 1]) {
+    i % 2 ? minus.push(-Math.pow(2, i)) : plus.push(Math.pow(2, i));
+    i++;
+  }
+  difference = input[0] - minus[minus.length - 1];
+  toBinary(difference, i - 1);
 } else {
-  //홀수번째
+  while (input[0] < plus[plus.length - 1]) {
+    i % 2 ? minus.push(-Math.pow(2, i)) : plus.push(Math.pow(2, i));
+    i++;
+  }
+  difference = plus[plus.length - 1] - input[0];
+  toBinary(difference, i - 1);
 }
+
+function toBinary(difference, i) {
+  console.log(difference, i);
+}
+
+console.log(plus, minus);
