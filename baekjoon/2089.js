48b9dcf5287c5dd29b13303d44cf4397584ebed8
maliethy <maliethy@gmail.com>
2022-01-13 20:18:49 +0900

    fix: 2089 https://jjuyaa.tistory.com/93?category=1179351 정답 확인하기

--- baekjoon/2089.js
+++ baekjoon/2089.js
@@ -9,42 +9,12 @@ const input = (
     : question
 ).split("\n");
 
-let difference = 0,
-  temp = [],
-  result = [],
-  i = 0;
+let list = [],
+  N = input[0];
+if (N === 0) console.log(N);
 
-while (!temp.length || input[0] < temp[temp.length - 1]) {
-  i % 2 ? temp.push(-Math.pow(2, i)) : temp.push(Math.pow(2, i));
-  i++;
+while (N !== 0) {
+  list.push(Math.abs(N % -2));
+  N = Math.ceil(N / -2);
 }
-
-// if (input[0] < -1) {
-//   difference = input[0] - temp[temp.length - 1];
-// } else {
-
-// }
-
-difference = temp[temp.length - 1] - input[0];
-temp[temp.length - 1] = 1;
-function toBinary(difference, i) {
-  console.log(i, difference, temp[i - 1], temp);
-
-  if (i > 0) {
-    if (!((i - 1) % 2) && difference <= temp[i - 1]) {
-      //1, 4, 16
-      difference = difference + temp[i - 1];
-      temp[i - 1] = 1;
-    } else if (!!((i - 1) % 2) && difference <= temp[i - 1]) {
-      //-2, -8
-      difference = difference - temp[i - 1];
-      temp[i - 1] = 1;
-    } else {
-      temp[i - 1] = 0;
-    }
-    toBinary(difference, i - 1);
-  }
-
-  return temp;
-}
-console.log(toBinary(difference, i - 1));
+console.log(list.reverse().join(""));
