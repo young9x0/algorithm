029ffea6a56dd61e96bd4688022c2b4d637c1914
maliethy <maliethy@gmail.com>
2022-01-13 20:09:18 +0900

    fix: toBinary 조건 test하기

--- baekjoon/2089.js
+++ baekjoon/2089.js
@@ -14,25 +14,37 @@ let difference = 0,
   result = [],
   i = 0;
 
-if (input[0] < -1) {
-  while (!temp.length || input[0] < temp[temp.length - 1]) {
-    i % 2 ? temp.push(-Math.pow(2, i)) : temp.push(Math.pow(2, i));
-
-    i++;
-  }
-  difference = temp[temp.length - 1] - input[0];
-  toBinary(difference, i - 1);
+while (!temp.length || input[0] < temp[temp.length - 1]) {
+  i % 2 ? temp.push(-Math.pow(2, i)) : temp.push(Math.pow(2, i));
+  i++;
 }
+
+// if (input[0] < -1) {
+//   difference = input[0] - temp[temp.length - 1];
+// } else {
+
+// }
+
+difference = temp[temp.length - 1] - input[0];
+temp[temp.length - 1] = 1;
 function toBinary(difference, i) {
-  console.log(difference, i);
+  console.log(i, difference, temp[i - 1], temp);
 
-  temp[i] = 1;
-
-  if (difference < temp[i - 1]) {
-    difference = difference + temp[i - 1];
+  if (i > 0) {
+    if (!((i - 1) % 2) && difference <= temp[i - 1]) {
+      //1, 4, 16
+      difference = difference + temp[i - 1];
+      temp[i - 1] = 1;
+    } else if (!!((i - 1) % 2) && difference <= temp[i - 1]) {
+      //-2, -8
+      difference = difference - temp[i - 1];
+      temp[i - 1] = 1;
+    } else {
+      temp[i - 1] = 0;
+    }
+    toBinary(difference, i - 1);
   }
 
-  !difference ? temp : toBinary(difference, i - 1);
+  return temp;
 }
-
-console.log(temp, result);
+console.log(toBinary(difference, i - 1));
