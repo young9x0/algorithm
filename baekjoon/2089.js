9fd7446c01bc8c026f00234b9e0cef25cfe012f7
maliethy <maliethy@gmail.com>
2022-01-13 11:41:29 +0900

    fix: 2089 plus, minus arr를 temp arr로 통합하기  <type>: <subject>

--- baekjoon/2089.js
+++ baekjoon/2089.js
@@ -10,28 +10,29 @@ const input = (
 ).split("\n");
 
 let difference = 0,
-  plus = [1],
-  minus = [-2];
+  temp = [],
+  result = [],
+  i = 0;
 
-let i = 2;
 if (input[0] < -1) {
-  while (input[0] < minus[minus.length - 1]) {
-    i % 2 ? minus.push(-Math.pow(2, i)) : plus.push(Math.pow(2, i));
+  while (!temp.length || input[0] < temp[temp.length - 1]) {
+    i % 2 ? temp.push(-Math.pow(2, i)) : temp.push(Math.pow(2, i));
+
     i++;
   }
-  difference = input[0] - minus[minus.length - 1];
-  toBinary(difference, i - 1);
-} else {
-  while (input[0] < plus[plus.length - 1]) {
-    i % 2 ? minus.push(-Math.pow(2, i)) : plus.push(Math.pow(2, i));
-    i++;
-  }
-  difference = plus[plus.length - 1] - input[0];
+  difference = temp[temp.length - 1] - input[0];
   toBinary(difference, i - 1);
 }
-
 function toBinary(difference, i) {
   console.log(difference, i);
+
+  temp[i] = 1;
+
+  if (difference < temp[i - 1]) {
+    difference = difference + temp[i - 1];
+  }
+
+  !difference ? temp : toBinary(difference, i - 1);
 }
 
-console.log(plus, minus);
+console.log(temp, result);
