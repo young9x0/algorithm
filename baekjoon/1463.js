862351e2c0d4a98e9fa82147519ec0b85d95acc3
juyoung <juyoung@juyoungui-MacBookPro.local>
2022-01-23 22:29:22 +0900

    fix: 1463 틀렸습니다

--- baekjoon/1463.js
+++ baekjoon/1463.js
@@ -1,25 +1,36 @@
 //1로 만들기
 
-// const question = `2`;//1
+// const question = `25`; //3
+// const question = `2`; //1
 const question = `10`; //3
 
-const fs = require("fs");
+const fs = require('fs');
 const input = (
-  process.platform === "linux"
-    ? fs.readFileSync("/dev/stdin", "utf8").toString().trim()
+  process.platform === 'linux'
+    ? fs.readFileSync('/dev/stdin', 'utf8').toString().trim()
     : question
-).split("\n");
+).split('\n');
 
 const N = Number(input[0]);
-let count = 0,
-  maxThree = 0;
+let count = 0;
 
-if (N < 3) {
-  N > 1 ? (count = 1) : (count = 0);
-} else {
-  let multiple = 1;
-  while (N >= multiple * 3) {
-    multiple += 1;
+multiple(N, 3);
+
+function multiple(num, idx) {
+  // console.log(num, idx, count);
+  if (num > 0) {
+    if (num >= idx) {
+      let multipleIdx = 1;
+      while (num >= Math.pow(idx, multipleIdx)) {
+        multipleIdx++;
+      }
+      count += multipleIdx - 1;
+      multiple(num - Math.pow(idx, count), idx - 1);
+    } else if (num < idx) {
+      count += 1;
+    }
   }
+  return count;
 }
+
 console.log(count);
