9b0caa765963d29e0ea3a725f670b474de403ebf
juyoung <juyoung@juyoungui-MacBookPro.local>
2022-01-25 11:41:49 +0900

    fix: 1463 StackSizeExceeded

--- baekjoon/1463.js
+++ baekjoon/1463.js
@@ -1,6 +1,7 @@
 //1로 만들기
+//https://mizzo-dev.tistory.com/entry/baekjoon1463
 
-// const question = `25`; //3
+// const question = `25`; //6
 // const question = `2`; //1
 const question = `10`; //3
 
@@ -11,26 +12,47 @@ const input = (
     : question
 ).split('\n');
 
-const N = Number(input[0]);
+const num = Number(input[0]);
+let prev = [num],
+  next = [];
 let count = 0;
+// 1. depth+, makeNext
+// 2. check one
+// 3. depth+, makeNext
+// ...
 
-multiple(N, 3);
-
-function multiple(num, idx) {
-  // console.log(num, idx, count);
-  if (num > 0) {
-    if (num >= idx) {
-      let multipleIdx = 1;
-      while (num >= Math.pow(idx, multipleIdx)) {
-        multipleIdx++;
-      }
-      count += multipleIdx - 1;
-      multiple(num - Math.pow(idx, count), idx - 1);
-    } else if (num < idx) {
-      count += 1;
-    }
-  }
-  return count;
+if (next.length === 0) {
+  count++;
+  checkOne(makeNext(prev, next));
 }
 
-console.log(count);
+function makeNext(prev, next) {
+  // console.log('prev', count, prev);
+  prev.map((num) => {
+    if (!(num % 3)) {
+      next.push(num / 3);
+    }
+    if (!(num % 2)) {
+      next.push(num / 2);
+    }
+
+    if (num > 1) {
+      next.push(num - 1);
+    }
+  });
+
+  return next;
+}
+
+function checkOne(next) {
+  // console.log('next', count, next);
+  const filterOne = next.filter((res) => res === 1);
+  if (filterOne.length) {
+    return console.log(count);
+  } else {
+    count++;
+    prev = next;
+    next = [];
+    checkOne(makeNext(prev, next));
+  }
+}
