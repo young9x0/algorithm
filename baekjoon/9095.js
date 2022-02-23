acf8445e95902de9aaa4f0d085f6140849a4b5ad
maliehty <maliethy@gmail.com>
2022-02-23 22:12:50 +0900

    fix: 9095 정답과 같은 출력 성공, 틀렸습니다

--- baekjoon/9095.js
+++ baekjoon/9095.js
@@ -12,39 +12,34 @@ const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString().trim() : question).split(
   '\n',
 );
-
-// console.log(input);
-// input.map(n=>{
-//   const num = Number(n)
-//   if(num>3){
-
-//   }
-// })
-
+let result = [];
 const checkLength = {
   1: 0,
   2: { 1: 1 },
   3: { 1: 2, 2: 1, 3: 0 }, //3
-  4: { 1: 4, 2: 2, 3: 1 }, //7
-  5: { 1: 7, 2: 4, 3: 2 }, //12
-  6: { 1: 12, 2: 7, 3: 4 }, //23
-  7: { 1: 23, 2: 9, 3: 5 },
+  // 4: { 1: 4, 2: 2, 3: 1 }, //7
+  // 5: { 1: 7, 2: 4, 3: 2 }, //12
 };
+
 function makeCheckLength(raw) {
-  for (let num = 4; num <= raw; num++) {
-    let twoDivideCheck = 2,
-      threeDivideCheck = 1;
-    if (num % 2 === 0) twoDivideCheck = 3;
-    if (num % 3 === 0) threeDivideCheck = 2;
-    console.log('num', num);
-    console.log('twoDivideCheck', twoDivideCheck);
-    console.log('threeDivideCheck', threeDivideCheck);
-    checkLength[num] = {
-      1: checkLength[num - 1][1] + checkLength[num - 1][2] + checkLength[num - 1][3],
-      2: checkLength[num - 1][2] + twoDivideCheck,
-      3: checkLength[num - 1][3] + threeDivideCheck,
+  for (let num = 3; num < raw; num++) {
+    // console.log('num', num);
+
+    checkLength[num + 1] = {
+      1: checkLength[num][1] + checkLength[num][2] + (checkLength[num][3] || checkLength[num - 1][1]),
+      2: checkLength[num][1],
+      3: checkLength[num][2],
     };
-    console.log(checkLength);
+    // console.log(checkLength);
   }
+  return checkLength[raw][1] + checkLength[raw][2] + checkLength[raw][3];
 }
-makeCheckLength(7);
+
+input.map((n, idx) => {
+  if (idx > 0) {
+    const int = Number(n);
+    if (int < 12) result.push(makeCheckLength(int));
+  }
+});
+
+console.log(result.join('\n'));
