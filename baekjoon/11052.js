f0e48e405ba7c44068c2372ab7605ed040bdd0a2
maliehty <maliethy@gmail.com>
2022-02-27 22:25:47 +0900

    fix: 11052 31%까지 통과

--- baekjoon/11052.js
+++ baekjoon/11052.js
@@ -8,61 +8,40 @@
 // 1 1 2 3 5 8 13 21 34 55`; //55
 // const question = `10
 // 5 10 11 12 13 30 35 40 45 47`; //50
-const question = `4
-5 2 8 10`; //20
 // const question = `4
-// 3 5 15 16`; //18
+// 5 2 8 10`; //20
+const question = `4
+3 5 15 16`; //18
 
 const fs = require('fs');
-const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').trim() : question).split('\n');
-let result = 0,
-  PList = { 0: 0 },
-  wanted = Number(input[0]);
-
+const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString().trim() : question).split(
+  '\n',
+);
+let pList = { 0: 0 },
+  wanted = Number(input[0]),
+  pRes = 0;
 const splited = input[1].split(' ');
 
 for (let i = 1; i < wanted + 1; i++) {
-  PList[i] = Number(splited[i - 1]);
+  pList[i] = Number(splited[i - 1]);
 }
-
-// console.log('PList', PList);
+// console.log('pList', pList);
 for (let i = 1; i < wanted + 1; i++) {
-  // console.log('i', i, 'result', result);
-  result = PList[1] * wanted;
-  let temp = checkDividedMax(i, wanted - i, 0);
-  //1,4
-  if (result < temp) result = temp;
-}
-
-function checkDividedMax(fixed, excepted, acc) {
-  // console.log('fixed', fixed, 'excepted', excepted, 'acc', acc);
-
-  let next = 0,
-    cur = acc + makeSum(fixed, excepted);
-  //1 4 = 10 7=17
-  //1 3 1= 10 8 10 = 28
-
-  if (excepted > 2) {
-    next = acc + PList[fixed] + makeSum(excepted - 1, 1);
-    //1 3 1=10 8 10 = 28
-  } else {
-    next = acc + PList[fixed] + PList[1] * excepted;
-    // 10 40=50
+  let cardLen = wanted;
+  while (i * cardLen > wanted) {
+    cardLen--;
   }
-
-  // console.log('cur', cur);
-  // console.log('next', next);
-  if (cur < next) {
-    if (excepted > 2) {
-      checkDividedMax(excepted - 1, 1, PList[fixed]);
+  // console.log('cardLen', cardLen);
+  let count = cardLen,
+    temp = pList[count];
+  while (count > 0) {
+    if (wanted !== count * i) {
+      temp = pList[i] * count + pList[wanted - count * i];
+      // console.log(i, '카드가', count, '개', '나머지 카드', wanted - count * i, '총액은', temp);
     }
-    cur = next;
+    pRes < temp && (pRes = temp);
+    count--;
   }
-  return cur;
 }
 
-function makeSum(fixed, excepted) {
-  return PList[fixed] + PList[excepted];
-}
-
-console.log(result);
+console.log(pRes);
