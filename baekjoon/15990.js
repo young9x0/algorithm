d3693d30de80cf25dbded244714faed2531326ab
maliehty <maliethy@gmail.com>
2022-04-02 13:09:04 +0900

    fix: 15990 reduce 대체해도 여전히 시간초과

--- baekjoon/15990.js
+++ baekjoon/15990.js
@@ -15,51 +15,49 @@ const result = [];
 input.map((N, idx) => {
   if (idx > 0) {
     const num = Number(N);
-
     result.push(makeAddFactors(num) % 1000000009);
   }
 });
 
+// makeAddFactors(7);
 function makeAddFactors(num) {
-  let count = 0;
+  const checkCount = { count: 0 };
+  let history = { sum: 0, prev: 0 };
   // console.log('------------input num', num);
   for (let i = 1; i < 4; i++) {
     if (num === i) {
-      count += 1;
+      checkCount.count += 1;
     } else {
-      const addFactors = [];
-
-      count += checkPrev(num, [i], addFactors).length;
-      // console.log('================count is', count);
+      history.prev = i;
+      history.sum = i;
+      checkPrev(num, checkCount, history);
+      // console.log('input result', checkCount.count);
     }
   }
 
-  return count;
+  return checkCount.count;
 }
 
-function checkPrev(num, temp, addFactors) {
-  // console.log('temp', temp);
-  const tempReduce = temp.reduce((acc, cur) => acc + cur, 0);
-  // console.log('num-tempReduce', num - tempReduce);
+function checkPrev(num, checkCount, history) {
+  // console.log('count', checkCount.count);
+  // console.log('sum', history.sum);
   for (let i = 1; i < 4; i++) {
-    if (temp[temp.length - 1] !== i) {
+    if (history.prev !== i) {
       // console.log('i', i);
-      // console.log('reduce', tempReduce);
-      if (tempReduce !== num)
-        if (num - tempReduce > i) {
+
+      if (history.sum !== num) {
+        if (num - history.sum > i) {
           // console.log('again------------------');
 
-          checkPrev(num, temp.concat(i), addFactors);
-        } else if (num - tempReduce === i) {
-          // console.log('num === i', num - tempReduce === i);
-          // console.log('temp final', temp.concat(i));
-          temp.concat(i);
-          addFactors.push(temp);
+          checkPrev(num, checkCount, { sum: history.sum + i, prev: i });
+        } else if (num - history.sum === i) {
+          // console.log('final', history);
+
+          checkCount.count++;
         }
+      }
     }
   }
-
-  return addFactors;
 }
 
 console.log(result.join('\n'));
