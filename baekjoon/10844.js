7289b346ef44d550628129d43d53e2da83c16637
maliehty <maliethy@gmail.com>
2022-04-06 21:29:22 +0900

    fix: 10844 시간초과

--- baekjoon/10844.js
+++ baekjoon/10844.js
@@ -1,14 +1,33 @@
 //쉬운 계단 수
 
 // const question = `1`; //9
-const question = `2`; //17
+// const question = `2`; //17
+const question = `3`; //32
 
 const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').trim() : question).split('\n');
-const num = input[0];
+const num = Number(input[0]);
+
+const nextChildLenType = [1, 2, 2, 2, 2, 2, 2, 2, 2, 1];
 
 let result = 0;
+if (num > 1) {
+  for (let i = 10; i < 10 ** (num - 1); i++) {
+    const temp = i + '';
+    const splited = temp.split('');
+    // console.log('splited', splited);
+    const lastNum = splited[splited.length - 1];
+    const secLastNum = splited[splited.length - 2];
 
-console.log(10 ** num);
+    if (+splited[0] !== 0 && Math.abs(lastNum - secLastNum) === 1) {
+      //   console.log('lastNum', lastNum);
+      //   console.log('secLastNum', secLastNum);
+      result += nextChildLenType[lastNum];
+    }
+    // console.log('result', result);
+  }
+} else {
+  result = 9;
+}
 
 console.log(result % 1000000000);
