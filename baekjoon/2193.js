8d021fcc3e33ab08b70ca8d95da111dc6d83114a
admin <admin@adminui-MacBookPro-2.local>
2022-04-08 12:15:38 +0900

    fix: 2193 틀렸습니다

--- baekjoon/2193.js
+++ baekjoon/2193.js
@@ -1,6 +1,6 @@
 //이친수
 
-const question = '3'; //2
+const question = '6'; //2
 
 const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
@@ -11,16 +11,12 @@ let pinaryPrev = [1, 0];
 for (let i = 2; i < num ; i++) {
   // console.log('i', i);
 let temp=[0,0]
-    for (let flagZero = 0; flagZero < pinaryPrev[0]; flagZero++) {
-      temp[0] += 1;
-      temp[1] += 1;
-    }
-    for (let flagOne = 0; flagOne < pinaryPrev[1]; flagOne++) {
-      temp[0] += 1;
-    }
+    temp[0] += pinaryPrev[0] + pinaryPrev[1];
+    temp[1] += pinaryPrev[0];
+
     pinaryPrev = temp
 }
-
+// console.log(pinaryPrev)
 console.log( pinaryPrev[0]+pinaryPrev[1]);
 
 
