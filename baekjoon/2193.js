94046ad3365dcb600a7196bfef73548ae0382332
admin <admin@adminui-MacBookPro-2.local>
2022-04-08 12:06:40 +0900

    fix: 2193 시간초과2

--- baekjoon/2193.js
+++ baekjoon/2193.js
@@ -1,35 +1,26 @@
 //이친수
 
-const question = '6'; //2
+const question = '3'; //2
 
 const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
 
 const num = +input[0];
+let pinaryPrev = [1, 0];
 
-const pinary = Array(num - 1);
-pinary[0] = [0, 1];
-pinary[1] = [1, 0];
-let flag = 2;
-while (flag < num - 1) {
-  pinary[flag++] = [0, 0];
-}
-
-for (let i = 1; i < num - 2; i++) {
+for (let i = 2; i < num ; i++) {
   // console.log('i', i);
-
-  if (i + 1 < num) {
-    for (let flagZero = 0; flagZero < pinary[i][0]; flagZero++) {
-      pinary[i + 1][0] += 1;
-      pinary[i + 1][1] += 1;
+let temp=[0,0]
+    for (let flagZero = 0; flagZero < pinaryPrev[0]; flagZero++) {
+      temp[0] += 1;
+      temp[1] += 1;
     }
-    for (let flagOne = 0; flagOne < pinary[i][1]; flagOne++) {
-      pinary[i + 1][0] += 1;
+    for (let flagOne = 0; flagOne < pinaryPrev[1]; flagOne++) {
+      temp[0] += 1;
     }
-  }
-
-  // console.log('pinary', pinary);
+    pinaryPrev = temp
 }
-const count = pinary.reduce((acc, cur) => acc + cur[0], 1);
 
-console.log(count);
+console.log( pinaryPrev[0]+pinaryPrev[1]);
+
+
