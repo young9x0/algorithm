fc6e38a41ab40fb654f3c3290909bea4a2acb9d2
admin <admin@adminui-MacBookPro-2.local>
2022-04-08 12:30:43 +0900

    fix: 2193 https://webruden.tistory.com/1038 정답 확인하기

--- baekjoon/2193.js
+++ baekjoon/2193.js
@@ -1,22 +1,24 @@
 //이친수
 
-const question = '6'; //2
+const question = '7'; //2
 
 const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
 
 const num = +input[0];
-let pinaryPrev = [1, 0];
+const pinaryPrev = [1, 0];
 
 for (let i = 2; i < num ; i++) {
   // console.log('i', i);
-let temp=[0,0]
-    temp[0] += pinaryPrev[0] + pinaryPrev[1];
-    temp[1] += pinaryPrev[0];
+  // console.log('pinaryPrev',pinaryPrev)
+  let newZero=0, newOne=0;
 
-    pinaryPrev = temp
+    newZero = BigInt(pinaryPrev[0] + pinaryPrev[1]);
+    newOne = BigInt(pinaryPrev[0]) 
+    pinaryPrev[0] = newZero;
+    pinaryPrev[1] = newOne;
 }
 // console.log(pinaryPrev)
-console.log( pinaryPrev[0]+pinaryPrev[1]);
+console.log(String(pinaryPrev[0]+pinaryPrev[1]));
 
 
