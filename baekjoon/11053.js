ae9f28ceb020fa5bcbb2492dfe9dd0aeccc9abc0
maliethy <maliethy@gmail.com>
2022-04-11 23:59:39 +0900

    fix: 11053 https://nyang-in.tistory.com/m/275 정답 확인하기

--- baekjoon/11053.js
+++ baekjoon/11053.js
@@ -1,19 +1,33 @@
 //가장 긴 증가하는 부분 수열
 
-const question = `6
-10 20 10 30 20 50 40 70 80 90 50 40 110 100`; //4
+const question = `11
+10 20 10 30 20 50 60 40 50 70 80`; //4
 
 const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
-
+const len = Number(input[0]);
 const sequence = input[1].split(' ').map(Number);
 // console.log(sequence);
-let prev = sequence[0],
-  count = 1;
-sequence.map((int) => {
-  if (prev < int) {
-    prev = int;
-    count++;
+const dp = Array(len).fill(1);
+let result = 0;
+
+for (let i = 0; i < len; i++) {
+  let count = 1;
+  for (let j = 0; j < i; j++) {
+    // console.log('i', i, sequence[i]);
+    // console.log('j', j, sequence[j]);
+    if (sequence[i] > sequence[j] && count <= dp[j]) {
+      count = dp[j] + 1;
+    }
+    // console.log('count', count);
   }
-});
-console.log(count);
+
+  if (dp[i] < count) {
+    dp[i] = count;
+  }
+  // console.log('dp', dp);
+  if (result < count) {
+    result = count;
+  }
+}
+console.log(result);
