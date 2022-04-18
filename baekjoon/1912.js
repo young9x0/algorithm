f565b6e8dfa1dbcb090690a6831da34c7a489894
maliethy <maliethy@gmail.com>
2022-04-18 11:14:35 +0900

    fix: 1912 https://kscodebase.tistory.com/359 정답 확인하기

--- baekjoon/1912.js
+++ baekjoon/1912.js
@@ -1,29 +1,24 @@
 //52. 연속합
 
-// const question = `10
-// 10 -4 3 1 5 6 -35 12 21 -1`; //33
+const question = `10
+10 -4 3 1 5 6 -35 12 21 -1`; //33
 // const question = `10
 // 2 1 -4 3 4 -4 6 5 -5 1`; //14
-const question = `5
--1 -2 -3 -4 -5`; //-1
+// const question = `5
+// -1 -2 -3 -4 -5`; //-1
 
 const fs = require('fs');
 const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
 const len = Number(input[0]);
 const sequence = input[1].split(' ').map(Number);
 // console.log(sequence);
-let result = sequence[0];
+const DP = [];
 
 for (let i = 0; i < len; i++) {
-  //   console.log('slice is:', sequence.slice(i, len));
-  let max = sequence[i];
-  sequence.slice(i, len).reduce((acc, cur, idx) => {
-    if (idx + 1 < len) {
-      max < acc + cur && (max = acc + cur);
-      return acc + cur;
-    }
-  }, 0);
-  //   console.log('max is:', i, max);
-  result < max && (result = max);
+  DP[i] = sequence[i];
+  //   console.log(DP[i], DP[i - 1] + sequence[i]);
+  if (DP[i] < DP[i - 1] + sequence[i]) {
+    DP[i] = DP[i - 1] + sequence[i];
+  }
 }
-console.log(result);
+console.log(Math.max(...DP));
