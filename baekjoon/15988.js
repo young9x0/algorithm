55fca3c4b2e3ff19e0d4e9fa3b4d797dbf8bf6a1
maliethy <maliethy@gmail.com>
2022-12-13 00:21:00 +0900

    56. RGB거리 - 혼자서 못풀겠다 꾀꼬리

--- baekjoon/15988.js
+++ baekjoon/15988.js
@@ -1,5 +1,5 @@
 //55.1, 2, 3 더하기 3
-
+// 정답 참고 https://leylaoriduck.tistory.com/529
 const question = `3
 4
 7
@@ -10,24 +10,18 @@ const question = `3
 
 const fs = require('fs');
 
-const input = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
+const inputs = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
 // n을 1, 2, 3의 합으로 나타내는 방법의 수를 1_000_000_009 로 나눈 나머지를 출력한다.
 
-const DP = [
-  [1, 0, 0],
-  [1, 1, 0],
-];
-const answer = [];
+const cases = Number(inputs[0]);
+inputs.shift();
+const arr = [0, 1, 2, 4];
+let num = Math.max(...inputs);
 
-for (let t = 1; t <= input[0]; t++) {
-  let n = input[t];
-  for (let i = 1; i < input[t]; i++) {
-    DP[i + 1] = [];
-    DP[i + 1][0] = (DP[i][0] + DP[i][1] + DP[i][2]) % 1_000_000_009;
-    DP[i + 1][1] = DP[i][0];
-    DP[i + 1][2] = DP[i][1];
-  }
-  // console.log('DP', DP[n][0]);
-  answer.push(DP[n][0]);
+for (let j = 4; j <= num; j++) {
+  arr[j] = (arr[j - 1] + arr[j - 2] + arr[j - 3]) % 1000000009;
 }
-console.log(answer.join('\n'));
+
+for (let i = 0; i < cases; i++) {
+  console.log(arr[inputs[i]]);
+}
