125ef6dd83c18b18ab584e08b3b1e9dbfa20ea42
maliethy <maliethy@gmail.com>
2023-01-08 14:54:54 +0900

    백준 2156 포도주 시식 - 정답확인

--- baekjoon/2156.js
+++ baekjoon/2156.js
@@ -1,5 +1,6 @@
 //60. 2156 포도주 시식
 //1/8 pm 12:00 - 14:04
+//해설: https://tesseractjh.tistory.com/116(가장 효율적인 풀이), https://amunre21.github.io/boj/2-2156/(생각할 땐 최신 기준으로 생각하기)
 const question = `6
 6
 10
@@ -7,44 +8,33 @@ const question = `6
 9
 8
 1
-1
-2
-16
-8
-8
-8`;
+`;
 //33
 
 const fs = require('fs');
 
 const inputs = (process.platform === 'linux' ? fs.readFileSync('/dev/stdin', 'utf8').toString() : question).split('\n');
 
-const splited = inputs.map(Number);
-const n = splited[0];
+const [n, ...wine] = inputs.map(Number);
+const solve = (n, wine) => {
+  if (n === 1) return wine[0];
+  else if (n === 2) return wine[0] + wine[1];
 
-const dp = [];
-for (let i = 0; i < n / 3; i++) {
-  const idx = i * 3;
+  const dp = new Array(n).fill(0);
+  dp[1] = wine[0];
+  dp[2] = wine[0] + wine[1];
 
-  dp[i] = [];
-  dp[i][0] = splited[idx + 1] + splited[idx + 2];
-  dp[i][1] = splited[idx + 1] + splited[idx + 3];
-  dp[i][2] = splited[idx + 2] + splited[idx + 3];
-}
-
-// console.log(n, dp);
-
-let temp = Math.max(dp[0][0] + dp[1][0], dp[0][0] + dp[1][1], dp[0][0] + dp[1][2]);
-for (let k = 1; k < 3; k++) {
-  const newSum = Math.max(dp[0][k] + dp[1][1], dp[0][k] + dp[1][2]);
-  if (temp < newSum) temp = newSum;
-}
-
-const result = dp.reduce((acc, cur, idx) => {
-  if (idx < 2) {
-    return acc;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = Math.max(
+      // 지금 먹음
+      dp[i - 3] + wine[i - 2] + wine[i - 1], //  x, o, o
+      dp[i - 2] + wine[i - 1], // o, x, o
+      // 지금 안 먹음
+      dp[i - 1], // o, o, x
+    );
   }
-  return acc + Math.max(cur[0], cur[1], cur[2]);
-}, temp);
 
-console.log(result);
+  return dp[n];
+};
+
+console.log(solve(n, wine));
