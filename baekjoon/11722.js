201107cf57c370e1f6c9c65f46a52b066179e6e6
maliethy <maliethy@gmail.com>
2023-01-29 23:35:17 +0900

    백준 11722 가장 긴 감소하는 부분 수열 - 해설확인

--- baekjoon/11722.js
+++ baekjoon/11722.js
@@ -1,10 +1,10 @@
 //61. 11722 가장 긴 감소하는 부분 수열
-//1/29 pm 8:44 -
-//해설:
-const question = `1
-10`;
-// const question = `6
-// 10 30 10 20 20 10`;
+//1/29 pm 8:44 - pm 11:35
+//해설: https://leylaoriduck.tistory.com/528
+// const question = `1
+// 10`;
+const question = `6
+10 30 10 20 20 10`;
 // // 3;
 // const question = `11
 // 10 9 30 20 10 8 7 6 20 20 10`;
@@ -16,35 +16,22 @@ const [total, str] = inputs;
 const arr = str.split(' ').map(Number);
 const N = Number(total);
 
-const dp = Array(N).fill(1);
+const dp = Array(N).fill(0);
 
-for (let i = 0; i < N - 1; i++) {
-  let idx = i,
-    count = 1;
+for (let i = 0; i < N; i++) {
+  let max = 0;
 
-  // console.log('arr[i]', i, arr[i]);
-  for (let j = i; j < N; j++) {
-    // console.log('arr[j]', j, arr[j]);
-
-    if (arr[i] < arr[j]) {
-      break;
-    }
-
-    if (arr[idx] < arr[j]) {
-      idx = j;
-      dp[i] = Math.max(dp[i], count);
-      count = 0;
-    }
-
-    // console.log('arr[idx]', arr[idx]);
-    if (arr[i] > arr[j] && arr[idx] >= arr[j]) {
-      idx = j;
-      count += 1;
-      // console.log('dp[i]', dp[i]);
+  // console.log('arr[i]', arr[i]);
+  for (let j = 0; j < i; j++) {
+    if (arr[j] > arr[i] && dp[j] > max) {
+      // console.log('arr[j]', arr[j]);
+      max = dp[j];
     }
   }
 
-  dp[i] = Math.max(dp[i], count);
+  // console.log('max', max);
+  dp[i] = max + 1;
 }
+
 // console.log(dp);
 console.log(Math.max(...dp));
