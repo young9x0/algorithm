d5c866d20e65d3db59510b44e430d4315636a0e4
maliethy <maliethy@gmail.com>
2023-12-28 23:37:21 +0900

    유사 칸토어 비트열 해법

--- programmers/level2/148652.js
+++ programmers/level2/148652.js
@@ -1,36 +1,23 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/148652
 //유사 칸토어 비트열
+//해설: https://velog.io/@sean2337/Programmers-%EC%9C%A0%EC%82%AC-%EC%B9%B8%ED%86%A0%EC%96%B4-%EB%B9%84%ED%8A%B8%EC%97%B4-JavaScript
+function check(num) {
+  // 5이하로 떨어졌을 때 11011이기 => 2만 제외하고 true 리턴
+  if (num < 5 && num != 2) return true;
+  // 5개 중에 2번째 값은 0이기 때문에 false 리턴
+  if ((num - 2) % 5 === 0) return false;
+
+  //만약 5를 넘고, 2번째 값이 아니라면 5를 나눠서 진행
+  return check(Math.floor(num / 5));
+}
+
 function solution(n, l, r) {
-  let answer = 0;
-  let cantor = '1';
-  for (let i = 0; i < n; i++) {
-    const temp = [...cantor].reduce((acc, cur, idx) => {
-      if (cur === '0') {
-        acc[idx] = '00000';
-      }
-      if (cur === '1') {
-        acc[idx] = '11011';
-      }
-
-      return acc;
-    }, []);
-    // console.log('temp', temp);
-    cantor = temp.join('');
+  var answer = 0;
+  for (var i = l - 1; i < r; i++) {
+    if (check(i)) {
+      answer++;
+    }
   }
-
-  // console.log('cantor', [...cantor]);
-  answer = [...cantor].reduce((acc, cur, idx) => {
-    if (idx < l - 1 || idx > r) {
-      return acc;
-    }
-
-    if (cur === '1') {
-      return (acc += 1);
-    }
-
-    return acc;
-  }, 0);
-
   return answer;
 }
-console.log(solution(2, 4, 17)); //8
+console.log(solution(3, 4, 17)); //8
