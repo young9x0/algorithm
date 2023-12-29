04f1e3c395645fd66c5d95cc1641c12ca1c7054e
maliethy <maliethy@gmail.com>
2023-12-30 01:20:29 +0900

    하노이의 탑 해법 주석 수정

--- programmers/level2/12946.js
+++ programmers/level2/12946.js
@@ -1,6 +1,11 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12946
 //하노이의 탑
 //해설: https://brunch.co.kr/@younggiseo/139
+//youtube 해설: https://youtu.be/vq7dpFWpwAE?si=IfU2aYgzGb5zY8xP
+//1. [1,2], 1->2, 1번에 있는 [1, 2]를 2번 기둥으로 이동한다.
+//2. [3], 1->3, 1번에 있는 [3]를 3번 기둥으로 이동한다.
+//3. [1,2], 2->3, 2번에 있는 [1, 2]를 3번 기둥으로 이동한다.
+//
 function solution(n) {
   const answer = [];
   let count = 0;
@@ -14,14 +19,15 @@ function solution(n) {
       return answer.push([from, to]);
     } else {
       console.log('[move] last:', num, 'from', from, 'to', to, 'via', via);
-      //가장 아래 원반을 움직이기 위해, 위에 있는 원반이 가장 아래 원반이 이동하고자 하는 목적지를 제외한 한 곳으로 이동한다.
+      //1. 가장 아래 원반을 움직이기 위해, 나머지 원반들이 가장 아래 원반이 이동하고자 하는 목적지를 제외한 한 곳으로 이동한다.
       hanoi(num - 1, from, via, to);
 
       console.log('answer', answer);
       console.log('[move] from:', from, '-> to:', to, '[', from, ',', to, ']');
-      //가장 아래 원반을 원하는 목적지로 이동한다.
+      //2. 가장 아래 원반을 원하는 목적지로 이동한다.
       answer.push([from, to]);
 
+      //3. 나머지 원반들이 가장 아래 원반이 이동한 목적지로 이동한다.
       console.log('[move] remain disks last:', num, 'from', from, 'to', to, 'via', via);
       hanoi(num - 1, via, to, from);
     }
@@ -31,55 +37,25 @@ function solution(n) {
   return answer;
 }
 
-console.log(solution(3));
-
-//n=3
-// 1 hanoi disks: 3 from 1 to 3 via 2
-//   [move] last: 3 from 1 to 3 via 2
-// 2 hanoi disks: 2 from 1 to 2 via 3
-//   [move] last: 2 from 1 to 2 via 3
-// 3 hanoi disks: 1 from 1 to 3 via 2
-// only one
-// answer []
-//   [move] from: 1 -> to: 3 [ 1 , 3 ]
-// answer [ [ 1, 3 ] ]
-//   [move] from: 1 -> to: 2 [ 1 , 2 ]
-//   [move] remain disks last: 2 from 1 to 2 via 3
-// 4 hanoi disks: 1 from 3 to 2 via 1
-// only one
-// answer [ [ 1, 3 ], [ 1, 2 ] ]
-//   [move] from: 3 -> to: 2 [ 3 , 2 ]
-// answer [ [ 1, 3 ], [ 1, 2 ], [ 3, 2 ] ]
-//   [move] from: 1 -> to: 3 [ 1 , 3 ]
-//   [move] remain disks last: 3 from 1 to 3 via 2
-// 5 hanoi disks: 2 from 2 to 3 via 1
-//   [move] last: 2 from 2 to 3 via 1
-// 6 hanoi disks: 1 from 2 to 1 via 3
-// only one
-// answer [ [ 1, 3 ], [ 1, 2 ], [ 3, 2 ], [ 1, 3 ] ]
-//   [move] from: 2 -> to: 1 [ 2 , 1 ]
-// answer [ [ 1, 3 ], [ 1, 2 ], [ 3, 2 ], [ 1, 3 ], [ 2, 1 ] ]
-//   [move] from: 2 -> to: 3 [ 2 , 3 ]
-//   [move] remain disks last: 2 from 2 to 3 via 1
-// 7 hanoi disks: 1 from 1 to 3 via 2
-// only one
-// answer [ [ 1, 3 ], [ 1, 2 ], [ 3, 2 ], [ 1, 3 ], [ 2, 1 ], [ 2, 3 ] ]
-//   [move] from: 1 -> to: 3 [ 1 , 3 ]
-//   [
-//   [ 1, 3 ], [ 1, 2 ],
-//     [ 3, 2 ], [ 1, 3 ],
-//     [ 2, 1 ], [ 2, 3 ],
-//     [ 1, 3 ]
-//   ]
+console.log(solution(4));
 
 //n=4
 // [
-//   [ 1, 2 ], [ 1, 3 ],
-//   [ 2, 3 ], [ 1, 2 ],
-//   [ 3, 1 ], [ 3, 2 ],
-//   [ 1, 2 ], [ 1, 3 ],
-//   [ 2, 3 ], [ 2, 1 ],
-//   [ 3, 1 ], [ 2, 3 ],
-//   [ 1, 2 ], [ 1, 3 ],
-//   [ 2, 3 ]
+//1. 가장 아래 원반=4, 나머지 원반들=(1,2,3) 일 때 (1,2,3) 이동
+    //1. 가장 아래 원반=3, 나머지 원반들=(1,2) 일 때 (1,2) 이동
+    //1. (1) = [ 1, 2 ], (2) = [ 1, 3 ],  (1) = [ 2, 3 ], -> [(3,4),(),(1,2)]
+  //2. 가장 아래 원반=3 이동
+  //2. (3) = [ 1, 2 ],  -> [(4),(3),(1,2)]
+    //3. (1,2) 이동
+    //3. (1) = [ 3, 1 ], (2) = [ 3, 2 ], (1) = [ 1, 2 ], -> [(4),(1,2,3),()]
+
+//2. 가장 아래 원반=4 이동
+//2. (4) = [ 1, 3 ], -> [(),(1,2,3),(4)]
+
+    //1. 가장 아래 원반=3, 나머지 원반들=(1,2) 일 때 (1,2) 이동
+    //1. (1) = [ 2, 3 ], (2) = [ 2, 1 ], (1) = [ 3, 1 ], -> [(1,2),(3),(4)]
+  //2. 가장 아래 원반=3 이동
+  //2. (3) = [ 2, 3 ], -> [(1,2),(),(3,4)]
+    //3. (1,2) 이동
+    //3. (1) = [ 1, 2 ], (2) = [ 1, 3 ], (1) = [ 2, 3 ] -> [(),(),(1,2,3,4)]
 // ]
