259ab9e27cdabf8e7499dc3ae9d3694860336595
maliethy <maliethy@gmail.com>
2024-01-02 01:56:10 +0900

    3 x n 타일링2 0%/0%

--- programmers/level2/12902.js
+++ programmers/level2/12902.js
@@ -1,48 +1,26 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12902
 //3 x n 타일링
 function solution(n) {
-  let answer = 0;
-
   if (n < 2 || n % 2 === 1) {
     return 0
   }
 
-  function divideByFour(num, count) {
-    // console.log('num', num)
-    // console.log('count', count)
-
-    if (num < 4) {
-      return answer += Math.pow(3, n / 2)
-    }
-
-    // console.log('num', num)
-    count *= Math.pow(2, Math.floor(num / 4))
-    // console.log('count', count)
-    if (num % 4 === 2) {
-      answer += count * 3
-      // console.log('num % 4 === 2',)
-      // console.log('answer', answer)
-    } else {
-      answer += count
-      // console.log('num % 4 !== 2',)
-      // console.log('answer----', answer)
-    }
-
-    num -= 4
-    // console.log('num', num)
-    const divideIndex = Math.floor(((n - num) / 4))
-    divideByFour(num, Math.pow(9, divideIndex))
+  if (n === 2) {
+    return 3
   }
 
-  divideByFour(n, 1)
+  const cache = [3]
+  for (let i = 1; i < n / 2; i++) {
+    cache.push(2 + cache[cache.length - 1] * 3)
+  }
 
   // console.log('answer', answer)
-  return answer % 1_000_000_007
+  return cache[cache.length - 1] % 1_000_000_007
 }
 
 // console.log(solution(2))//3
 // console.log(solution(4))//11
-// console.log(solution(6))//33
-// console.log(solution(8))//103
-console.log(solution(10))//309
-// console.log(solution(12))//935
+// console.log(solution(6))//35
+// console.log(solution(8))//107
+// console.log(solution(10))//
+// console.log(solution(12))//
