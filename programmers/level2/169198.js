390d8f8d969c1c6ac803b3a6907215e90c8683f8
maliethy <maliethy@gmail.com>
2024-01-05 20:24:51 +0900

    당구 연습 해법

--- programmers/level2/169198.js
+++ programmers/level2/169198.js
@@ -1,22 +1,55 @@
 //169198
 //https://school.programmers.co.kr/learn/courses/30/lessons/169198
 //당구 연습
+//해법: https://wooserk.tistory.com/113
 function solution(m, n, startX, startY, balls) {
   let answer = [];
-  balls.forEach(([targetX, targetY]) => {
-    if (startX === targetX) {
-      answer.push(Math.pow(Math.abs(targetY - startY), 2) + Math.pow(
-          2 * Math.min(m - startX, startX), 2))
-    } else if (startY === targetY) {
-      answer.push(Math.pow(Math.abs(targetX - startX), 2) + Math.pow(
-          2 * Math.min(n - startY, startY), 2))
-    } else {
-      answer.push(
-          Math.pow(Math.abs(targetX - startX), 2) + Math.pow(
-              Math.abs(n - targetY) + Math.abs(n - startY),
-              2))
+  const transformStart = [
+    [startX, 2 * n - startY],
+    [startX, -startY],
+    [-startX, startY],
+    [2 * m - startX, startY]
+  ]
+  for (let i = 0; i < balls.length; i++) {
+    let min = Number.MAX_VALUE;// 1.7976931348623157e+308
+
+    for (let j = 0; j < transformStart.length; j++) {
+      const [transX, transY] = transformStart[j]
+      console.log('transX', transX)
+      console.log('balls[i][0]', balls[i][0])
+
+      if (transX === balls[i][0]) {
+        const maxY = Math.max(startY, transY)
+        const minY = Math.min(startY, transY)
+        console.log('startY', startY)
+        console.log('maxY', maxY)
+        console.log('minY', minY)
+        if (minY < balls[i][1] && balls[i][1] < maxY) {
+          continue
+        }
+      }
+
+      console.log('transY', transY)
+      console.log('balls[i][1]', balls[i][1])
+      if (transY === balls[i][1]) {
+        const maxX = Math.max(startX, transX)
+        const minX = Math.min(startX, transX)
+        console.log('startX', startX)
+        console.log('maxX', maxX)
+        console.log('minX', minX)
+        if (minX < balls[i][0] && balls[i][0] < maxX) {
+          continue
+        }
+      }
+
+      const temp = (transX - balls[i][0]) ** 2 + (transY - balls[i][1]) ** 2
+      console.log('temp', temp)
+      min = Math.min(min, temp)
+      console.log('min', min)
     }
-  })
+
+    answer.push(min)
+  }
   return answer;
 }
 
