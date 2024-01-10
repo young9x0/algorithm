c7fcda956359553885386c2c303c0ee640e1a179
maliethy <maliethy@gmail.com>
2024-01-11 00:49:24 +0900

    가장 큰 정사각형 찾기 정확성: 25.1 효율성: 0.0 합계: 25.1

--- programmers/level2/12905.js
+++ programmers/level2/12905.js
@@ -1,79 +1,69 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12905
 //가장 큰 정사각형 찾기
 function solution(board) {
-    let answer = board.length
-    const rStore = Array(board.length).fill(null)
-    const cStore = Array(board.length).fill(null)
+  let maxLen = 0
+  for (let row = 0; row < board.length; row++) {
+    let maxRowLen = 0, curRowLen = 0, sIdx = 0, maxSIdx = 0
 
-    for (let i = 0; i < board.length; i++) {
-        rStore[i] = Array(board.length).fill(0)
-        cStore[i] = Array(board.length).fill(0)
+    // console.log('1 ---row', row)
+    for (let col = 0; col < board[0].length; col++) {
+      // console.log('--col', col)
+      if (board[row][col] === 0) {
+        curRowLen = 0
+      }
+      if (curRowLen === 0 && board[row][col] === 1) {
+        curRowLen = 1
+        sIdx = col
+        // console.log('curRowLen start')
+      }
+      if (board[row][col] === 1 && board[row][col + 1] === 1) {
+        curRowLen += 1
+      }
+      // console.log('curRowLen', curRowLen)
+
+      if (maxRowLen < curRowLen) {
+        maxRowLen = curRowLen
+        maxSIdx = sIdx
+      }
     }
 
-    // console.log('rStore', rStore)
-    // console.log('cStore', cStore)
+// 한 row 검사가 끝났을 때
+//     console.log('----maxRowLen', maxRowLen)
+    if (maxRowLen === 0) {
+      continue
+    }
 
-    for (let i = 0; i < board.length; i++) {
-        let maxRowLen = 0, rIdx = 0
-
-        // console.log('i', i)
-        for (let p = 0; p + 1 < board.length; p++) {
-            // console.log('------check row',)
-            // console.log('p', p)
-            if (board[i][p] === 0) {
-                maxRowLen = 0
-            }
-            if (maxRowLen === 0 && board[i][p] === 1) {
-                maxRowLen = 1
-                rIdx = p
-                // console.log('rIdx', rIdx)
-            }
-            if (board[i][p] === 1 && board[i][p + 1] === 1) {
-                maxRowLen += 1
-            }
-            // console.log('maxRowLen', maxRowLen)
-            rStore[i][rIdx] = maxRowLen
-            // console.log('rStore', rStore)
+    let maxColLen = 1
+    for (let row2 = row + 1; row2 < board.length; row2++) {
+      for (let col = maxSIdx; col < maxSIdx + maxRowLen; col++) {
+        // console.log('maxSIdx', maxSIdx)
+        // console.log('row2', row2)
+        // console.log('col', col)
+        // console.log('board[row2][col]', board[row2][col])
+        if (board[row2][col] === 0) {
+          row2 = maxRowLen
+          break
         }
-    }
 
-    for (let i = 0; i < board.length; i++) {
-        // console.log('i', i)
-        let maxRowCol = 0, cIdx = 0
+        if (col === maxSIdx + maxRowLen - 1) {
 
-        for (let p = 0; p + 1 < board.length; p++) {
-            // console.log('-----check col',)
-            // console.log('p', p)
-            if (board[p][i] === 0) {
-                maxRowCol = 0
-            }
-            if (maxRowCol === 0 && board[p][i] === 1) {
-                maxRowCol = 1
-                cIdx = p
-                // console.log('cIdx', cIdx)
-            }
-            if (board[p][i] === 1 && board[p + 1][i] === 1) {
-                maxRowCol += 1
-            }
-            // console.log('maxRowCol', maxRowCol)
-            cStore[cIdx][i] = Math.max(cStore[cIdx][i], maxRowCol)
-            // console.log('cStore', cStore)
+          maxColLen += 1
+          // console.log('----break')
+          break
         }
+
+      }
     }
+    // console.log('maxColLen', maxColLen)
+    const temp = Math.min(maxRowLen, maxColLen)
+    // console.log('temp', temp)
+    maxLen = Math.max(maxLen, temp)
+    // console.log('maxLen', maxLen)
+  }
 
-    for (let i = 0; i < board.length; i++) {
-        for (let p = 0; p + 1 < board.length; p++) {
-            if (rStore[i][p] > 0 && rStore[i][p] === cStore[i][p]) {
-                answer = Math.min(answer, rStore[i][p])
-            }
-
-            // console.log('answer', answer)
-        }
-    }
-
-
-    return answer ** 2
+  return maxLen ** 2
 }
 
-// console.log(solution([[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]))//9
-console.log(solution([[0, 0, 1, 1], [1, 1, 1, 1]]))//4
+console.log(solution([[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]))//9
+// console.log(solution([[1, 1, 0, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 1]]))//4
+// console.log(solution([[0, 0, 1, 1], [1, 1, 1, 1]]))//4
