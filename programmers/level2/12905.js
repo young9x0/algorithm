333d8f83deefc071bdf2f8b837499efff5b0b044
maliethy <maliethy@gmail.com>
2024-01-21 16:32:37 +0900

    가장 큰 정사각형 찾기 해답

--- programmers/level2/12905.js
+++ programmers/level2/12905.js
@@ -1,67 +1,29 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/12905
 //가장 큰 정사각형 찾기
+//해답: https://velog.io/@proshy/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4JS-%EA%B0%80%EC%9E%A5-%ED%81%B0-%EC%A0%95%EC%82%AC%EA%B0%81%ED%98%95-%EC%B0%BE%EA%B8%B0
 function solution(board) {
-  let maxLen = 0
-  for (let row = 0; row < board.length; row++) {
-    let maxRowLen = 0, curRowLen = 0, sIdx = 0, maxSIdx = 0
+  let answer = 0
+  const row = board.length
+  const column = board[0].length
 
-    // console.log('1 ---row', row)
-    for (let col = 0; col < board[0].length; col++) {
-      // console.log('--col', col)
-      if (board[row][col] === 0) {
-        curRowLen = 0
-      }
-      if (curRowLen === 0 && board[row][col] === 1) {
-        curRowLen = 1
-        sIdx = col
-        // console.log('curRowLen start')
-      }
-      if (board[row][col] === 1 && board[row][col + 1] === 1) {
-        curRowLen += 1
-      }
-      // console.log('curRowLen', curRowLen)
-
-      if (maxRowLen < curRowLen) {
-        maxRowLen = curRowLen
-        maxSIdx = sIdx
-      }
-    }
-
-// 한 row 검사가 끝났을 때
-//     console.log('----maxRowLen', maxRowLen)
-    if (maxRowLen === 0) {
-      continue
-    }
-
-    let maxColLen = 1
-    for (let row2 = row + 1; row2 < board.length; row2++) {
-      for (let col = maxSIdx; col < maxSIdx + maxRowLen; col++) {
-        // console.log('maxSIdx', maxSIdx)
-        // console.log('row2', row2)
-        // console.log('col', col)
-        // console.log('board[row2][col]', board[row2][col])
-        if (board[row2][col] === 0) {
-          row2 = maxRowLen
-          break
-        }
-
-        if (col === maxSIdx + maxRowLen - 1) {
-
-          maxColLen += 1
-          // console.log('----break')
-          break
-        }
-
-      }
-    }
-    // console.log('maxColLen', maxColLen)
-    const temp = Math.min(maxRowLen, maxColLen)
-    // console.log('temp', temp)
-    maxLen = Math.max(maxLen, temp)
-    // console.log('maxLen', maxLen)
+  if (row <= 1 || column <= 1) {
+    return 1
   }
 
-  return maxLen ** 2
+  for (let i = 1; i < row; i++) {
+    for (let j = 1; j < column; j++) {
+      if (board[i][j] > 0) {
+        const up = board[i - 1][j]
+        const left = board[i][j - 1]
+        const cross = board[i - 1][j - 1]
+        const minNum = Math.min(up, left, cross)
+        board[i][j] = minNum + 1
+        answer = Math.max(answer, board[i][j])
+      }
+    }
+  }
+
+  return answer * answer
 }
 
 console.log(solution([[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]))//9
