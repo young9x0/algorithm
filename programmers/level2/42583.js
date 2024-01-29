f578b1930389105ca7fe31a644ff59a90b41c185
maliethy <maliethy@gmail.com>
2024-01-29 21:21:34 +0900

    다리를 지나는 트럭 해답

--- programmers/level2/42583.js
+++ programmers/level2/42583.js
@@ -1,60 +1,37 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42583
 // 다리를 지나는 트럭
+// 해답:https://velog.io/@chloeelog/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%8B%A4%EB%A6%AC%EB%A5%BC-%EC%A7%80%EB%82%98%EB%8A%94-%ED%8A%B8%EB%9F%AD-JavaScript
+
 
 function solution(bridge_length, weight, truck_weights) {
   let time = 0;
+  let bridge = Array(bridge_length).fill(0);
+  let bridge_sum = 0;
 
-  let curCapa = bridge_length;
-  const waiting = [...truck_weights];
-  let current = [];
+  time++;
+  bridge.shift();
+  bridge_sum += truck_weights[0];
+  bridge.push(truck_weights.shift());
+  console.log('time', time);
+  console.log('bridge', bridge);
+  console.log('bridge_sum', bridge_sum);
+  while (bridge_sum > 0) {
+    time++;
+    bridge_sum -= bridge.shift(); //[6]
+    console.log('time', time);
+    console.log('bridge', bridge);
+    console.log('bridge_sum', bridge_sum);
 
-  function pushCurrent() {
-    const shifted = waiting.shift();
-    // console.log('waiting', waiting);
-    current.push(shifted);
-    // console.log('current', current);
-    curCapa--;
-    // console.log('curCapa', curCapa);
+    if (truck_weights.length > 0 && bridge_sum + truck_weights[0] <= weight) {
+      bridge_sum += truck_weights[0];
+      bridge.push(truck_weights.shift()); //[0,6]
+      console.log('bridge_sum', bridge_sum);
+    } else {
+      bridge.push(0); //[6,0]
+    }
+    console.log('bridge', bridge);
   }
 
-  for (let i = 0; i < truck_weights.length; i++) {
-    // console.log('i', i);
-    // console.log('-current', current);
-    // console.log('-waiting', waiting);
-    //다리에 트럭이 없을 때
-    if (current.length === 0 && waiting.length > 0) {
-      pushCurrent();
-      time++;
-    }
-    // console.log('time', time);
-
-    while (current.length > 0) {
-      // console.log('while current', current);
-      const totalWeight = current.reduce((acc, cur) => acc + cur, 0);
-      // console.log('totalWeight', totalWeight);
-
-      if (waiting.length > 0 && totalWeight + waiting[0] <= weight) {
-        pushCurrent(waiting[0]);
-        time++;
-        // console.log('add waiting', time);
-      } else {
-        // console.log('over weight/no waiting curCapa', curCapa);
-        time += curCapa;
-
-        // console.log('over weight/no waiting time', time);
-        time+= current.length;
-        current = [];
-        // console.log('reset current', time);
-
-        if (waiting.length > 0 && current.length === 0) {
-          // console.log('waiting exist');
-          curCapa = bridge_length;
-          // console.log('reset  curCapa');
-          pushCurrent();
-        }
-      }
-    }
-  }
   return time;
 }
 
