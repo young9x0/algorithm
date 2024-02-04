816c955ab6361ac6f730cfd68950b427ed1f3ad2
maliethy <maliethy@gmail.com>
2024-02-04 23:53:14 +0900

    구명보트 해답

--- programmers/level2/42885.js
+++ programmers/level2/42885.js
@@ -1,36 +1,22 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42885
 //구명보트
+//해답: https://velog.io/@eldoradodo/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-JavaScript-%EA%B5%AC%EB%AA%85%EB%B3%B4%ED%8A%B8
 
 function solution(people, limit) {
   let answer = 0;
-
-  const temp = [...people];
-  temp.sort((a,b)=> b-a)
-  let weight = temp.shift();
-  answer++;
-  let idx = 0;
-  while (temp.length > 0) {
-    // console.log('temp', temp);
-    if (idx === temp.length) {
-      answer++;
-      idx = 0;
-      weight = temp.shift();
-      // console.log('----');
+  people.sort((a, b) => b - a);
+  for (let i = 0, j = people.length - 1; i <= j; i++) {
+    console.log('i', i);
+    console.log('j', j);
+    console.log('p i', people[i]);
+    console.log('p j', people[j]);
+    if (people[i] + people[j] <= limit) {
+      j--;
     }
-    // console.log('weight', weight);
-    if (weight + temp[idx] <= limit) {
-      const [el] = temp.splice(idx, 1);
-      // console.log('el', el);
-      weight += el;
-      // console.log('add', weight);
-    } else {
-      idx++;
-      // console.log('next', idx);
-    }
+    answer++;
   }
-
   return answer;
 }
 
-console.log(solution([70, 50, 80, 50], 100)); //3
-console.log(solution([70, 80, 50], 100)); //3
+// console.log(solution([70, 50, 80, 50], 100)); //3
+console.log(solution([70, 80, 90], 100)); //3
