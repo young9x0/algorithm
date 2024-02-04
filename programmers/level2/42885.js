726e2a0f99a5c51fd2b603f81ee75563233fcee4
maliethy <maliethy@gmail.com>
2024-02-04 23:37:57 +0900

    구명보트 정확성: 14.8 효율성: 0.0

---
+++ programmers/level2/42885.js
@@ -0,0 +1,36 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42885
+//구명보트
+
+function solution(people, limit) {
+  let answer = 0;
+
+  const temp = [...people];
+
+  let weight = temp.shift();
+  answer++;
+  let idx = 0;
+  while (temp.length > 0) {
+    // console.log('temp', temp);
+    if (idx === temp.length) {
+      answer++;
+      idx = 0;
+      weight = temp.shift();
+      // console.log('----');
+    }
+    // console.log('weight', weight);
+    if (weight + temp[idx] <= limit) {
+      const [el] = temp.splice(idx, 1);
+      // console.log('el', el);
+      weight += el;
+      // console.log('add', weight);
+    } else {
+      idx++;
+      // console.log('next', idx);
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([70, 50, 80, 50], 100)); //3
+console.log(solution([70, 80, 50], 100)); //3
