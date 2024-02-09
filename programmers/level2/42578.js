aa7fbbc754bd7321e8da8e4c32b2289a7fd38654
maliethy <maliethy@gmail.com>
2024-02-09 17:14:39 +0900

    의상 정확성 해답

--- programmers/level2/42578.js
+++ programmers/level2/42578.js
@@ -1,25 +1,26 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42578
 //의상
+//해답: https://supersfel.tistory.com/515
 
 function solution(clothes) {
-  let answer = 0;
-
-  const clean = clothes.reduce((acc, cur) => {
-    if (!acc[cur[1]]) {
-      acc[cur[1]] = 1;
-      return acc;
+  const map = new Map();
+  for (const [name, type] of clothes) {
+    const tCount = map.get(type);
+    if (tCount) {
+      map.set(type, tCount + 1);
+    } else {
+      map.set(type, 1);
     }
-    acc[cur[1]]++;
-    return acc;
-  }, {});
-
-  const cValue = Object.values(clean);
-  answer = cValue.reduce((acc, cur) => acc * cur, 1);
-  if (cValue.length > 1) {
-    answer += cValue.reduce((acc, cur) => acc + cur, 0);
   }
 
-  return answer;
+  let answer = 1;
+  for (const [key, val] of map) {
+    //안입는 경우도 옵션에 더해주기
+    answer *= val + 1;
+    // console.log('answer', answer);
+  }
+  //한벌도 안입는 경우 빼주기
+  return answer - 1;
 }
 
 console.log(
@@ -29,10 +30,20 @@ console.log(
     ['green_turban', 'headgear'],
   ]),
 ); //5
-console.log(
-  solution([
-    ['crow_mask', 'face'],
-    ['blue_sunglasses', 'face'],
-    ['smoky_makeup', 'face'],
-  ]),
-); //3
+// console.log(
+//   solution([
+//     ['crow_mask', 'face'],
+//     ['blue_sunglasses', 'face'],
+//     ['smoky_makeup', 'face'],
+//   ]),
+// ); //3
+// console.log(
+//     solution([
+//       ['crow_mask', 'face'],
+//       ['blue_sunglasses', 'face'],
+//       ['smoky_makeup', 'face'],
+//       ['yellow_hat', 'headgear'],
+//       ['blue_sunglasses', 'eyewear'],
+//       ['green_turban', 'headgear'],
+//     ]),
+// ); //3
