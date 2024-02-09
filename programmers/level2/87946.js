f68c0dfbee763404d6198991ca661806ef430b4c
maliethy <maliethy@gmail.com>
2024-02-09 13:53:27 +0900

    피로도 정확성 100%

--- programmers/level2/87946.js
+++ programmers/level2/87946.js
@@ -1,30 +1,41 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/87946
 //피로도
 
-
 function solution(k, dungeons) {
   let answer = -1;
-  const origin = [1, 2, 3, 4,5];
 
   function recursive(result, remain) {
     // console.log('result', result);
     // console.log('remain', remain);
 
-    if (result.length === origin.length ) {
-      console.log('----result', result);
+    if (result.length === dungeons.length) {
+      // console.log('----result', result);
+      let power = k;
+      const count = result.reduce((acc, cur) => {
+        if (power >= cur[0]) {
+          power -= cur[1];
+          acc++;
+        }
+        // console.log('acc', acc);
+        return acc;
+      }, 0);
+
+      answer = Math.max(answer, count);
       return;
     }
 
     for (let i = 0; i < remain.length; i++) {
-      const newResult = [...result, remain[i]]
+      const rCopy = Array.from(result, ([a, b]) => [a, b]);
+      const newResult = [...rCopy, [remain[i][0], remain[i][1]]];
       const rest = remain.filter((_, idx) => idx !== i);
       recursive(newResult, [...rest]);
     }
   }
 
-  for (let i = 0; i < origin.length; i++) {
-    const temp = [origin[i]];
-    const rest = origin.filter((_, idx) => idx !== i);
+  for (let i = 0; i < dungeons.length; i++) {
+    const dCopy = Array.from(dungeons, ([a, b]) => [a, b]);
+    const temp = [[dCopy[i][0], dCopy[i][1]]];
+    const rest = dCopy.filter((_, idx) => idx !== i);
     recursive(temp, [...rest]);
   }
 
