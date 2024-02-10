a97f9e9e3a4688fc2dfaa8e7c782424545bc5788
maliethy <maliethy@gmail.com>
2024-02-10 20:53:52 +0900

    귤 고르기 해답

--- programmers/level2/138476.js
+++ programmers/level2/138476.js
@@ -1,9 +1,7 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/138476
 //귤 고르기
-
+//해답: https://velog.io/@sisofiy626/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EA%B7%A4-%EA%B3%A0%EB%A5%B4%EA%B8%B0-JavaScript
 function solution(k, tangerine) {
-  let answer = Number.MAX_SAFE_INTEGER;
-
   const tChecked = tangerine.reduce((acc, cur) => {
     if (!acc[cur]) {
       acc[cur] = 1;
@@ -12,45 +10,22 @@ function solution(k, tangerine) {
     }
     return acc;
   }, {});
-  // console.log('tChecked', tChecked);
-  const tValue = Object.values(tChecked);
-  const visited = Array(tValue.length).fill(0);
 
-  function dfs(count) {
-    // console.log('dfs-------');
-    // console.log('count', count);
+  const tValue = Object.values(tChecked).sort((a, b) => b - a);
+  // console.log('tValue', tValue);
 
-    if (count >= k) {
-      // console.log('visited', visited);
-      const sum = visited.reduce((acc, cur) => acc + cur, 0);
-      // console.log('sum', sum);
-      // console.log('==answer', answer);
-
-      return (answer = Math.min(answer, sum));
+  let count = 0;
+  for (const value of tValue) {
+    // console.log('count', count)
+    if (k <= 0) {
+      return count;
     }
 
-    tValue.forEach((num, idx) => {
-      let cnt = count
-      // console.log('idx', idx);
-      // console.log('num', num);
-
-      if(num >= k){
-        return answer =1
-      }
-
-      if (cnt + num <= k && !visited[idx]) {
-        cnt += num;
-        visited[idx] = 1;
-        dfs(cnt);
-        visited[idx] = 0;
-      }
-    });
+    count++;
+    k -= value;
   }
-
-  dfs(0);
-  return answer;
 }
 
-// console.log(solution(6, [1, 3, 2, 5, 4, 5, 2, 3])); //3
+console.log(solution(6, [1, 3, 2, 5, 4, 5, 2, 3])); //3
 // console.log(solution(4, [1, 3, 2, 5, 4, 5, 2, 3])); //2
-console.log(solution(2, [1, 1, 1, 1, 2, 2, 2, 3])); //1
+// console.log(solution(2, [1, 1, 1, 1, 2, 2, 2, 3])); //1
