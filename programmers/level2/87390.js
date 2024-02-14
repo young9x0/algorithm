7adfa6c9bd23cd3869d7144d853516fb976b1b41
maliethy <maliethy@gmail.com>
2024-02-14 23:19:03 +0900

    n^2 배열 자르기 100%

--- programmers/level2/87390.js
+++ programmers/level2/87390.js
@@ -4,20 +4,22 @@
 function solution(n, left, right) {
   const answer = [];
 
-  for (let i = 0; i < n; i++) {
-    // console.log('i+1',i+1)
-    for (let j = 1; j <= n; j++) {
-      // console.log('j',j)
-      if (j < i + 1) {
-        answer.push(i + 1);
-      } else {
-        answer.push(j);
-      }
+  for (let i = left; i <= right; i++) {
+    // console.log('i', i);
+    const rowIdx = Math.floor(i / n) + 1;
+    const colIdx = (i % n) + 1;
+    // console.log('rowIdx', rowIdx);
+    // console.log('colIdx', colIdx);
+
+    if (colIdx < rowIdx) {
+      answer.push(rowIdx);
+    } else {
+      answer.push(colIdx);
     }
   }
-
-  return answer.slice(left, right + 1);
+  return answer;
 }
 
 // console.log(solution(3, 2, 5)); //[3,2,2,3]
-console.log(solution(4, 7, 14)); //[4,3,3,3,4,4,4,4]
+// console.log(solution(4, 7, 14)); //[4,3,3,3,4,4,4,4]
+console.log(solution(10 ** 7, 7, 14));
