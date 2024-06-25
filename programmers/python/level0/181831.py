7e41044ac89377fdd5bb0ccac5216d2e12a07e38
veganee <maliethy@gmail.com>
2024-06-25 13:44:04 +0900

    특별한 이차원 배열2 다른 풀이

--- programmers/python/level0/181831.py
+++ programmers/python/level0/181831.py
@@ -1,13 +1,7 @@
 def solution(arr):
-  answer = 1
+  return int(arr == list(map(list, zip(*arr))))
 
-  for i in range(len(arr)):
-    for j in range(len(arr)):
-      if arr[i][j] != arr[j][i]:
-        answer=0
-  return answer
-
-# print(solution([[5, 192, 33], [192, 72, 95], [33, 95, 999]]))
+print(solution([[5, 192, 33], [192, 72, 95], [33, 95, 999]]))
 # 1
 print(solution([[19, 498, 258, 587], [63, 93, 7, 754], [258, 7, 1000, 723], [587, 754, 723, 81]]	))
 # 0
