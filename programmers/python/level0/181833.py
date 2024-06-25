eb9d6693724df7428deb92ac592c05f17a2d750f
veganee <maliethy@gmail.com>
2024-06-25 11:32:57 +0900

    특별한 이차원 배열1 다른 풀이

--- programmers/python/level0/181833.py
+++ programmers/python/level0/181833.py
@@ -1,13 +1,13 @@
-def solution( n):
-  answer = []
-  for i in range(n):
-    answer.append([])
-    for j in range(n):
-      if i == j:
-        answer[i].append(1)
-      else:
-        answer[i].append(0)
-  return answer
+# def solution( n):
+#   answer=[[0]*n for i in range(n)]
+#   for i in range(n):
+#     answer[i][i]=1
+#   return answer
 
-# print(solution(3))
-print(solution(6))
+
+def solution(n):
+  # print(int(False))
+  return [[int(i==j) for i in range(n)] for j in range(n)]
+
+print(solution(3))
+# print(solution(6))
