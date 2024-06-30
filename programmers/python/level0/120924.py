f542cd4ac658ebcbcb4b6fad3f47077445f27e81
veganee <maliethy@gmail.com>
2024-06-30 16:59:45 +0900

    다음에 올 숫자 정확성: 100

--- programmers/python/level0/120924.py
+++ programmers/python/level0/120924.py
@@ -1,19 +1,19 @@
-def solution(array):
-  [fir, sec, thr, *rest] = array
-  last = array[len(array)-1]
+def solution(common):
+  [fir, sec, thr, *rest] = common
+  last = common[len(common)-1]
   minus = sec - fir
 
   if sec + minus == thr:
     return last + minus
   else:
-    return last * minus
+    return last * (sec/fir)
 
 
 
 
 
 
-# print(solution([1, 2, 3, 4]))
-print(solution([2, 4, 8]	))
+print(solution([1, 2, 3, 4]))
+# print(solution([2, 4, 8]	))
 # [1, 2, 3, 4]	5
 # [2, 4, 8]	16
