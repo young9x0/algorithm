71b4bea1b5bbbaf5ded0cedfe12d01fd58574110
veganee <maliethy@gmail.com>
2024-07-01 19:18:45 +0900

    분수의 덧셈 정확성: 20

--- programmers/python/level0/120808.py
+++ programmers/python/level0/120808.py
@@ -1,14 +1,24 @@
 def solution(numer1, denom1, numer2, denom2):
-  numer = 0
-  denom=0
-  if denom2 % denom1 == 0:
-    denom = denom2
-    numer = numer1 * (denom2//denom1) + numer2
-  else:
-    denom = denom2 * denom1
-    numer = denom2 * numer1 + denom1 * numer2
+  lcm = findLCM(denom2, denom1)
 
-  return [numer, denom]
+  if lcm == 1:
+    return [numer1*denom2 + numer2*denom1, denom1*denom2]
+  else:
+    multi1 = denom1 // lcm
+    multi2 = denom2 // lcm
+    return [numer1 * multi2+numer2*multi1, multi2 * multi1 * lcm ]
+
+
+
+
+# least common multiple
+def findLCM(a,b):
+  if a % b == 0:
+    return b
+  elif a % b == 1:
+    return 1
+  else:
+    findLCM(b, a%b )
 
 print(solution(1,	2,	3,	4))
 print(solution(9,	2,	1,	3))
