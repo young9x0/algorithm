dc4009854911bff832b9a8f23d373327fa6cc793
veganee <maliethy@gmail.com>
2024-07-03 11:15:43 +0900

    옹알이(1) 다른 풀이

--- programmers/python/level0/120956.py
+++ programmers/python/level0/120956.py
@@ -1,22 +1,18 @@
 import re
 def solution(babbling):
-  possible = [ "aya", "ye", "woo", "ma"]
+  reg = re.compile('^(aya|ye|woo|ma)+$')
+
   answer = 0
   for sound in babbling:
-    # print('sound', sound)
-    for pos in possible:
-      sound =sound.replace(pos,'0')
-    # print('* sound', sound)
-    reg = re.compile('[a-z]')
-    x = reg.search(sound)
-    # print('x', x == None)
-    if x == None:
+    print('reg sound',sound, reg.match(sound))
+    if reg.match(sound):
       answer += 1
 
   return answer
 
-print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
+# print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
 # print(solution(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]))
+print(solution(["yemama"]))
 
 # ["aya", "yee", "u", "maa", "wyeoo"]	1
 # ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]	3
