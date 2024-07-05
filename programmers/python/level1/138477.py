7ce5ddbc46e671152c112dbabb744751e5531218
veganee <maliethy@gmail.com>
2024-07-05 10:00:53 +0900

    명예의 전당(1) 다른 풀이

--- programmers/python/level1/138477.py
+++ programmers/python/level1/138477.py
@@ -3,13 +3,19 @@ def solution(k, score):
   temp = []
   for s in score:
     temp.append(s)
-    temp.sort()
-    # print('temp', temp)
-    answer.append(temp[-k:][0])
+    print('s',s)
+    if (len(temp) > k):
+      print('temp', temp)
+
+      print('min(temp)',  min(temp))
+      temp.remove( min(temp))
+    print('min(temp)',  min(temp))
+    answer.append( min(temp))
+
   return answer
 
-# print(solution(3,	[10, 100, 20, 150, 1, 100, 200]))
-print(solution( 4,	[0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]	))
+print(solution(3,	[10, 100, 20, 150, 1, 100, 200]))
+# print(solution( 4,	[0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]	))
 
 # 3	[10, 100, 20, 150, 1, 100, 200]	[10, 10, 10, 20, 20, 100, 100]
 # 4	[0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]	[0, 0, 0, 0, 20, 40, 70, 70, 150, 300]
