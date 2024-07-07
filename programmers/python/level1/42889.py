745cb684d219c0330cd4d6178fca3c0240b28510
veganee <maliethy@gmail.com>
2024-07-07 16:39:52 +0900

    실패율 정확성: 70.4

--- programmers/python/level1/42889.py
+++ programmers/python/level1/42889.py
@@ -1,22 +1,34 @@
 def solution(N, stages):
+
   userLen = len(stages)
   dict = {}
   i = 1
+
   while i <= N:
-   dict[i] = 0
+   dict[i] =[ 0, userLen]
    i+=1
 
-  for stage in stages:
+
+  for  stage in (stages):
     if stage == N+1:
       continue
-    else:
-      dict[stage] += 1
 
-  k = userLen
+    else:
+      dict[stage][0] += 1
+
+
   for key, value in dict.items():
-    dict[key] = value * 1/k
-    k -= value
+    if key == 1:
+      continue
+    else:
+      dict[key][1] = dict[key-1][1] - dict[key-1][0]
+
   # print(dict)
+
+
+  for key, value in dict.items():
+    dict[key] = (value[0]/ value[1])
+
   result = sorted(dict.items(), key= lambda x: x[1], reverse=True)
   # print(result)
   answer = []
@@ -25,7 +37,7 @@ def solution(N, stages):
 
   return answer
 
-# print(solution(5,	[2, 1, 2, 6, 2, 4, 3, 3]))
-print(solution( 4,	[4,4,4,4,4]))
+print(solution(5,	[2, 1, 2, 6, 2, 4, 3, 3]))
+# print(solution( 4,	[4,4,4,4,4]))
 # 5	[2, 1, 2, 6, 2, 4, 3, 3]	[3,4,2,1,5]
 # 4	[4,4,4,4,4]	[4,1,2,3]
