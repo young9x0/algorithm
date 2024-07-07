7311dbeca92b7ddb79953bf3bb15d092c92d6cdf
veganee <maliethy@gmail.com>
2024-07-07 17:00:37 +0900

    실패율 다른 풀이2

--- programmers/python/level1/42889.py
+++ programmers/python/level1/42889.py
@@ -1,16 +1,28 @@
+from fractions import Fraction
 def solution(N, stages):
-  result = {}
-  userLen = len(stages)
-  for stage in range(1,N+1):
-    if userLen !=0:
-      cnt = stages.count(stage)
-      result[stage] = cnt/userLen
-      userLen -= cnt
-    else:
-      result[stage] = 0
-  return sorted(result, key= lambda x: result[x], reverse=True)
+  answer = []
+  fail = []
+  info = [0] * (N+2)
 
-print(solution(5,	[2, 1, 2, 6, 2, 4, 3, 3]))
-# print(solution( 4,	[4,4,4,4,4]))
+  for stage in stages:
+    info[stage] += 1
+  # print(info)
+
+  for i in range(N):
+    be = sum(info[(i+1):])
+    yet = info[i+1]
+    if be == 0:
+      fail.append(((i+1), 0))
+    else:
+      fail.append(((i+1), Fraction(yet, be)))
+  # print(fail)
+
+  for item in sorted(fail, key=lambda x: x[1], reverse=True):
+    answer.append(item[0])
+
+  return answer
+
+# print(solution(5,	[2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution( 4,	[4,4,4,4,4]))
 # 5	[2, 1, 2, 6, 2, 4, 3, 3]	[3,4,2,1,5]
 # 4	[4,4,4,4,4]	[4,1,2,3]
