71d1a4e725c572680d0b1fdacb9c70b9c92d18c4
veganee <maliethy@gmail.com>
2024-07-08 13:02:40 +0900

    소수 만들기 다른 풀이2

--- programmers/python/level1/12977.py
+++ programmers/python/level1/12977.py
@@ -1,18 +1,18 @@
 from itertools import combinations
-def prime_number(num):
-  answer = 0
-  for i in range(1, int(num**0.5)+1):
-    print('num', num)
-    print('i', i)
-    if num%i == 0:
-      answer += 1
-  print('answer', answer)
-  return 1 if answer == 1 else 0
-
-
 def solution(nums):
-  return sum([prime_number(sum(sNum)) for sNum in combinations(nums, 3)])
+  answer = 0
 
+  for comb in combinations(nums, 3):
+    cSum = sum(comb)
+    for divide in range(2, cSum):
+      print(cSum, divide)
+      if cSum % divide == 0:
+        break
+
+    else:
+      print('hello')
+      answer += 1
+  return answer
 
 
 
