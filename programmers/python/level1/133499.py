c5c35a6c9886c96312877403d460f2557dfcb024
veganee <maliethy@gmail.com>
2024-07-09 11:49:51 +0900

    옹알이(2) 다른 풀이2

--- programmers/python/level1/133499.py
+++ programmers/python/level1/133499.py
@@ -1,17 +1,19 @@
 def solution(babbling):
+  baby_say = ["aya", "ye", "woo", "ma",
+              "00","11","22","33"]
   answer = 0
-  for ba in babbling:
-    for wo in ['aya','ye','woo','ma']:
-      if wo*2 not in ba:
-        ba = ba.replace(wo,' ')
-    if len(ba.strip()) == 0:
-      answer += 1
-
-
+  for bab in babbling:
+    print('1 bab',bab)
+    for i in range(4):
+      bab = bab.replace(baby_say[i], str(i))
+      print('2 bab',bab)
+    for i in range(4,8):
+      bab = bab.replace(baby_say[i], "a")
+      print('3 bab',bab)
+    if bab.isdigit(): answer += 1
   return answer
 
-print(solution(["aywa", "yemae", "aauy", "mawaoo"]))
-print(solution(["aya", "yee", "u", "maa"]))
+# print(solution(["aya", "yee", "u", "maa"]))
 print(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]))
 # ["aya", "yee", "u", "maa"]	1
 # ["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]	2
