7d147c3e9ff529de9959f4eb32d93818877e0521
veganee <maliethy@gmail.com>
2024-07-10 11:26:47 +0900

    덧칠하기 정확성: 80.0

--- programmers/python/level1/161989.py
+++ programmers/python/level1/161989.py
@@ -1,30 +1,32 @@
 def solution(n, m, section):
-  temp = [0]*(n+1)
+  dict = {}
   for sec in section:
-    temp[sec] = 1
+    dict[sec] = 0
 
-  answer = 0
-  while sum(temp) > 0:
-    for idx in range(section[0], section[-1]+1):
-      flag = False
-      for lIdx in range(idx, min(idx+m, n+1)):
-        # print('temp[lIdx]', temp[lIdx])
-        if temp[lIdx] == 1:
-          temp[lIdx] = 0
-          flag = True
-
-      # print('flag', flag)
-      # print('temp', temp)
-      if flag:
+  answer = 1
+  idx = section[0] -1
+  for key, val in dict.items():
+    # print('key, val', key, val)
+    if  val == 1:
+      continue
+    else:
+      # print('idx', idx)
+      # print('idx + m', idx + m)
+      if idx + m >= key:
+        if dict[key] == 0:
+          dict[key] = 1
+      else:
+        # print('next')
         answer+=1
+        idx += m
 
   return answer
 
 
 
-print(solution(8,	4,	[2, 3, 6]))
+# print(solution(8,	4,	[2, 3, 6]))
 # print(solution( 5,	4,	[1, 3]))
-# print(solution( 4,	1,	[1, 2, 3, 4]))
+print(solution( 4,	1,	[1, 2, 3, 4]))
 # 8	4	[2, 3, 6]	2
 # 5	4	[1, 3]	1
 # 4	1	[1, 2, 3, 4]	4
