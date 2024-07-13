3b0322102f431e6e725a3cc7e39d58ac124caa46
veganee <maliethy@gmail.com>
2024-07-13 12:56:39 +0900

    과일 장수 다른 풀이

--- programmers/python/level1/135808.py
+++ programmers/python/level1/135808.py
@@ -1,15 +1,7 @@
 def solution(k, m, score):
-  score.sort(reverse=True)
-  # print('score',score)
+  return sum(sorted(score)[len(score)%m::m])*m
 
-  answer = 0
-  for bLen in range(len(score)//m):
-   idx = bLen*m
-   # print(bLen,score[idx:idx+m], min(score[idx:idx+m]))
-   answer += min(score[idx:idx+m]) * m
-  return answer
-
-# print(solution(3,	4,	[1, 2, 3, 1, 2, 3, 1]))
+print(solution(3,	4,	[1, 2, 3, 1, 2, 3, 1]))
 print(solution(4,	3,	[4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]))
 # 3	4	[1, 2, 3, 1, 2, 3, 1]	8
 # 4	3	[4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]	33
