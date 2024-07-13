a76194c5ae504845ccc37b703b8d3337cf76d269
veganee <maliethy@gmail.com>
2024-07-13 13:54:27 +0900

    로또의 최고 순위와 최저 순위 다른 풀이

--- programmers/python/level1/77484.py
+++ programmers/python/level1/77484.py
@@ -1,33 +1,12 @@
 def solution(lottos, win_nums):
-  zLen = 0
-  for num in lottos:
-    if num == 0:
-      zLen += 1
+  rank = [6,6,5,4,3,2,1]
+  cnt_0 = lottos.count(0)
+  answer = 0
+  for num in win_nums:
+    if num in lottos:
+      answer += 1
 
-  lottos.sort()
-  # print(zLen)
-  lottos = lottos[zLen:]
-  # print(lottos)
-  win_nums.sort()
-  same = 0
-  for pick_num in lottos:
-    for win_num in win_nums:
-      if pick_num == win_num:
-        same += 1
-
-  # print(same)
-
-  levels = {
-    6:1,
-    5:2,
-    4:3,
-    3:4,
-    2:5,
-    1:6,
-    0:6,
-  }
-
-  return [levels[zLen+same], levels[same]]
+  return rank[cnt_0+answer], rank[answer]
 
 # print(solution([44, 1, 0, 0, 31, 25],	[31, 10, 45, 1, 6, 19]	))
 print(solution( [0, 0, 0, 0, 0, 0],	[38, 19, 20, 40, 15, 25]))
