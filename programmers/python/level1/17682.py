fd0af7782b1b3c8a16840721a98cf00e8254be97
veganee <maliethy@gmail.com>
2024-07-15 10:31:52 +0900

    [1차] 다트 게임 다른 풀이

--- programmers/python/level1/17682.py
+++ programmers/python/level1/17682.py
@@ -1,66 +1,27 @@
 import re
 def solution(dartResult):
-  # print(dartResult)
-  numReg = re.compile('[0-9]')
-  numList = numReg.findall(dartResult)
-  # print(numList)
-  intList = []
-  for idx, num in enumerate(numList):
-    if int(num) == 0 and intList[idx-1] == 1:
-      intList[idx-1] = 10
-    else:
-      intList.append(int(num))
-  # print(intList)
+  bonus = {'S':1, 'D':2, 'T':3}
+  option = {'':1,'*':2,'#':-1}
+  reg = re.compile('([0-9]+)([SDT])([*#]?)')
+  dart = reg.findall(dartResult)
+  # print('dart',dart)
 
-  exDict= {
-    'S':1,
-    'D':2,
-    'T':3,
-  }
+  for idx in range(len(dart)):
+    if dart[idx][2] == '*':
+      dart[idx-1] *= 2
+      # print('before',dart[idx-1])
+    dart[idx] = int(dart[idx][0]) ** bonus[dart[idx][1]] * option[dart[idx][2]]
+    # print('after',dart[idx])
+    # print('new dart',dart)
 
-  exReg = re.compile('S|D|T')
-  exList = exReg.findall(dartResult)
-  temp = []
-
-  for int_idx, dart_int in enumerate(intList):
-    temp.append(dart_int ** exDict[exList[int_idx]])
-  # print('temp',temp)
-
-  strReg = re.compile('[^0-9]')
-  strList = strReg.findall(dartResult)
-  # print('strList',strList)
-
-  for idx, str in enumerate(strList):
-    # print('str', str)
-    # print('temp', temp)
-    if str == '#':
-        if idx == 1:
-          temp[0] *= -1
-        elif idx == len(strList) -1:
-          temp[2] *= -1
-        else:
-          temp[1] *= -1
-
-    if str == '*':
-      if idx == 1:
-        temp[0] *=2
-      elif idx == len(strList) -1:
-        temp[1] *=2
-        temp[2] *=2
-      else:
-        temp[0] *=2
-        temp[1] *=2
-
-
-  # print('final',temp)
-  return sum(temp)
+  return sum(dart)
 
 # print(solution('1S2D*3T'))
-print(solution('1D2S#10S'))
-print(solution('1D2S0T'))
-print(solution('1S*2T*3S'))
+# print(solution('1D2S#10S'))
+# print(solution('1D2S0T'))
+# print(solution('1S*2T*3S'))
 print(solution('1D#2S*3S'))
-print(solution('1T2D3D#'))
+# print(solution('1T2D3D#'))
 print(solution('1D2S3T*'))
 
 # 1	1S2D*3T	37	11 * 2 + 22 * 2 + 33
