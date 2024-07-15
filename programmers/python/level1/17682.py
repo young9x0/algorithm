d2ed679e5e9a77ab061cf58f8c542190660b5ab8
veganee <maliethy@gmail.com>
2024-07-15 10:44:01 +0900

    [1차] 다트 게임 다른 풀이2

--- programmers/python/level1/17682.py
+++ programmers/python/level1/17682.py
@@ -1,28 +1,34 @@
-import re
 def solution(dartResult):
-  bonus = {'S':1, 'D':2, 'T':3}
-  option = {'':1,'*':2,'#':-1}
-  reg = re.compile('([0-9]+)([SDT])([*#]?)')
-  dart = reg.findall(dartResult)
-  # print('dart',dart)
 
-  for idx in range(len(dart)):
-    if dart[idx][2] == '*':
-      dart[idx-1] *= 2
-      # print('before',dart[idx-1])
-    dart[idx] = int(dart[idx][0]) ** bonus[dart[idx][1]] * option[dart[idx][2]]
-    # print('after',dart[idx])
-    # print('new dart',dart)
+  answer = []
+  dartResult = dartResult.replace('10','k')
+  points = ['10' if str == 'k' else str for str in dartResult]
+  print('points', points)
+  idx = -1
+  sdt = ['S','D','T']
+  for point in points:
+    print('idx', idx)
+    if point in sdt:
+      answer[idx] = answer[idx] ** (sdt.index(point) + 1)
+      print('answer',answer)
+    elif point == '*':
+      answer[idx]=answer[idx]*2
+      if idx !=0 :
+        answer[idx-1] = answer[idx-1]*2
+    elif point == '#':
+      answer[idx] = answer[idx] * (-1)
+    else:
+      answer.append(int(point))
+      idx += 1
 
-  return sum(dart)
-
-# print(solution('1S2D*3T'))
+  return sum(answer)
+print(solution('1S2D*3T'))
 # print(solution('1D2S#10S'))
 # print(solution('1D2S0T'))
 # print(solution('1S*2T*3S'))
-print(solution('1D#2S*3S'))
+# print(solution('1D#2S*3S'))
 # print(solution('1T2D3D#'))
-print(solution('1D2S3T*'))
+# print(solution('1D2S3T*'))
 
 # 1	1S2D*3T	37	11 * 2 + 22 * 2 + 33
 # 2	1D2S#10S	9	12 + 21 * (-1) + 101
