89099fdeea5c1ab04a96a27d1ed3287ebda59da7
veganee <maliethy@gmail.com>
2024-07-16 13:54:01 +0900

    둘만의 암호 나의 풀이 오류 원인 찾아 해결하기

--- programmers/python/level1/155652.py
+++ programmers/python/level1/155652.py
@@ -1,18 +1,58 @@
-from string import ascii_lowercase
-def solution(s, skip, index):
-  answer = ''
-  a_to_z = set(ascii_lowercase)
-  print(a_to_z)
-  a_to_z -= set(skip)
-  a_to_z = sorted(a_to_z)
-  l = len(a_to_z)
-  alpha_dict = {alpha:idx for idx, alpha in enumerate(a_to_z)}
-  print(alpha_dict)
+def solution1(s, skip, index):
+  print('s',s)
+  new_list = []
+  for code in range(97,123):
+    if chr(code) not in skip:
+      new_list.append(chr(code))
+  print(new_list)
 
+
+  new_dict={}
+  idx = 0
+  for ch in new_list:
+    new_dict[ch] = idx
+    idx += 1
+
+  # print("new_dict",new_dict)
+
+  idx = index % len(new_list) # index가 list length를 초과할 경우 고려 필요
+  new_list = new_list[idx:] + new_list[:idx]
+  # print("new_list",new_list)
+  # print("new_list[index:]", new_list[idx:])
+  # print(" new_list[:index]",  new_list[:idx])
+
+# 이 부분이 오류
+
+
+  answer = ''
   for ch in s:
-    answer += a_to_z[(alpha_dict[ch] + index) % l]
+    answer += new_list[new_dict[ch]]
 
   return answer
 
-print(solution("aukks",	"wbqd",	5	))
+def solution2(s, skip, index):
+  new_list = []
+  for code in range(97,123):
+    if chr(code) not in skip:
+      new_list.append(chr(code))
+
+  new_dict={}
+  idx = 0
+  for ch in new_list:
+    new_dict[ch] = idx
+    idx += 1
+
+  answer = ''
+  # print('len(new_dict)',len(new_dict))
+  for ch in s:
+    # print('new_dict[ch] + index',new_dict[ch] + index)
+    answer += new_list[(new_dict[ch] + index) % len(new_list)]
+
+
+  return answer
+
+
+print(solution1( "a", "bcdefghijk", 20))
+print(solution2( "a", "bcdefghijk", 20))
 # "aukks"	"wbqd"	5	"happy"
+#  "a", "bcdefghijk", 20, "o"
