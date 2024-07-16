ccc74f9b2fe4789c328ed597f0d5a3b8e46787f7
veganee <maliethy@gmail.com>
2024-07-16 09:29:48 +0900

    대충 만든 자판 다른 풀이

--- programmers/python/level1/160586.py
+++ programmers/python/level1/160586.py
@@ -1,31 +1,27 @@
 def solution(keymap, targets):
   answer = []
-  for target in targets:
-    # print('target', target)
-    idx_list = []
+  hs = {}
 
-    for tStr in target:
-      # print('tStr',tStr)
-      cnt = 100
-      for key in keymap:
-        # print('key', key)
-        if tStr in key:
-          # print('key.index(tStr)+1',key.index(tStr)+1)
-          cnt = min(cnt, key.index(tStr)+1)
-          # print('cnt', cnt)
+  for key in keymap:
+    for idx, ch in enumerate(key):
+      hs[ch] = min(idx+1, hs[ch]) if ch in hs else idx+1
 
-      if cnt == 100:
-        idx_list.append(-1)
-      else:
-        idx_list.append(cnt)
-      # print('idx_list', idx_list)
+  print('hs',hs)
 
-    answer.append(sum(idx_list))
+  for target in (targets):
+    cnt = 0
+    for ch in target:
+      if ch not in hs:
+        cnt = -1
+        break
+      cnt += hs[ch]
+    answer.append(cnt)
+
   return answer
 
-# print(solution(["ABACD", "BCEFD"],	["ABCD","AABB"]	))
+print(solution(["ABACD", "BCEFD"],	["ABCD","AABB"]	))
 # print(solution(["AA"],	["B"]	))
-print(solution(["AGZ", "BSSS"],	["ASA","BGZ"]	))
+# print(solution(["AGZ", "BSSS"],	["ASA","BGZ"]	))
 # ["ABACD", "BCEFD"]	["ABCD","AABB"]	[9, 4]
 # ["AA"]	["B"]	[-1]
 # ["AGZ", "BSSS"]	["ASA","BGZ"]	[4, 6]
