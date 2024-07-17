83c8afb38aa8474157f44f66555ddf69b1778b12
veganee <maliethy@gmail.com>
2024-07-17 12:43:51 +0900

    이웃한 칸 정확성: 68.0

---
+++ programmers/python/level1/250125.py
@@ -0,0 +1,19 @@
+def solution(board, h, w):
+  answer = 0
+  if board[h][w] == board[h-1][w]:
+    answer += 1
+  if board[h][w] == board[h+1][w]:
+    answer += 1
+  if board[h][w] == board[h][w-1]:
+    answer += 1
+  if board[h][w] == board[h][w+1]:
+    answer += 1
+
+  return answer
+
+print(solution( [["blue", "red", "orange", "red"], ["red", "red", "blue", "orange"], ["blue", "orange", "red", "red"], ["orange", "orange", "red", "blue"]]	,1	,1	))
+print(solution( [["yellow", "green", "blue"], ["blue", "green", "yellow"], ["yellow", "blue", "blue"]]	,0	,1))
+# [["blue", "red", "orange", "red"], ["red", "red", "blue", "orange"], ["blue", "orange", "red", "red"], ["orange", "orange", "red", "blue"]]
+# 1	1	2
+# [["yellow", "green", "blue"], ["blue", "green", "yellow"], ["yellow", "blue", "blue"]]
+# 0	1	1
