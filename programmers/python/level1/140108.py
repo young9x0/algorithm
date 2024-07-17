e5b1d4a23d9dd403204d909bf44d2c07f27e828f
veganee <maliethy@gmail.com>
2024-07-17 11:17:36 +0900

    문자열 나누기 다른 풀이

--- programmers/python/level1/140108.py
+++ programmers/python/level1/140108.py
@@ -1,39 +1,27 @@
 def solution(s):
   answer = 0
-  sList = [ch for ch in s]
-  target = sList.pop(0)
-  cnt=[1,0]
-  while 0 < len(sList):
-    # print('1 sList',sList)
-    # print('target',target)
-
-    other =sList.pop(0)
-    # print('other',other)
-    # print('2 sList',sList)
-    if target == other:
-      cnt[0] += 1
-
-    else:
-      cnt[1] += 1
-
-    # print('cnt',cnt)
-    if cnt[0] == cnt[1]:
-      # print('hello')
+  save1 = 0
+  save2 = 0
+  print('s',s)
+  for ch in s:
+    print('---ch',ch)
+    print('save1',save1,s[save1])
+    print('save2',save2,s[save2])
+    if save1 == save2:
       answer += 1
-      cnt = [1,0]
+      target = ch
 
-      if len(sList) > 0:
-        target = sList.pop(0)
-      else:
-        target = ''
-      continue
+    print('target:', target)
+    if ch == target:
+      save1 += 1
+    else:
+      save2 += 1
 
-  # print(target)
-  return answer + len(target)
+  return answer
 
-# print(solution("banana"))
+print(solution("bznako"))
 # print(solution("abracadabra"))
-print(solution("aaabbaccccabba"))
+# print(solution("aaabbaccccabba"))
 # "banana"	3
 # "abracadabra"	6
 # "aaabbaccccabba"	3
