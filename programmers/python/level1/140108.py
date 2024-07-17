f0fe56b77a6a8ea6c10aadc0fcf5a7fe01b575e9
veganee <maliethy@gmail.com>
2024-07-17 11:31:08 +0900

    문자열 나누기 다른 풀이3

--- programmers/python/level1/140108.py
+++ programmers/python/level1/140108.py
@@ -1,35 +1,23 @@
-from collections import deque
 def solution(s):
-  q = deque(s)
-  print(q)
   answer = 0
+  idx = 0
+  cnt = 0
 
-  print('s',s)
-  while q:
-    a, b = 1, 0
-    print('---q',q)
-    target = q.popleft()
+  for index, ch in enumerate(s):
+    print('s[idx]',s[idx])
+    print('ch',ch)
+    cnt += 1 if s[idx] == ch else -1
+    print('cnt',cnt)
 
-    while q:
-      other = q.popleft()
-      print('target',target)
-      print('other',other)
-      if target == other:
-        a+=1
-      else:
-        b+=1
+    if cnt == 0:
+      answer+=1
+      idx = index +1
 
-      print('a:',a)
-      print('b:',b)
-      if a == b:
-        answer+=1
-        break
-
-  return answer
+  return answer + 1 if cnt else answer
 
 # print(solution("bznako"))
-# print(solution("abracadabra"))
-print(solution("aaabbaccccabba"))
+print(solution("abracadabra"))
+# print(solution("aaabbaccccabba"))
 # "banana"	3
 # "abracadabra"	6
 # "aaabbaccccabba"	3
