f954df7f2edf0fd03813e4f1074d053d307719e2
veganee <maliethy@gmail.com>
2024-07-17 12:49:35 +0900

    이웃한 칸 정확성: 74.0

--- programmers/python/level1/250125.py
+++ programmers/python/level1/250125.py
@@ -1,10 +1,10 @@
 def solution(board, h, w):
   answer = 0
-  if board[h][w] == board[h-1][w]:
+  if h-1>=0 and board[h][w] == board[h-1][w]:
     answer += 1
   if board[h][w] == board[h+1][w]:
     answer += 1
-  if board[h][w] == board[h][w-1]:
+  if w-1>=0 and board[h][w] == board[h][w-1]:
     answer += 1
   if board[h][w] == board[h][w+1]:
     answer += 1
