3ff019b34fdfa4197661cd7b508f8f5c7c7f5779
veganee <maliethy@gmail.com>
2024-07-19 10:03:14 +0900

    체육복 정확성: 93.3

--- programmers/python/level1/42862.py
+++ programmers/python/level1/42862.py
@@ -4,7 +4,7 @@ def solution(n, lost, reserve):
     r_dict[key] = 0
   # print(r_dict)
 
-  for num in lost:
+  for num in sorted(lost):
     if num in reserve and r_dict[num] == 0:
       r_dict[num] = 1
       continue
@@ -14,14 +14,16 @@ def solution(n, lost, reserve):
     if (num + 1) in reserve and r_dict[num+1] == 0:
       r_dict[num+1] = 1
 
-  # print(r_dict)
+  print(r_dict)
   # print(sum(r_dict.values()))
 
   return n- (len(lost) - sum(r_dict.values()))
 
 # print(solution(5,	[2, 4],	[1, 3, 5]))
 # print(solution(5,	[2, 4],	[3]))
-print(solution(3,	[3],	[1]))
+# print(solution(3,	[3],	[1]))
+print(solution(5,	[4,2],	[5,3]))
 # 5	[2, 4]	[1, 3, 5]	5
 # 5	[2, 4]	[3]	4
 # 3	[3]	[1]	2
+# 5,	[4,2],	[5,3] 5
