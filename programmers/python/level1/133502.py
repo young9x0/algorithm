2eba45ff2b6467e628cf53a48f9102c336f3fbf3
veganee <maliethy@gmail.com>
2024-07-20 17:25:45 +0900

    햄버거 만들기 정확성: 50.0

--- programmers/python/level1/133502.py
+++ programmers/python/level1/133502.py
@@ -1,19 +1,43 @@
 def solution(ingredient):
   cnt = 0
-  in_len = len(ingredient)
-  idx = 0
-  while idx < in_len - 3:
-    # print('idx',idx)
-    if ingredient[idx:idx+4] == [1,2,3,1]:
-      cnt += 1
-      ingredient = ingredient[:idx] + ingredient[idx+4:]
-      idx = 0
-      in_len = len(ingredient)
-      # print('ingredient', ingredient)
-    else:
-      idx += 1
+  in_dict = {}
+  order = [1,2,3,1]
+  cur_idx = 0
 
-  # print('cnt', cnt)
+  for idx, num in enumerate(ingredient):
+    in_dict[idx] = 1
+
+  idx = 0
+  while idx < len(ingredient):
+    # print('-----idx',idx, 'ingredient[idx]', ingredient[idx])
+    # print('cur_idx', cur_idx, 'order[cur_idx]',order[cur_idx])
+    # print('in_dict[idx]', in_dict[idx])
+    if in_dict[idx] == 0:
+      idx += 1
+      continue
+
+    if ingredient[idx] == order[cur_idx]:
+      # print('hello', cur_idx)
+      if cur_idx == 3:
+        cnt += 1
+        for i in range(4):
+          # print('i',i, idx-i)
+          in_dict[idx-i] = 0
+        # print('in_dict',in_dict)
+        idx, cur_idx = 0, 0
+        continue
+
+      cur_idx += 1
+    else:
+      # print('reset')
+      if cur_idx > 0:
+        cur_idx = 0
+        continue
+
+    idx += 1
+    # print('in_dict', in_dict)
+
+
   return cnt
 
 # print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
