8d8ba9e42ab180231c8f41a119f8a7abcd975cff
veganee <maliethy@gmail.com>
2024-07-21 12:19:05 +0900

    햄버거 만들기 다른 풀이2

--- programmers/python/level1/133502.py
+++ programmers/python/level1/133502.py
@@ -1,17 +1,19 @@
 def solution(ingredient):
   cnt = 0
-  temp = []
+  temp = ''.join((map(str ,ingredient)))
+  # print(temp)
 
-  for i in ingredient:
-    temp.append(i)
-    if temp[-4:] == [1,2,3,1]:
-      cnt += 1
-      for n in range(4):
-        temp.pop()
+  while '1231' in temp:
+    # print('---',temp, temp.find('1231'))
+    if temp.find('1231') >= 4:
+      temp = temp[temp.find('1231')-4:]
+      # print('changed temp', temp)
+    temp = temp.replace('1231','',1)
+    cnt+=1
   return cnt
 
-# print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
-print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2]))
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1,3,2,3,2,3,1,2,3,1]))
+# print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2]))
 # [2, 1, 1, 2, 3, 1, 2, 3, 1]	2
 # [1, 3, 2, 1, 2, 1, 3, 1, 2]	0
 # 1, 2, 3, 1
