8ebf1c1db52dbf5694cf019baf22a06ab3e1a76a
veganee <maliethy@gmail.com>
2024-07-23 12:14:15 +0900

    크레인 인형뽑기 게임 다른 풀이

--- programmers/python/level1/64061.py
+++ programmers/python/level1/64061.py
@@ -1,5 +1,6 @@
 def solution(board, moves):
   temp=[]
+  answer = 0
 
   for move in moves:
     # print('---move', move)
@@ -10,23 +11,12 @@ def solution(board, moves):
         temp.append(board[dep][move-1])
         board[dep][move-1] = 0
         # print('board', board)
-        break
 
-  # print('temp',temp)
-  answer = 0
-  idx = 0
-  while idx+1 < len(temp):
-    # print('idx',idx)
-    if temp[idx] == temp[idx+1]:
-      answer += 2
-      temp.pop(idx+1)
-      temp.pop(idx)
-      # print('temp', temp)
-      if idx > 0:
-        idx -= 1
-      continue
-    else:
-      idx += 1
+        if len(temp) > 1:
+          if temp[-1] == temp[-2]:
+            temp[-2:] = []
+            answer += 2
+        break
 
   return answer
 
