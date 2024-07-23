84715060249cee8a94fedb99a1d071ff59a3b69f
veganee <maliethy@gmail.com>
2024-07-23 13:47:12 +0900

    성격유형 검사하기 다른 풀이

--- programmers/python/level1/118666.py
+++ programmers/python/level1/118666.py
@@ -1,55 +1,30 @@
 def solution(survey, choices):
-  choice_dict = {
-    1:3,
-    2:2,
-    3:1,
-    4:0,
-    5:1,
-    6:2,
-    7:3
-  }
-  survey_score={
-    'R':0,
-    'T':0,
-    'C':0,
-    'F':0,
-    'J':0,
-    'M':0,
-    'A':0,
-    'N':0,
-  }
 
-
-  for idx,type in enumerate(survey):
-    if choices[idx] > 4:
-      survey_score[type[1]] += choice_dict[choices[idx]]
-    elif choices[idx] < 4:
-      survey_score[type[0]] += choice_dict[choices[idx]]
-
-  # print(survey_score)
-  def check_score(val1,val2):
-    if val1>val2:
-      return 0
-    elif val1<val2:
-      return 1
+  survey_dict = {'RT':0,'CF':0, 'JM':0, 'AN':0}
+  for key, val in zip(survey, choices):
+    print('key', key)
+    print('val', val-4)
+    if key not in survey_dict.keys():
+      key = key[::-1]
+      print('c key', key)
+      survey_dict[key] -= val-4
     else:
-      return 0
+      survey_dict[key] += val-4
 
+    print('survey_dict',survey_dict)
 
-  def add_type(type1, type2):
-    if check_score(survey_score[type1], survey_score[type2]) == 0:
-      return type1
-    else:
-      return type2
-
-  types = ["RT", "CF", "JM","AN"]
   answer = ''
-  for type in types:
-      answer += add_type(type[0],type[1])
+  for key, val in survey_dict.items():
+    if val > 0:
+      answer += key[1]
+    elif val < 0:
+      answer += key[0]
+    else:
+      answer += key[0]
 
   return answer
 
-# print(solution(["AN", "CF", "MJ", "RT", "NA"],	[5, 3, 2, 7, 5]	))
-print(solution( ["TR", "RT", "TR"],	[7, 1, 3]	))
+print(solution(["AN", "CF", "MJ", "RT", "NA"],	[5, 3, 2, 7, 5]	))
+# print(solution( ["TR", "RT", "TR"]s,	[7, 1, 3]	))
 # ["AN", "CF", "MJ", "RT", "NA"]	[5, 3, 2, 7, 5]	"TCMA"
 # ["TR", "RT", "TR"]	[7, 1, 3]	"RCJA"
