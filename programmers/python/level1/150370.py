94874530e8e1b1a8e9b8e4823d4f80687ce281cc
ju0jung <maliethy@gmail.com>
2024-07-27 14:41:17 +0900

    개인정보 수집 유효기간 다른 풀이

--- programmers/python/level1/150370.py
+++ programmers/python/level1/150370.py
@@ -1,52 +1,15 @@
+def to_days(date):
+  year, month, day = map(int, date.split('.'))
+  return year*28*12+month*28+day
 def solution(today, terms, privacies):
-  answer = []
-  term_dic={term[0]:int(term[2:]) for term in terms}
-  privacy_dic={int(idx+1):list(map(int,privacy[:-2].split('.'))) for idx, privacy in enumerate(privacies)}
+  plus_month={term[0]: int(term[2:]) *28 for term in terms}
+  today = to_days(today)
+  expire_list = [
+    idx+1 for idx, privacy in enumerate(privacies)
+    if to_days(privacies[:12]) + plus_month[privacy[-1]] <= today
+  ]
 
-  today_arr = list(map(int,today.split('.')))
-  # print(term_dic)
-  # print(privacy_dic)
-
-  for idx in privacy_dic.keys():
-    if privacy_dic[idx][2] == 1:
-      privacy_dic[idx][1] -= 1
-      privacy_dic[idx][2] = 28
-    else:
-      privacy_dic[idx][2] -= 1
-
-    plus_month= term_dic[privacies[idx-1][-1]]
-    divided_month= (privacy_dic[idx][1] + plus_month) % 12
-    if privacy_dic[idx][1] + plus_month >12:
-      privacy_dic[idx][0] += (privacy_dic[idx][1] + plus_month) // 12
-      privacy_dic[idx][1]= divided_month % 12
-      if divided_month == 0:
-        privacy_dic[idx][0] -= 1
-        privacy_dic[idx][1]= 12
-    else:
-      privacy_dic[idx][1] += plus_month
-
-  t_year, t_month, t_day = today_arr
-  print('t:',t_year,t_month,t_day)
-  for idx, date in privacy_dic.items():
-    year, month, day = date
-    # print('p:',year,month,day)
-    # print('t_year > year:',t_year > year)
-    if t_year < year:
-      continue
-    elif t_year> year:
-      answer.append(idx)
-      continue
-    # print('t_month > month:',t_month > month)
-    if t_month < month:
-      continue
-    elif t_month > month:
-      answer.append(idx)
-      continue
-    # print('t_day > day:',t_day > day)
-    if t_day > day:
-      answer.append(idx)
-
-  return answer
+  return expire_list
 
 print(solution( "2022.05.19",	["A 6", "B 12", "C 3"],	["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]	))
 # print(solution( "2020.01.01",	["Z 3", "D 5"],	["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]))
