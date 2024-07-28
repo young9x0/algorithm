40634ff8764402d333987c703adf4d30631ed802
ju0jung <maliethy@gmail.com>
2024-07-28 18:58:29 +0900

    공원 산책 다른 풀이

--- programmers/python/level1/172928.py
+++ programmers/python/level1/172928.py
@@ -1,75 +1,50 @@
 def solution(park, routes):
-  current = []
-  obstacle_list = []
-  for row, cols in enumerate(park):
-    if "S" in cols:
-      for idx in range(len(cols)):
-        if cols[idx] == 'S':
-          current = [row, idx]
-    if "X" in cols:
-      for idx in range(len(cols)):
-        if cols[idx] == 'X':
-          obstacle_list.append([row, idx])
+  result = []
+  for row,col in enumerate(park):
+    arr=list(col)
+    if 'S' in arr:
+       cur_row,cur_col  = row, arr.index("S")
+  def check_obstacle(row,col):
+    if (row<0 or row>= len(park)) or (col<0 or col>=len(park[0])) or park[row][col] == 'X': return False
+    else: return True
 
-  # print(current, obstacle_list)
-  max_row= len(park)-1
-  max_col = len(park[0])-1
-  route_dir =  [(route[0],int(route[-1])) for route in routes]
-  for (op, n) in route_dir:
-    cur_row, cur_col =current
-    # print(cur_row, cur_col)
-    # print('-'*50)
-    # print(op,n)
-    block=False
-    if op == "N":
-      if cur_row - n < 0:
-        continue
-      for obstacle in obstacle_list:
-        if cur_col == obstacle[1] and cur_row >  obstacle[0] and cur_row-n <= obstacle[0]:
-          block= True
-          break
-      if block:
-        continue
-      current = [cur_row-n, cur_col]
-      continue
-    if op == "S":
-      if cur_row + n > max_row:
-        continue
-      for obstacle in obstacle_list:
-        if cur_col == obstacle[1] and cur_row <  obstacle[0] and cur_row+n >= obstacle[0]:
-          block=True
-          break
-      if block:
-        continue
-      current = [cur_row+n, cur_col]
-      continue
-    if op == "W":
-      if cur_col - n <0:
-        continue
-      for obstacle in obstacle_list:
-        if cur_row == obstacle[0] and cur_col >  obstacle[1] and cur_col-n <= obstacle[1]:
-           block=True
-           break
-      if block:
-        continue
-      current = [cur_row, cur_col-n]
-      continue
-    if op == "E":
-      if cur_col + n > max_col:
-        continue
-      for obstacle in obstacle_list:
-        if cur_row == obstacle[0] and cur_col <  obstacle[1] and cur_col+n >= obstacle[1]:
-          block=True
-          break
-      if block:
-        continue
-      current = [cur_row, cur_col+n]
+  for route in routes:
+    direct, move = route.split()
+    move = int(move)
 
-  return current
+    if direct == 'E':
+      new_col = cur_col + move
+      for step_x in range(cur_col, new_col + 1):
+        if check_obstacle(cur_row,step_x) == False:
+          break
+      else:
+          cur_col = new_col
+    elif direct == 'W':
+          new_col = cur_col - move
+          for step_x in range(new_col, cur_col + 1):
+            if check_obstacle(cur_row,step_x) == False:
+              break
+          else:
+            cur_col = new_col
+    elif direct == 'N':
+      new_row = cur_row - move
+      for step_y in range(new_row, cur_row + 1):
+        if check_obstacle( step_y, cur_col) == False:
+          break
+      else:
+        cur_row = new_row
+    elif direct == 'S':
+      new_row = cur_row + move
+      for step_y in range(cur_row, new_row + 1):
+        if check_obstacle( step_y,cur_col) == False:
+          break
+      else:
+        cur_row = new_row
+  return [cur_row,cur_col]
 
-# print(solution(["OOO","OOO","OOS"],	["E 2","S 2","W 1"]	))
+print(solution(["OOO","OOO","OOS"],	["E 2","S 2","W 1"]	))
 # print(solution(["SOO","OXX","OOO"],	["E 2","S 2","W 1"]))
-print(solution(["OSO","OOO","OXO","OOO"],	["E 2","S 3","W 1"]))
+# print(solution(["OSO","OOO","OXO","OOO"],	["E 2","S 3","W 1"]))
 # ["SOO","OOO","OOO"]	["E 2","S 2","W 1"]	[2,1]
 # ["SOO","OXX","OOO"]	["E 2","S 2","W 1"]	[0,1]
 # ["OSO","OOO","OXO","OOO"]	["E 2","S 3","W 1"]	[0,0]
