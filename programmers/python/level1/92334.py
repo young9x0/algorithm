38eac7b35f4f64bc4b78d9085093c1ccdf10194e
ju0jung <maliethy@gmail.com>
2024-07-30 15:31:40 +0900

    신고 결과 받기 다른 풀이

--- programmers/python/level1/92334.py
+++ programmers/python/level1/92334.py
@@ -1,27 +1,18 @@
 def solution(id_list, report, k):
-  answer = []
-  informer_info_list={id:[] for id in id_list}
-  need_mail_list = {id:0 for id in id_list}
+  answer = [0]*len(id_list)
+  report_cnt={id:0 for id in id_list}
 
-  for info in report:
-   informer, id  =info.split()
-   if informer not in informer_info_list[id]:
-    informer_info_list[id].append(informer)
+  for info in set(report):
+    report_cnt[info.split()[1]] += 1
 
-  # print(informer_info_list)
-  for id in id_list:
-    if len(informer_info_list[id]) >= k:
-      for informer in informer_info_list[id]:
-        need_mail_list[informer] += 1
-
-  # print(need_mail_list)
-
-  for id in id_list:
-    answer.append(need_mail_list[id])
+  for info in set(report):
+    id, reported = info.split()
+    if report_cnt[reported] >= k:
+      answer[id_list.index(id)] += 1
 
   return answer
 
-# print(solution(["muzi", "frodo", "apeach", "neo"],	["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],	2))
+print(solution(["muzi", "frodo", "apeach", "neo"],	["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],	2))
 print(solution(["con", "ryan"],	["ryan con", "ryan con", "ryan con", "ryan con"],	3))
 # ["muzi", "frodo", "apeach", "neo"]	["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]	2	[2,1,1,0]
 # ["con", "ryan"]	["ryan con", "ryan con", "ryan con", "ryan con"]	3	[0,0]
