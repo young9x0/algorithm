a621c6ad9d2635ac48175767069096a4b83f341c
ju0jung <maliethy@gmail.com>
2024-08-01 20:13:52 +0900

    점프와 순간 이동 다른 풀이

--- programmers/python/level2/12980.py
+++ programmers/python/level2/12980.py
@@ -1,20 +1,10 @@
 def solution(n):
-  def divide_two(num, cnt):
-    # print('-'*50)
-    # print('num',num)
-    # print('cnt',cnt)
-    if num == 1:
-      return cnt
-    if num%2==1:
-      cnt+=1
-    return divide_two(num//2, cnt)
+  return bin(n).count(1)
 
-  return divide_two(n,1)
-
-# print(solution(5))
+print(solution(5))
 # print(solution(6))
 # print(solution(5000))
-print(solution(27))
+# print(solution(27))
 # 5	2
 # 6	2
 # 5000	5
