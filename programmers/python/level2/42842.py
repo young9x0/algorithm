7cb716b13220fddec1bb8f8f92b5aa253b9fef54
ju0jung <maliethy@gmail.com>
2024-08-02 20:10:09 +0900

    카펫 다른 풀이

--- programmers/python/level2/42842.py
+++ programmers/python/level2/42842.py
@@ -1,26 +1,14 @@
 def solution(brown, yellow):
-  def find_divider_list(num):
-    result = set()
-    for divider in range(1,num+1):
-      if num % divider == 0:
-        result.add(divider)
-    return result
-
-  yellow_set= find_divider_list(yellow)
-  yellow_list = sorted(list(yellow_set))
-  # print(yellow_list)
-  yellow_length = len(yellow_set)
-  for idx in range(yellow_length//2,yellow_length):
-    width= (yellow_list[idx])
-    height=int((yellow/yellow_list[idx]))
-    if brown == ((width + 2) + height)*2:
-      # print('hello')
-      return [width+ 2, height+2]
+  for divider in range(1, int(yellow**(1/2))+1):
+    if yellow % divider == 0:
+      print(yellow//divider)
+      if 2*(divider + yellow//divider) == brown-4:
+        return [yellow//divider + 2, divider+2]
 
 
 print(solution(10,2))
 print(solution(8,1))
-# print(solution( 24,	24))
+print(solution( 24,	24))
 # 10	2	[4, 3]
 # 8	1	[3, 3]
 # 24	24	[8, 6]
