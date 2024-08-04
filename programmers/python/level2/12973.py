9bf79dd5f0432a775ac1c044cc835afedfed750d
ju0jung <maliethy@gmail.com>
2024-08-04 14:42:45 +0900

    짝지어 제거하기 다른 풀이

--- programmers/python/level2/12973.py
+++ programmers/python/level2/12973.py
@@ -1,22 +1,18 @@
 def solution(s):
-  idx = 0
-  temp = s
-  while idx+1 < len(temp):
-    # print('temp',temp)
-    # print('idx',idx)
-    if temp[idx] == temp[idx+1]:
-      # print('hello')
-      temp = temp[:idx] + temp[idx+2:]
-      if idx>0:
-        idx -= 1
+  answer = []
+
+  for ch in s:
+    if not(answer):
+      answer.append(ch)
     else:
-      idx+=1
+      if answer[-1] == ch:
+        answer.pop()
+      else:
+        answer.append(ch)
 
-  # print(temp)
-
-  return 1 if len(temp) == 0 else 0
-
-# print(solution("baabaa"))
+  return 1 if len(answer) == 0 else 0
+# print(solution("abcdef"))
+print(solution("baabaa"))
 # print(solution("cdcd"))
 # baabaa	1
 # cdcd	0
