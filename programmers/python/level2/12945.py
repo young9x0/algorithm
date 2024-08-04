894fc7c1fff3602e78cdf64594c450b7de78eb83
ju0jung <maliethy@gmail.com>
2024-08-04 15:17:09 +0900

    피보나치 수 다른 풀이

--- programmers/python/level2/12945.py
+++ programmers/python/level2/12945.py
@@ -1,14 +1,8 @@
 def solution(n):
-  fibonacci_number = [0,1]
-  def add_fibonacci(num,list):
-    list.append(fibonacci_number[num-1] + fibonacci_number[num-2])
-
-  for num in range(2,n+1):
-    add_fibonacci(num, fibonacci_number)
-
-  # print(fibonacci_number)
-
-  return fibonacci_number[n] % 1234567
+  a,b=0,1
+  for i in range(n):
+    a,b = b, a+b
+  return a % 1234567
 
 # print(solution(3))
 print(solution(5))
