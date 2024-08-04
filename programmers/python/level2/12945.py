3ceb701d3cf64f8c1fbb5e3dae7fabfd63a3a89c
ju0jung <maliethy@gmail.com>
2024-08-04 15:10:07 +0900

    피보나치 수 정확성: 42.9

---
+++ programmers/python/level2/12945.py
@@ -0,0 +1,16 @@
+def solution(n):
+  fibonacci_number = [0,1]
+  def add_fibonacci(num,list):
+    list.append(fibonacci_number[num-1] + fibonacci_number[num-2])
+
+  for num in range(2,n+1):
+    add_fibonacci(num, fibonacci_number)
+
+  # print(fibonacci_number)
+
+  return fibonacci_number[n]
+
+# print(solution(3))
+print(solution(5))
+# 3	2
+# 5	5
