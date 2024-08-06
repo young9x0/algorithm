d51c5dcf3ebd39793e62fce44bfb25225e772b7f
ju0jung <maliethy@gmail.com>
2024-08-06 15:18:27 +0900

    JadenCase 문자열 만들기 정확성: 94.4

--- programmers/python/level2/12951.py
+++ programmers/python/level2/12951.py
@@ -1,21 +1,24 @@
 def solution(s):
-  answer = []
-  splited = s.split(" ")
+  answer = ""
+  splited = s.lower().split(" ")
+  # print(splited)
   for ch in splited:
     # print('ch', ch)
     if len(ch)>0:
       if ch[0].islower():
-        answer.append(ch[0].upper() + ch[1:].lower())
+        answer += ch[0].upper() + ch[1:]
       else:
-        answer.append(ch.lower())
-
+        answer += ch
+      if ch != splited[-1]:
+        answer += " "
     else:
-      answer.append(ch)
-  return " ".join(answer)
+      answer += " "
+  return answer
 
 # print(solution("3people unFollowed me"))
 # print(solution("3People unFollowed me"))
+print(solution("3People unFollowed me     1aA      2a"))
 # print(solution("for the last week"))
-print(solution("for    the last week"))
+# print(solution("for    the last week"))
 # "3people unFollowed me"	"3people Unfollowed Me"
 # "for the last week"	"For The Last Week"
