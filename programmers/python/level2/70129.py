285cd5160906d5c23e25fc4ad73729ad316eecf5
ju0jung <maliethy@gmail.com>
2024-08-06 14:02:52 +0900

    이진 변환 반복하기 다른 풀이

--- programmers/python/level2/70129.py
+++ programmers/python/level2/70129.py
@@ -3,14 +3,10 @@ def solution(s):
   process_cnt=0
 
   while s != "1":
-    zero_cnt += s.count("0")
     process_cnt += 1
-
-    if len(s) > 1:
-      new = s.replace("0","")
-      # print('new',new)
-      s = str('{0:b}'.format(len(new)))
-      # print('s',s)
+    one_cnt = s.count("1")
+    zero_cnt += len(s) - one_cnt
+    s = str('{0:b}'.format(one_cnt))
 
   return [process_cnt,zero_cnt]
 
