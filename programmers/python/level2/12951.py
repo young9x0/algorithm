3d91cee4c464847c175c2c9a819f53fe1559ee16
ju0jung <maliethy@gmail.com>
2024-08-06 15:59:38 +0900

    JadenCase 문자열 만들기 다른 풀이

--- programmers/python/level2/12951.py
+++ programmers/python/level2/12951.py
@@ -1,26 +1,13 @@
 def solution(s):
-  answer = ""
-  # print(len(s))
-  splited = s.lower().split(" ")
-  # print(splited)
+  answer = ''
 
-  for idx, ch in enumerate(splited):
-    # print('idx', idx)
-    # print('==ch', ch)
-    if ch != "":
-      if ch[0].islower():
-        answer += ch[0].upper() + ch[1:]
-      else:
-        answer += ch
-      if ch != splited[-1]:
-        # print('no fin')
-        answer += " "
+  for ch in s.lower().split(' '):
+    print("ch",ch.capitalize() == '')
+    if answer == '':
+      answer += ch.capitalize()
     else:
-      if idx != len(splited)-1:
-        # print('---fin')
-        answer += " "
+      answer += ' '+ ch.capitalize()
 
-  # print(len(answer))
   return answer
 
 # print(solution("3people unFollowed me"))
