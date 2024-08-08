91fa599ec9fd7516b34e725b9d4402fda599401e
ju0jung <maliethy@gmail.com>
2024-08-08 22:28:38 +0900

    최솟값 만들기 정확성: 69.6 효율성: 0.0

--- programmers/python/level2/12941.py
+++ programmers/python/level2/12941.py
@@ -1,21 +1,36 @@
 def solution(A,B):
   answer = 0
-  a_temp = [copy_a for copy_a in A]
-  b_temp = [copy_b for copy_b in B]
-  while len(a_temp) > 0:
-    a_val = max(a_temp)
-    a_idx = a_temp.index(a_val)
-    a_temp.pop(a_idx)
-    b_val = min(b_temp)
-    b_idx = b_temp.index(b_val)
-    b_temp.pop(b_idx)
-    answer += a_val * b_val
+  cnt = 0
+  used_a = [a for a in A]
+  used_b = [b for b in B]
+  limit = len(A)
+  while limit > cnt:
+    # print('-'*50)
+    # print('cnt',cnt)
+    # print('used_a',used_a)
+    # print('used_b',used_b)
+    cnt += 1
+    max_a=[0,0]
+    min_b=[0,max(B)]
+    for a_idx, a_val in enumerate(A):
+        # print('a_val',a_val)
+        if used_a[a_idx] > 0 and max_a[1] < a_val:
+          max_a = [a_idx,a_val]
+    for b_idx, b_val in enumerate(B):
+        if used_b[b_idx] > 0 and min_b[1] > b_val:
+          min_b = [b_idx,b_val]
 
+    # print('max_a',max_a)
+    # print('min_b',min_b)
+    answer += max_a[1] * min_b[1]
+    used_a[max_a[0]] = 0
+    used_b[min_b[0]] = 0
+    # print('answer',answer)
   return answer
 
 # print(solution([1, 4, 2],	[5, 4, 3]))
 # print(solution([1, 4, 2],	[5, 4, 4]))
 print(solution([1,2],	[3,4]))
-print(solution([2],	[3]))
+# print(solution([2],	[3]))
 # [1, 4, 2]	[5, 4, 4]	29
 # [1,2]	[3,4]	10
