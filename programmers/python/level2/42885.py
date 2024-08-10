0e7a000ecd267e29378e327e75d1ecf583771c60
ju0jung <maliethy@gmail.com>
2024-08-11 00:41:59 +0900

    구명보트2 정확성: 14.8 효율성: 0.0

--- programmers/python/level2/42885.py
+++ programmers/python/level2/42885.py
@@ -1,16 +1,34 @@
 def solution(people, limit):
+  answer = 0
+  boat_idx= 0
   saved=[]
-  for target_idx, target in enumerate(people):
-    for next_idx in range(target_idx+1, len(people)):
-      if next_idx in saved:
+  p_len = len(people)
+  while p_len-1> boat_idx:
+    # print('-'*50)
+    # print('boat_idx',boat_idx)
+    # print('saved',saved)
+    for remain_idx in range(boat_idx+1,p_len):
+      # print('remain_idx',remain_idx)
+      if remain_idx in saved:
+        if remain_idx == p_len - 1:
+          # print('finish')
+          answer += 1
+          boat_idx += 1
         continue
-      if target + people[next_idx] <= limit:
-        saved.append(next_idx)
+      if people[boat_idx] + people[remain_idx] <= limit:
+        # print('hello')
+        answer += 1
+        boat_idx += 1
+        saved.append(remain_idx)
+        continue
+      if remain_idx == p_len - 1:
+        # print('finish')
+        answer += 1
+        boat_idx += 1
 
-  # print(saved)
-  return len(people) - len(saved)
+  return answer + 1 - len(saved)
 
 print(solution([70, 50, 80, 50],	100	))
-print(solution([70, 80, 50],	100	))
+# print(solution([70, 80, 50],	100	))
 # [70, 50, 80, 50]	100	3
 # [70, 80, 50]	100	3
