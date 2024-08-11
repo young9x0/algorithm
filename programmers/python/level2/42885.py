775f123b30c707b5168c87d9df3d6824dc46f8f9
ju0jung <maliethy@gmail.com>
2024-08-11 17:52:07 +0900

    구명보트 정확성: 81.5 효율성: 7.4

--- programmers/python/level2/42885.py
+++ programmers/python/level2/42885.py
@@ -1,25 +1,26 @@
 def solution(people, limit):
-  people = sorted(people, reverse=True)
+  remain = sorted(people, reverse=True)
   answer = 0
   boat_idx= 0
-  saved=[]
-  p_len = len(people)
-  while boat_idx < p_len:
-    # print('--boat_idx',boat_idx, people[boat_idx])
-    if boat_idx in saved:
+
+  while boat_idx < len(remain):
+    # print('remain',remain)
+    # print('--boat_idx',boat_idx, remain[boat_idx])
+    if limit - remain[boat_idx] < 40:
+      # print('over')
       boat_idx += 1
+      answer += 1
       continue
 
-    for minus_idx in range(1,p_len-boat_idx):
-      # print('boat_idx',boat_idx, people[boat_idx])
-      # print('minus_idx',-(minus_idx), people[-(minus_idx)])
-      if p_len-minus_idx in saved:
-        continue
-      if people[boat_idx] + people[-(minus_idx)] <= limit:
+    for minus_idx in range(1,len(remain)):
+      # print('boat_idx',boat_idx, remain[boat_idx])
+      # print('minus_idx',-(minus_idx), remain[-(minus_idx)])
+
+      if remain[boat_idx] + remain[-(minus_idx)] <= limit:
         # print('hello')
         boat_idx += 1
         answer+=1
-        saved.append(p_len-minus_idx)
+        remain = remain[:-1]
         break
     else:
       answer+=1
@@ -30,7 +31,7 @@ def solution(people, limit):
 
 # print(solution([70, 50, 80, 50, 50 ],	100	))
 # print(solution([70, 50, 80, 50],	100	))
-print(solution([70, 80, 50],	100	))
-# print(solution([50, 50, 50, 60, 70, 80, 40],	110	))
+# print(solution([70, 80, 50],	100	))
+print(solution([50, 50, 50, 60, 70, 80, 40],	110	))
 # [70, 50, 80, 50]	100	3
 # [70, 80, 50]	100	3
