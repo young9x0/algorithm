6760c09cb8b30754c4a43f9cc7ca1b4c70d6833f
ju0jung <maliethy@gmail.com>
2024-08-12 15:36:38 +0900

    구명보트 다른 풀이2

--- programmers/python/level2/42885.py
+++ programmers/python/level2/42885.py
@@ -1,22 +1,25 @@
+from collections import deque
+
 def solution(people, limit):
-  answer = 0
-  people.sort()
-  # print(people)
-  in_boat = 0
-  need_check= len(people)-1
+  result=0
+  deque_people = deque(sorted(people))
+  print(deque_people)
+  while deque_people:
+    print('deque_people',deque_people)
+    left = deque_people.popleft()
+    if not deque_people:
+      print('if not', deque_people)
+      return result+1
+    right = deque_people.pop()
+    if left+right>limit:
+      deque_people.appendleft(left)
+    result+=1
+  return result
 
-  while in_boat < need_check:
-    print('people[in_boat]',in_boat,people[in_boat])
-    print('people[need_check]',need_check,people[need_check])
-    if people[need_check] + people[in_boat] <= limit:
-      in_boat += 1
-      answer += 1
-    need_check -= 1
-  return len(people)-answer
-
-print(solution([70, 50, 80, 50, 50 ],	100	))
+# print(solution([70, 50, 80, 50, 50 ],	100	))
 # print(solution([70, 50, 80, 50],	100	))
 # print(solution([70, 80, 50],	100	))
-# print(solution([50, 50, 50, 60, 70, 80, 40],	100	))
+# print(solution([70],	100	))
+print(solution([50, 50, 50, 60, 70, 80, 40],	100	))
 # [70, 50, 80, 50]	100	3
 # [70, 80, 50]	100	3
