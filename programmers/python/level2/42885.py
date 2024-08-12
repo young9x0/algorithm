1f89eaad081ff100f08f4c18b8139f771b178ffb
ju0jung <maliethy@gmail.com>
2024-08-12 15:29:06 +0900

    구명보트 다른 풀이

--- programmers/python/level2/42885.py
+++ programmers/python/level2/42885.py
@@ -1,37 +1,22 @@
 def solution(people, limit):
-  remain = sorted(people, reverse=True)
   answer = 0
-  boat_idx= 0
+  people.sort()
+  # print(people)
+  in_boat = 0
+  need_check= len(people)-1
 
-  while boat_idx < len(remain):
-    # print('remain',remain)
-    # print('--boat_idx',boat_idx, remain[boat_idx])
-    if limit - remain[boat_idx] < 40:
-      # print('over')
-      boat_idx += 1
+  while in_boat < need_check:
+    print('people[in_boat]',in_boat,people[in_boat])
+    print('people[need_check]',need_check,people[need_check])
+    if people[need_check] + people[in_boat] <= limit:
+      in_boat += 1
       answer += 1
-      continue
+    need_check -= 1
+  return len(people)-answer
 
-    for minus_idx in range(1,len(remain)):
-      # print('boat_idx',boat_idx, remain[boat_idx])
-      # print('minus_idx',-(minus_idx), remain[-(minus_idx)])
-
-      if remain[boat_idx] + remain[-(minus_idx)] <= limit:
-        # print('hello')
-        boat_idx += 1
-        answer+=1
-        remain = remain[:-1]
-        break
-    else:
-      answer+=1
-      boat_idx+=1
-      # print('next')
-
-  return answer
-
-# print(solution([70, 50, 80, 50, 50 ],	100	))
+print(solution([70, 50, 80, 50, 50 ],	100	))
 # print(solution([70, 50, 80, 50],	100	))
 # print(solution([70, 80, 50],	100	))
-print(solution([50, 50, 50, 60, 70, 80, 40],	110	))
+# print(solution([50, 50, 50, 60, 70, 80, 40],	100	))
 # [70, 50, 80, 50]	100	3
 # [70, 80, 50]	100	3
