d984ff78dde09d2657acaff83cf536fe259a1a0d
ju0jung <maliethy@gmail.com>
2024-08-13 13:34:05 +0900

    멀리 뛰기 정확성: 37.5

--- programmers/python/level2/12914.py
+++ programmers/python/level2/12914.py
@@ -8,7 +8,7 @@ def solution(n):
     multi = 1
     max_cnt = cnt
     min_cnt = n-cnt*2
-    if cnt < n-cnt:
+    if cnt < n-cnt*2:
       max_cnt = n-cnt*2
       min_cnt = cnt
     # print('max', max_cnt)
@@ -21,15 +21,14 @@ def solution(n):
     # print('multi', multi)
     return int(multi)
 
-  two_cnt = 2 if n > 3 else n//2
-  for cnt in range(two_cnt+1):
+  for cnt in range( n//2+1):
     answer += combi(cnt)
     # print('answer', answer)
   return answer % 1234567
 
 # print(solution(7))
-# print(solution(5))
-# print(solution(4))
-# print(solution(3))
+print(solution(5))
+print(solution(4))
+print(solution(3))
 # 4	5
 # 3	3
