b38a70e78eaeed714211b6a7bbdb2b62a3c660fb
ju0jung <maliethy@gmail.com>
2024-08-14 15:58:48 +0900

    멀리 뛰기 다른 풀이

--- programmers/python/level2/12914.py
+++ programmers/python/level2/12914.py
@@ -1,34 +1,20 @@
 def solution(n):
-  answer = 0
+  if n ==1:
+    return 1
+  a, b = 1,2
+  for i in range(2,n):
+  # print(i, 'a,b',a,b)
+    a, b = b, a+b
+  return b % 1234567
 
-  def combi(cnt):
-    # print('---', cnt, n-cnt*2)
-    if cnt == 0 or n-cnt*2 == 0:
-     return 1
-    multi = 1
-    max_cnt = cnt
-    min_cnt = n-cnt*2
-    if cnt < n-cnt*2:
-      max_cnt = n-cnt*2
-      min_cnt = cnt
-    # print('max', max_cnt)
-    # print('min', min_cnt)
-    for num in range(max_cnt+1,(max_cnt+min_cnt)+1):
-      # print('num', num)
-      multi *= num
-    for num in range(1,min_cnt+1):
-      multi /= num
-    # print('multi', multi)
-    return int(multi)
 
-  for cnt in range( n//2+1):
-    answer += combi(cnt)
-    # print('answer', answer)
-  return answer % 1234567
 
 # print(solution(7))
-print(solution(5))
-print(solution(4))
-print(solution(3))
+# print(solution(5))
+# print(solution(4))
+# print(solution(3))
+# print(solution(100))
+print(solution(1))
+
 # 4	5
 # 3	3
