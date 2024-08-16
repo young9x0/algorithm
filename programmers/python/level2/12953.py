d5248d1181eb4a4c88e80ff1ffb939c415e6d521
ju0jung <maliethy@gmail.com>
2024-08-16 15:01:48 +0900

    N개의 최소공배수 다른 풀이2

--- programmers/python/level2/12953.py
+++ programmers/python/level2/12953.py
@@ -1,22 +1,17 @@
+from functools import reduce
 def solution(arr):
+  # 유클리드 호제법 증명: https://velog.io/@gjtang/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C-%ED%98%B8%EC%A0%9C%EB%B2%95
   def gcd(a,b):
-    print('a',a)
-    print('b',b)
+    # print('a',a)
+    # print('b',b)
     if b == 0:
       return a
     return gcd(b, a%b)
 
-  answer = arr[0]
-  for num in arr:
-    print('num',num)
-    print('answer',answer)
-    print('gcd',gcd(num, answer))
-    answer = num*answer // gcd(num, answer)
+  return reduce(lambda a,b:a*b//gcd(a,b), arr)
 
-  return answer
-
-# print(solution([2,6,8,14]))
-print(solution([14,24,36,18]))
+print(solution([2,6,8,14]))
+# print(solution([14,24,36,18]))
 # print(solution([1,2,3]))
 # [2,6,8,14]	168
 # [1,2,3]	6
