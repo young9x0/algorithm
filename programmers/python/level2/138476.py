9f9b2883b0ecdd4141de6d38ab0908fc20c4c5c5
ju0jung <maliethy@gmail.com>
2024-08-18 17:51:02 +0900

    귤 고르기 다른 풀이2

--- programmers/python/level2/138476.py
+++ programmers/python/level2/138476.py
@@ -1,14 +1,9 @@
-import collections
+from collections import Counter
 def solution(k, tangerine):
-  answer=0
-  cnt=collections.Counter(tangerine)
-  print('cnt',cnt)
-  for val in sorted(cnt.values(), reverse=True):
-    k-=val
-    answer+=1
-    if k<=0:
-      break
-  return answer
+  counter=Counter(tangerine)
+  tangerine.sort(key=lambda t:(-counter[t],t))
+  print(tangerine)
+  return len(set(tangerine[:k]))
 
 print(solution(6,	[1, 3, 2, 5, 4, 5, 2, 3]))
 # print(solution(4,	[1, 3, 2, 5, 4, 5, 2, 3]))
