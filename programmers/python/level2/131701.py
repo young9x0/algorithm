1080e14e82130f0a495bba03cfb5303e45b30cab
ju0jung <maliethy@gmail.com>
2024-08-19 14:57:08 +0900

    연속 부분 수열 합의 개수 다른 풀이2

--- programmers/python/level2/131701.py
+++ programmers/python/level2/131701.py
@@ -1,13 +1,9 @@
 def solution(elements):
-  sum_set=set()
   el_len=len(elements)
-  for idx in range(el_len):
-    temp=elements[idx]
-    sum_set.add(temp)
-    for add_idx in range(idx+1,idx+el_len):
-      temp += elements[add_idx%el_len]
-      sum_set.add(temp)
-  return len(sum_set)
+  elements *=2
+
+  print(elements)
+  return len(set(sum(elements[j:j+n] for j in range(el_len) for n in range(1,el_len+1))))
 
 print(solution([7,9,1,1,4]))
 # [7,9,1,1,4]	18
