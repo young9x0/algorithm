e7cae8d2c5041ec624b69fa9d598f73a1f5f7e02
ju0jung <maliethy@gmail.com>
2024-08-22 15:14:54 +0900

    n^2 배열 자르기 다른 풀이

--- programmers/python/level2/87390.py
+++ programmers/python/level2/87390.py
@@ -1,15 +1,8 @@
 def solution(n, left, right):
   answer = []
-  for row in range(left//n,right//n+1):
-    for col in range(n):
-      # print('row',row)
-      # print('col',col)
-      # print('col%n+1',col%n+1)
-      answer.append(max(row+1,col%n+1))
-  # print('answer',answer)
-
-  except_cnt= (left//n)*n
-  return answer[left-(except_cnt):right+1-except_cnt]
+  for idx in range(left, right+1):
+    answer.append(max(idx//n,idx%n)+1)
+  return answer
 
 print(solution(3,	2,	5))
 # print(solution(4,	7,	14))
