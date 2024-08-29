109facf07e00723f0cd607b896176cd4dee202c2
ju0jung <maliethy@gmail.com>
2024-08-29 20:48:09 +0900

    의상 정확성: 96.4 - 2

--- programmers/python/level2/42578.py
+++ programmers/python/level2/42578.py
@@ -10,30 +10,32 @@ def solution(clothes):
   c_len=len(c_val)
   answer=0
 
-  def next_el(temp, next, result):
+  def next_el(multiple, next, result):
     # print('temp',temp)
     # print('next',next)
 
     if next == c_len:
-      # print('final',temp)
+      # print('final', multiple)
       # print('result',result)
       return result
 
     for idx in range(next,c_len):
-      comb= [v for v in temp]
-      comb.append(c_val[idx])
+      # comb= [v for v in temp]
+      # comb.append(c_val[idx])
       # print('comb',comb)
-      multiple=1
-      for cnt in comb:
-        multiple *= cnt
-      result.append(multiple)
-      # print('result',result)
-      next_el(comb, idx+1, result)
+      # res=1
+      # for cnt in comb:
+      #   res *= cnt
+      # result.append(res)
+      result.append(multiple*c_val[idx])
+      # print('prev',multiple*c_val[idx])
+      next_el(multiple*c_val[idx], idx+1, result)
 
 
   for idx, val in enumerate(c_val):
     res=[]
-    next_el([val],idx+1,res)
+    # next_el([val],idx+1,res)
+    next_el(val,idx+1,res)
     # print('res',res)
     answer += sum(res)
 
@@ -45,3 +47,8 @@ print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_make
 # print(solution([["crow_mask", "coat"], ["blue_sunglasses", "upper"],["smoky_makeup", "under"],["smoky_makeup", "face"], ["smoky_makeup", "face"]]))
 # [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	5
 # [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]	3
+# res [2, 6, 24, 8, 3, 12, 4]
+# res [6, 24, 8]
+# res [12]
+# res []
+# 119
