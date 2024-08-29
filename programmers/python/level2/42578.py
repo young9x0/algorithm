74be06a1cf974bed9882215273c6fdf55ac09640
ju0jung <maliethy@gmail.com>
2024-08-29 19:05:02 +0900

    의상 정확성: 96.4

--- programmers/python/level2/42578.py
+++ programmers/python/level2/42578.py
@@ -3,54 +3,45 @@ def solution(clothes):
   for item in clothes:
     c_dict[item[1]] += 1
 
-  # print((c_dict))
+  # print(c_dict)
   c_val=list(c_dict.values())
+  # print(c_val)
+  # c_val=[1,2,3,4]
   c_len=len(c_val)
-
-  def make_comb(limit_cnt,comb, result, idx):
-    # print('make_comb= limit_cnt,comb,result,idx',limit_cnt,comb,result,idx)
-    if limit_cnt == len(comb):
-      res=1
-      for cnt in comb:
-        res*=cnt
-      result.append(res)
-      comb.pop()
-      # print('limit',res)
-      if idx == c_len:
-        # print('finish',sum(result))
-        return sum(result)
-      else:
-        return make_comb(limit_cnt, comb, result, idx)
-    else:
-      for next in range(idx, c_len):
-        # print('c_val[next]',c_val[next])
-        comb.append(c_val[next])
-
-        return make_comb(limit_cnt, comb, result, idx+1)
-
   answer=0
-  for limit_cnt in range(1,c_len+1):
-    if limit_cnt == 1:
-      answer += sum(c_val)
-      # print('limit_cnt=1',answer)
-    else:
-      for idx in range(c_len):
-        if idx+limit_cnt > c_len:
-          continue
 
-        copy=[el for el in c_val]
-        # print('-'*50)
-        # print('limit_cnt',limit_cnt)
-        cnt=copy[idx]
-        # print('comb',[cnt])
-        # print('remain',copy[idx+1:])
-        # print('-'*50)
-        answer += make_comb(limit_cnt, [cnt], [], idx+1)
-  return answer
+  def next_el(temp, next, result):
+    # print('temp',temp)
+    # print('next',next)
+
+    if next == c_len:
+      # print('final',temp)
+      # print('result',result)
+      return result
+
+    for idx in range(next,c_len):
+      comb= [v for v in temp]
+      comb.append(c_val[idx])
+      # print('comb',comb)
+      multiple=1
+      for cnt in comb:
+        multiple *= cnt
+      result.append(multiple)
+      # print('result',result)
+      next_el(comb, idx+1, result)
+
+
+  for idx, val in enumerate(c_val):
+    res=[]
+    next_el([val],idx+1,res)
+    # print('res',res)
+    answer += sum(res)
+
+  return answer + sum(c_val)
 
 # print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
-print(solution([["crow_mask", "face"], ["blue_sunglasses", "under"],["blue_sunglasses", "under"], ["smoky_makeup", "upper"],["smoky_makeup", "upper"],["smoky_makeup", "upper"]]))
-# print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))
+# print(solution([["crow_mask", "face"],["crow_mask", "face"],["crow_mask", "face"],["crow_mask", "face"], ["blue_sunglasses", "under"],["blue_sunglasses", "under"], ["smoky_makeup", "upper"],["smoky_makeup", "upper"],["smoky_makeup", "upper"]]))
+print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))
 # print(solution([["crow_mask", "coat"], ["blue_sunglasses", "upper"],["smoky_makeup", "under"],["smoky_makeup", "face"], ["smoky_makeup", "face"]]))
 # [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	5
 # [["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]	3
