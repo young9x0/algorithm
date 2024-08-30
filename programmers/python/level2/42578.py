ad67779c5bc833f286a62c545b3b54d03922adfc
ju0jung <maliethy@gmail.com>
2024-08-30 16:06:05 +0900

    의상 정확성 다른 풀이2

--- programmers/python/level2/42578.py
+++ programmers/python/level2/42578.py
@@ -1,12 +1,21 @@
 def solution(clothes):
-  from collections import Counter
-  from functools import reduce
-  cnt=Counter([kind for name, kind in clothes])
-  print(cnt.values())
-  return reduce(lambda x,y:x*(y+1), cnt.values(),1)-1
+  c_type={}
+
+  for cloth,type in clothes:
+    if type not in c_type:
+      c_type[type] = 2
+    else:
+      c_type[type]+=1
+  print('c_type',c_type)
+  cnt=1
+  for num in c_type.values():
+    print('num',num)
+    cnt *= num
+
+  return cnt-1
 
 # print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
-print(solution([["crow_mask", "face"],["crow_mask", "face"],["crow_mask", "face"],["crow_mask", "face"], ["blue_sunglasses", "under"],["blue_sunglasses", "under"], ["smoky_makeup", "upper"],["smoky_makeup", "upper"],["smoky_makeup", "upper"]]))
+print(solution([["crow_mask", "face"],["crow_mask", "face"],["crow_mask", "face"],["crow_mask", "face"], ["blue_sunglasses", "under"],["blue_sunglasses", "under"], ["smoky_makeup", "upper"],["smoky_makeup", "upper"],["smoky_makeup", "upper"],["smoky_makeup", "head"]]))
 # print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))
 # print(solution([["crow_mask", "coat"], ["blue_sunglasses", "upper"],["smoky_makeup", "under"],["smoky_makeup", "face"], ["smoky_makeup", "face"]]))
 # [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]	5
