5e254b20960773573da3f66e53cb6bf843d30d4e
ju0jung <maliethy@gmail.com>
2024-09-03 14:49:51 +0900

    H-Index 정확성: 18.8

--- programmers/python/level2/42747.py
+++ programmers/python/level2/42747.py
@@ -1,17 +1,21 @@
 def solution(citations):
+  citations.sort()
   c_dict={key:0 for key in citations}
   for target in citations:
     for other in citations:
       if target <= other:
         c_dict[target] += 1
 
-  # print(c_dict)
-  answer = 0
-  for key,val in c_dict.items():
-    if key == val and answer < val:
-      answer = val
+  answer = c_dict[citations[0]]
+  for key,cnt in c_dict.items():
+    if key <= cnt and len(citations)-cnt <= key:
+      answer = key
 
   return answer
 
 print(solution([3, 0, 6, 1, 5]))
+# print(solution([0]))
+# print(solution([1]))
+# print(solution([10000]))
+# print(solution([88,89]))
 # [3, 0, 6, 1, 5]	3	3
