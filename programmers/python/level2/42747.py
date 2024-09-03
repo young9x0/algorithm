073534c4a7a31b34295d224044386733e535b0fb
ju0jung <maliethy@gmail.com>
2024-09-03 15:49:58 +0900

    H-Index 다른 풀이3

--- programmers/python/level2/42747.py
+++ programmers/python/level2/42747.py
@@ -1,10 +1,11 @@
+# H-Index란: https://ssuamje.tistory.com/47
 def solution(citations):
-  citations.sort()
-  c_len=len(citations)
+  sorted_citations= sorted(citations, reverse=True)
+  c_len=len(sorted_citations)
   for idx in range(c_len):
-    if citations[idx] >= c_len-idx:
-      return c_len-idx
-  return 0
+    if sorted_citations[idx] <= idx:
+      return idx
+  return c_len
 
 # print(solution([3, 0, 6, 1, 5]))
 # print(solution([0]))
@@ -12,4 +13,5 @@ def solution(citations):
 # print(solution([10000]))
 # print(solution([88,89]))
 print(solution([46, 328, 8344, 164, 1]))
+# print(solution([0,1,2,3,3,3,3,3,3,4,10,20,30,40]))
 # [3, 0, 6, 1, 5]	3	3
