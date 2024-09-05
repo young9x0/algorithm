451ceb853d054008c3b142389d5834071d466a53
ju0jung <maliethy@gmail.com>
2024-09-05 23:25:27 +0900

    [1차] 캐시 정확성: 100

--- programmers/python/level2/17680.py
+++ programmers/python/level2/17680.py
@@ -3,28 +3,29 @@ def solution(cacheSize, cities):
   answer = 0
   cache=deque()
   for city in cities:
+    city = city.lower()
     # print('-'*50)
     # print('city',city)
     # print('cache',cache)
     # print('answer',answer)
-    city = city.lower()
     if city in cache:
       answer += 1
       cache.remove(city)
       cache.appendleft(city)
     else:
       answer += 5
-      cache.append(city)
+      cache.appendleft(city)
       if len(cache) > cacheSize:
-        cache.popleft()
+        cache.pop()
   return answer
 
-# print(solution(3,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
-# print(solution(3,	["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
-# print(solution(2,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	))
-# print(solution(5,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	))
+print(solution(3,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(3,	["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]))
+print(solution(2,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	))
+print(solution(5,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	))
 print(solution(2,	["Jeju", "Pangyo", "NewYork", "newyork"]	))
-# print(solution(0,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+print(solution(0,	["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]))
+# print(solution(3, ["A", "B", "C", "A", "D", "G", "A"] ))#27
 # 3	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]	50
 # 3	["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]	21
 # 2	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	60
