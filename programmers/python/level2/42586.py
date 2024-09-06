24b3416d0c493ecad49206ad55cd4a5815127fe0
ju0jung <maliethy@gmail.com>
2024-09-06 21:44:25 +0900

    기능개발 다른 풀이

--- programmers/python/level2/42586.py
+++ programmers/python/level2/42586.py
@@ -1,31 +1,19 @@
 import math
 def solution(progresses, speeds):
-  date_list=[math.ceil((100-progress)/speeds[idx]) for idx,progress in enumerate(progresses)]
-  # print(date_list)
-  answer = []
-  idx=0
-  while len(date_list)>idx:
-    cnt=1
-    for next in range(idx+1,len(date_list)):
-      # print('date',idx, date_list[idx])
-      # print('next',date_list[next])
-      if date_list[idx] >= date_list[next]:
-        # print('add')
-        cnt+=1
-      else:
-        # print('break')
-        answer.append(cnt)
-        idx+=cnt
-        break
+  Q=[]
+  for p, s in zip(progresses,speeds):
+    remain=-((p-100)//s)
+    print('remain',remain)
+    print('Q',Q)
+    if len(Q) == 0 or Q[-1][0] < remain:
+      Q.append([remain,1])
     else:
-      # print('cnt',cnt)
-      answer.append(cnt)
-      idx+=cnt
-    # print('answer',answer)
-  return answer
+      Q[-1][1]+=1
+  print('Q',Q)
+  return [q[1] for q in Q]
 
-print(solution([90, 90, 90], [1, 5, 4]))
-# print(solution([93, 30, 55],	[1, 30, 5]))
+# print(solution([90, 90, 90], [1, 5, 4]))
+print(solution([93, 30, 55],	[1, 30, 5]))
 # print(solution([95, 90, 99, 99, 80, 99],	[1, 1, 1, 1, 1, 1]))
 # [93, 30, 55]	[1, 30, 5]	[2, 1]
 # [95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
