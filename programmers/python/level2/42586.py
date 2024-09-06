83fdec99b63db7d3fefa7c46766e0e0725be980b
ju0jung <maliethy@gmail.com>
2024-09-06 21:51:55 +0900

    기능개발 다른 풀이2

--- programmers/python/level2/42586.py
+++ programmers/python/level2/42586.py
@@ -1,17 +1,25 @@
 import math
 def solution(progresses, speeds):
-  Q=[]
-  for p, s in zip(progresses,speeds):
-    remain=-((p-100)//s)
-    print('remain',remain)
-    print('Q',Q)
-    if len(Q) == 0 or Q[-1][0] < remain:
-      Q.append([remain,1])
+  print('p',progresses)
+  print('s',speeds)
+  answer=[]
+  time=0
+  cnt=0
+  while len(progresses)>0:
+    print('time',time)
+    if(progresses[0] + time*speeds[0]) >= 100:
+      progresses.pop(0)
+      speeds.pop(0)
+      cnt+=1
+      print('finish')
     else:
-      Q[-1][1]+=1
-  print('Q',Q)
-  return [q[1] for q in Q]
-
+      print('cnt',cnt)
+      if cnt>0:
+        answer.append(cnt)
+        cnt=0
+      time+=1
+  answer.append(cnt)
+  return answer
 # print(solution([90, 90, 90], [1, 5, 4]))
 print(solution([93, 30, 55],	[1, 30, 5]))
 # print(solution([95, 90, 99, 99, 80, 99],	[1, 1, 1, 1, 1, 1]))
