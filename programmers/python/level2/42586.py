15fb6eb53e3064a8f4c280b3058b9d08b02e05c2
ju0jung <maliethy@gmail.com>
2024-09-06 21:56:49 +0900

    기능개발 다른 풀이3

--- programmers/python/level2/42586.py
+++ programmers/python/level2/42586.py
@@ -1,25 +1,21 @@
-import math
+from math import ceil
 def solution(progresses, speeds):
-  print('p',progresses)
-  print('s',speeds)
-  answer=[]
-  time=0
-  cnt=0
-  while len(progresses)>0:
-    print('time',time)
-    if(progresses[0] + time*speeds[0]) >= 100:
-      progresses.pop(0)
-      speeds.pop(0)
-      cnt+=1
-      print('finish')
-    else:
-      print('cnt',cnt)
-      if cnt>0:
-        answer.append(cnt)
-        cnt=0
-      time+=1
-  answer.append(cnt)
-  return answer
+  daysLeft=list(map(lambda x:ceil((100-progresses[x]) / speeds[x]), range(len(progresses))))
+  print(daysLeft)
+  cnt=1
+  retList=[]
+
+  for idx in range(len(daysLeft)):
+    try:
+      if daysLeft[idx] < daysLeft[idx+1]:
+        retList.append(cnt)
+        cnt=1
+      else:
+        daysLeft[idx+1]=daysLeft[idx]
+        cnt+=1
+    except IndexError:
+      retList.append(cnt)
+  return retList
 # print(solution([90, 90, 90], [1, 5, 4]))
 print(solution([93, 30, 55],	[1, 30, 5]))
 # print(solution([95, 90, 99, 99, 80, 99],	[1, 1, 1, 1, 1, 1]))
