19c1a2ce5dd0d3074673641fa72fc772a8e8e905
ju0jung <maliethy@gmail.com>
2024-09-08 17:39:01 +0900

    튜플 다른 풀이2

--- programmers/python/level2/64065.py
+++ programmers/python/level2/64065.py
@@ -1,16 +1,23 @@
 def solution(s):
-  only_digit=re.findall('\d+',s)
-  print(only_digit)
-  cnt=Counter(only_digit)
-  print(cnt)
+  answer=[]
+  s1=s.lstrip('{').rstrip('}').split('},{')
+  print(s1)
+  new_s=[]
+  for i in s1:
+    new_s.append(i.split(','))
+  print(new_s)
+  new_s.sort(key=len)
 
-  return list(map(int, [key for key,val in sorted(cnt.items(), key=lambda x:x[1], reverse=True)]))
+  for i in new_s:
+    for j in range(len(i)):
+      if int(i[j]) not in answer:
+        answer.append(int(i[j]))
 
-import re
-from collections import Counter
+  return answer
 
-print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
-# print(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"))
+
+# print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
+print(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"))
 # print(solution("{{20,111},{111}}"))
 # print(solution("{{123}}"))
 # print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
