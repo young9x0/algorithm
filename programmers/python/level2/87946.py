1086ad8fec761e3151d14f74a9d16641167e50b7
ju0jung <maliethy@gmail.com>
2024-09-09 22:13:03 +0900

    피로도 다른 풀이

--- programmers/python/level2/87946.py
+++ programmers/python/level2/87946.py
@@ -1,46 +1,32 @@
+answer=0
+n=0
+visited=[]
+d_list = []
+
+def dfs(k,cnt):
+  print('---k',k)
+  print('cnt',cnt)
+  print('visited',visited)
+  global answer, d_list
+  if cnt > answer:
+    print('change')
+    answer = cnt
+  for idx in range(n):
+    print('idx',idx)
+    print('old visited',visited)
+    if k >= d_list[idx][0] and not visited[idx]:
+      visited[idx] = 1
+      dfs(k-d_list[idx][1], cnt+1)
+      visited[idx] = 0
+
 def solution(k, dungeons):
-  idx_list = [idx for idx in range(len(dungeons))]
-  def shuffle_idx(temp):
-    # print('temp',temp)
-    if len(temp) == len(dungeons):
-      result.append(temp)
-      return
+  global n, visited, d_list
+  n=len(dungeons)
+  visited = [0]*n
+  d_list = dungeons
+  dfs(k,0)
 
-    for idx in idx_list:
-      if idx not in temp:
-        new = [i for i in temp]
-        new.append(idx)
-        # print('new',new)
-        shuffle_idx(new)
-
-  result=[]
-  for idx in idx_list:
-    # print('-'*50)
-    # print('idx',idx)
-    shuffle_idx([idx])
-    # print('result',result)
-
-
-  max_cnt = 0
-  for temp in result:
-    answer = 0
-    user_degree=k
-    # print('temp',temp)
-    for idx in temp:
-      # print('dungeon',dungeons[idx])
-      min_degree, minus_degree = dungeons[idx]
-      # print('user_degree',user_degree)
-      if user_degree >= min_degree:
-        user_degree -= minus_degree
-        answer += 1
-      else:
-        break
-
-    if answer > max_cnt:
-      max_cnt = answer
-      # print('max',max_cnt)
-
-  return max_cnt
+  return answer
 
 print(solution(80,	[[80,20],[50,40],[30,10]]))
 # 3
