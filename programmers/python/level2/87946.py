8279a5a119bbebbb1e98881465738a36aa921eef
ju0jung <maliethy@gmail.com>
2024-09-09 22:18:43 +0900

    피로도 다른 풀이2

--- programmers/python/level2/87946.py
+++ programmers/python/level2/87946.py
@@ -1,32 +1,20 @@
-answer=0
-n=0
-visited=[]
-d_list = []
-
-def dfs(k,cnt):
-  print('---k',k)
-  print('cnt',cnt)
-  print('visited',visited)
-  global answer, d_list
-  if cnt > answer:
-    print('change')
-    answer = cnt
-  for idx in range(n):
-    print('idx',idx)
-    print('old visited',visited)
-    if k >= d_list[idx][0] and not visited[idx]:
-      visited[idx] = 1
-      dfs(k-d_list[idx][1], cnt+1)
-      visited[idx] = 0
+from itertools import permutations
 
 def solution(k, dungeons):
-  global n, visited, d_list
-  n=len(dungeons)
-  visited = [0]*n
-  d_list = dungeons
-  dfs(k,0)
+  visited=0
+  for dungeon_p in permutations(dungeons):
+    print(dungeon_p)
+    have, cnt=k,0
+    for need, cost in dungeon_p:
+      print(need,cost)
+      if have>=need:
+        have-=cost
+        cnt+=1
+      else:
+        break
+    visited = max(visited,cnt)
 
-  return answer
+  return visited
 
 print(solution(80,	[[80,20],[50,40],[30,10]]))
 # 3
