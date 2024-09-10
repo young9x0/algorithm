28d5979133eb0bd74e9cc62f1ea1ac1a8d5e1750
ju0jung <maliethy@gmail.com>
2024-09-10 18:08:25 +0900

    프로세스 다른 풀이3

--- programmers/python/level2/42587.py
+++ programmers/python/level2/42587.py
@@ -1,21 +1,26 @@
 def solution(priorities, location):
   answer=0
-  p_max = max(priorities)
+  search,c=sorted(priorities,reverse=True),0
+  print('search',search)
   while True:
-    cur= priorities.pop(0)
-    if p_max == cur:
-      answer+=1
-      if location == 0:
-        break
-      else:
-        location-=1
-      p_max=max(priorities)
+    for idx, priority in enumerate(priorities):
+      s=search[c]
+      print('-'*50)
+      print('idx, priority',idx, priority)
+      print('c',c)
+      print('s',s)
+      print('priority',priority)
+      if priority == s:
+        c+=1
+        answer+=1
+        if idx == location:
+          print('find location')
+          break
     else:
-      priorities.append(cur)
-      if location == 0:
-        location = len(priorities)-1
-      else:
-        location-=1
+      print('else')
+      continue
+    print('break')
+    break
   return answer
 # print(solution([2, 1, 3, 2],	2))
 print(solution([1, 1, 9, 1, 1, 1],	0	))
