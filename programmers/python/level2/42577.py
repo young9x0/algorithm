59c9458cd8f15754a01075e9fa050641570ae1c3
ju0jung <maliethy@gmail.com>
2024-09-11 14:55:09 +0900

    전화번호 목록 정확성: 70.8 효율성: 8.3 합계: 79.2 / 100.0

--- programmers/python/level2/42577.py
+++ programmers/python/level2/42577.py
@@ -3,13 +3,13 @@ def solution(phone_book):
   p_len= len(phone_book)
   for pre_idx in range(p_len):
     for check_idx in range(pre_idx+1,p_len):
-      if phone_book[pre_idx] in phone_book[check_idx]:
+      if phone_book[pre_idx] in phone_book[check_idx][:len(phone_book[pre_idx])]:
         return False
   return answer
 
-# print(solution(["119", "97674223", "1195524421"]))
+print(solution(["119", "97674223", "1195524421"]))
 # print(solution(["123","456","789"]))
-print(solution(["12","123","1235","567","88"]))
+# print(solution(["12","123","1235","567","88"]))
 # ["119", "97674223", "1195524421"]	false
 # ["123","456","789"]	true
 # ["12","123","1235","567","88"]	false
