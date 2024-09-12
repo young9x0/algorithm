8c83030433411154a52c67a53681d87b986a7aec
ju0jung <maliethy@gmail.com>
2024-09-12 11:09:02 +0900

    전화번호 목록 100.0

--- programmers/python/level2/42577.py
+++ programmers/python/level2/42577.py
@@ -1,17 +1,20 @@
 def solution(phone_book):
   phone_book.sort()
+  # print(phone_book)
   p_len= len(phone_book)
-  for pre_idx in range(p_len):
-    for check_idx in range(pre_idx+1,p_len):
+
+  for pre_idx in range(p_len-1):
       pre_len= len(phone_book[pre_idx])
-      check_len= len(phone_book[check_idx])
-      if pre_len < check_len and phone_book[pre_idx] in phone_book[check_idx][:len(phone_book[pre_idx])]:
+      if phone_book[pre_idx] == phone_book[pre_idx+1][:pre_len]:
         return False
+
   return True
 
-print(solution(["119", "97674223", "1195524421"]))
+# print(solution(["119", "97674223", "1195524421"]))
 # print(solution(["123","456","789"]))
-# print(solution(["12","123","1235","567","88"]))
+print(solution(["12","123","1235","567","88"]))
+# print(solution(["119","1000","1190","567","88"]))
+# print(solution(["118","12","180","567","88"]))
 # ["119", "97674223", "1195524421"]	false
 # ["123","456","789"]	true
 # ["12","123","1235","567","88"]	false
