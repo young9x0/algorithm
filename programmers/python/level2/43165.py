c5c11009a0fe470c1888700c5ff1e395a404f7c6
ju0jung <maliethy@gmail.com>
2024-09-21 20:18:21 +0900

    타겟 넘버 - 정확성 0 (fine runtime error case)

--- programmers/python/level2/43165.py
+++ programmers/python/level2/43165.py
@@ -1,56 +1,77 @@
 def solution(numbers, target):
   answer = 0
-  hist={idx:[1,1] for idx in range(len(numbers))}
-  def dfs(root_idx,node_idx, temp):
+  n_len=len(numbers)
+  hist={idx:{'-':False,'+':False} for idx in range(n_len)}
+
+  def dfs(node_idx, temp, is_reverse):
     nonlocal answer
-    nonlocal hist
-    # print('answer',answer)
-    # print('hist',hist)
-    # print('root_idx',root_idx)
-    # print('node_idx',node_idx)
-    # print('temp',temp)
-    if root_idx == len(numbers):
-      # print('answer root_idx',root_idx)
-      return answer
-    if node_idx == len(numbers):
-      # print('last node_idx',node_idx)
-      if temp == target:
-        answer+=1
-      temp=sum(numbers[:root_idx+1])
-      return dfs(root_idx+1,root_idx+1,temp)
+    print('-'*50)
+    print('node_idx',node_idx)
+    print('temp',temp)
+    print('answer',answer)
+    print('hist',hist)
+    print('is_reverse',is_reverse)
 
-    # print('node_idx',node_idx, numbers[node_idx], hist[node_idx][0], (temp-numbers[node_idx]) + sum(numbers[node_idx+1:]))
-    if hist[node_idx][0] == 1:
-      if (temp-numbers[node_idx]) + sum(numbers[node_idx+1:])==target:
-        # print('-:same as target')
-        answer+=1
-        if node_idx == 0 or (node_idx > 0 and sum(hist[node_idx-1]) == 0):
-          hist[node_idx][0] = 0
-        temp=sum(numbers[:root_idx+1])
-        dfs(root_idx,root_idx, temp)
-      elif (temp-numbers[node_idx]) + sum(numbers[node_idx+1:])>target:
-        # print('-:more than target')
-        temp-=numbers[node_idx]
-        if node_idx == 0 or (node_idx > 0 and sum(hist[node_idx-1]) == 0):
-          hist[node_idx][0] = 0
-        dfs(root_idx,node_idx+1, temp)
+    if is_reverse:
+      if node_idx == 0 and hist[node_idx]['-']==True and hist[node_idx]['+']==True:
+        print('visited all and finally arrive root', answer)
+        return answer
+
+      if hist[node_idx]['+']==True:
+        print('go to prev')
+        hist[node_idx]['-']=False
+        hist[node_idx]['+']=False
+        temp-=(numbers[node_idx])
+        dfs(node_idx-1, temp,True)
       else:
-        # print('+:block path')
-        if node_idx == 0 or (node_idx > 0 and sum(hist[node_idx-1]) == 0):
-          hist[node_idx][0]=0
-          hist[node_idx][1]=0
-        temp+=numbers[node_idx]
-        dfs(root_idx,node_idx+1, temp)
-    else:
-      # print('visited')
-      hist[node_idx][1]=0
-      temp=sum(numbers[:root_idx+1])
-      dfs(root_idx+1,node_idx+1, temp)
+        print('go to plus path')
+        temp+=((numbers[node_idx])*2)
+        hist[node_idx]['+']=True
+        dfs(node_idx+1, temp,False)
 
-  dfs(0, 0,0)
+    else:
+      next_idx=node_idx+1
+      if hist[node_idx]['-'] == False:
+        minus_path_value=temp + (-numbers[node_idx]) + sum(numbers[node_idx+1:])
+        hist[node_idx]['-'] = True
+        print('minus_path_value',minus_path_value,'+temp',temp, '-cur',(numbers[node_idx]), '+rest',sum(numbers[node_idx+1:]))
+        if minus_path_value>target and node_idx<n_len-1:
+          print('need check next')
+          temp-=(numbers[node_idx])
+          dfs(next_idx, temp, False)
+          return
+        else:
+          if minus_path_value==target:
+            print('minus path is target path!')
+            answer+=1
+
+      print("cur node minus path checked, go to plus path")
+      temp+=numbers[node_idx]
+      hist[node_idx]['+']=True
+      if node_idx == n_len-1:
+        print('leaf node')
+        if temp==target:
+          print('plus path is target path!')
+          answer+=1
+        print("return to prev")
+        hist[node_idx]['-']=False
+        hist[node_idx]['+']=False
+        temp-=numbers[node_idx]
+        dfs(node_idx-1,temp, True)
+        return
+
+      dfs(next_idx,temp, False)
+
+  dfs(0, 0, False)
+  # node_idx: 확인해 볼 cur idx 담아서 보내기
+  # temp: cur 아직 더하지 않은 값, cur 전까지 값
+  # is_reverse: go to prev node
   return answer
 
-print(solution([1, 1, 1, 1, 1],	3))
+#runtime error case
+print(solution([1,2,3,4,5,6,7,8,9,10],	10))
+
+# print(solution([1, 1, 1, 1, 1],	3))
 # print(solution([4, 1, 2, 1],	4))
 # [1, 1, 1, 1, 1]	3	5
 # [4, 1, 2, 1]	4	2
