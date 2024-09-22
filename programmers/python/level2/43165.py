a6abe806bbad74296b99fe35d52536199b21aeaf
recursive-0 <maliethy@gmail.com>
2024-09-22 20:16:14 +0900

    타겟 넘버 다른 풀이2

--- programmers/python/level2/43165.py
+++ programmers/python/level2/43165.py
@@ -1,43 +1,19 @@
+from itertools import product
 def solution(numbers, target):
-  answer = 0
-  n_len=len(numbers)
-  def dfs(temp, idx):
-    nonlocal answer
-    # print('-'*50)
-    # print('temp',temp)
-    # print('idx',idx)
-    # print('answer',answer)
-    # print('test',test)
-
-    if len(temp) == n_len:
-      # print('length')
-      if sum(temp) == target:
-        # print('target')
-        answer+=1
-        return
-    else:
-      next_idx=idx+1
-      new1 = [num for num in temp]
-      new2 = [num for num in temp]
-      # print('append', numbers[next_idx])
-
-      new1.append(-numbers[next_idx])
-      new2.append(numbers[next_idx])
-
-      dfs(new1, next_idx)
-      dfs(new2, next_idx)
-
-  dfs([-numbers[0]],0)
-  dfs([numbers[0]],0)
-  return answer
-
-#runtime error case
-print(solution([1,2,3,4,5,6,7,8,9,10],	10))
-# print(solution([1,2,2,1,1,3,2,3,2,2],	2))
-# print(solution([1,2,1,2,1,3,1,2,1,2],	3))
-
+  numbers=[1,2,3,4]
+  num_pair=[(num,-num) for num in numbers]
+  product_pair=product(*num_pair)
+  # list(product_pair)
+  # list(product_pair) 를 하고 나면 sum_res이 빈배열이 되어버리는 이유는?
+  # 파이썬 코드에서 product_pair는 itertools.product 객체로, 이 객체는 한 번만 순회 가능한 이터레이터입니다.
+  # 그래서 list(product_pair)를 호출하면 이터레이터가 모두 순회되어 소비되고, 다시 사용하려고 할 때는 빈 이터레이터가 됩니다.
+  # 따라서 그 이후 map(sum, product_pair)를 실행하면 이미 이터레이터가 소진되어 아무 값도 없고, 빈 배열이 나옵니다.
+  # print(product_pair)
+  sum_res=list(map(sum,product_pair))
+  print(sum_res)
+  return sum_res.count(target)
 
 # print(solution([1, 1, 1, 1, 1],	3))
-# print(solution([4, 1, 2, 1],	4))
+print(solution([4, 1, 2, 1],	4))
 # [1, 1, 1, 1, 1]	3	5
 # [4, 1, 2, 1]	4	2
