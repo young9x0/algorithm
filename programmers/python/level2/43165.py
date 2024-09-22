f6fbd88599ef037d717d890fad81d0113f2b283c
ju0jung <maliethy@gmail.com>
2024-09-22 18:30:55 +0900

    타겟 넘버 - 정확성 100 (+AI 피드백)

--- programmers/python/level2/43165.py
+++ programmers/python/level2/43165.py
@@ -1,75 +1,41 @@
 def solution(numbers, target):
   answer = 0
   n_len=len(numbers)
-  hist={idx:{'-':False,'+':False} for idx in range(n_len)}
-
-  def dfs(node_idx, temp, is_reverse):
+  def dfs(temp, idx):
     nonlocal answer
-    print('-'*50)
-    print('node_idx',node_idx)
-    print('temp',temp)
-    print('answer',answer)
-    print('hist',hist)
-    print('is_reverse',is_reverse)
+    # print('-'*50)
+    # print('temp',temp)
+    # print('idx',idx)
+    # print('answer',answer)
+    # print('test',test)
 
-    if is_reverse:
-      if node_idx == 0 and hist[node_idx]['-']==True and hist[node_idx]['+']==True:
-        print('visited all and finally arrive root', answer)
-        return answer
-
-      if hist[node_idx]['+']==True:
-        print('go to prev')
-        hist[node_idx]['-']=False
-        hist[node_idx]['+']=False
-        temp-=(numbers[node_idx])
-        dfs(node_idx-1, temp,True)
-      else:
-        print('go to plus path')
-        temp+=((numbers[node_idx])*2)
-        hist[node_idx]['+']=True
-        dfs(node_idx+1, temp,False)
-
-    else:
-      next_idx=node_idx+1
-      if hist[node_idx]['-'] == False:
-        minus_path_value=temp + (-numbers[node_idx]) + sum(numbers[node_idx+1:])
-        hist[node_idx]['-'] = True
-        print('minus_path_value',minus_path_value,'+temp',temp, '-cur',(numbers[node_idx]), '+rest',sum(numbers[node_idx+1:]))
-        if minus_path_value>target and node_idx<n_len-1:
-          print('need check next')
-          temp-=(numbers[node_idx])
-          dfs(next_idx, temp, False)
-          return
-        else:
-          if minus_path_value==target:
-            print('minus path is target path!')
-            answer+=1
-
-      print("cur node minus path checked, go to plus path")
-      temp+=numbers[node_idx]
-      hist[node_idx]['+']=True
-      if node_idx == n_len-1:
-        print('leaf node')
-        if temp==target:
-          print('plus path is target path!')
-          answer+=1
-        print("return to prev")
-        hist[node_idx]['-']=False
-        hist[node_idx]['+']=False
-        temp-=numbers[node_idx]
-        dfs(node_idx-1,temp, True)
+    if len(temp) == n_len:
+      # print('length')
+      if sum(temp) == target:
+        # print('target')
+        answer+=1
         return
+    else:
+      next_idx=idx+1
+      new1 = [num for num in temp]
+      new2 = [num for num in temp]
+      # print('append', numbers[next_idx])
 
-      dfs(next_idx,temp, False)
+      new1.append(-numbers[next_idx])
+      new2.append(numbers[next_idx])
 
-  dfs(0, 0, False)
-  # node_idx: 확인해 볼 cur idx 담아서 보내기
-  # temp: cur 아직 더하지 않은 값, cur 전까지 값
-  # is_reverse: go to prev node
+      dfs(new1, next_idx)
+      dfs(new2, next_idx)
+
+  dfs([-numbers[0]],0)
+  dfs([numbers[0]],0)
   return answer
 
 #runtime error case
 print(solution([1,2,3,4,5,6,7,8,9,10],	10))
+# print(solution([1,2,2,1,1,3,2,3,2,2],	2))
+# print(solution([1,2,1,2,1,3,1,2,1,2],	3))
+
 
 # print(solution([1, 1, 1, 1, 1],	3))
 # print(solution([4, 1, 2, 1],	4))
