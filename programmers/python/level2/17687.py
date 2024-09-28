8088762007f4fd9906cd68d3e0a082284b683fb2
recursive-0 <maliethy@gmail.com>
2024-09-28 16:56:27 +0900

    [3차] n진수 게임 다른 풀이

--- programmers/python/level2/17687.py
+++ programmers/python/level2/17687.py
@@ -1,42 +1,35 @@
-from collections import deque
+big=["A","B","C","D","E","F"]
 def solution(n, t, m, p):
   #진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p
   answer = ''
-  # print(chr(10+55))
-  # #A
-  # print(chr(15+55))
-  # #F
+  a="0"
+  i=0
+  while True:
+    print('-'*50)
+    print('a',a)
+    if len(a)>=t*m:
+      break
+    b=""
+    j=i
+    print('j',j)
+    while (j):
+      print('in while: j',j)
+      if j%n>9:
+        b=big[j%n-10]+b
+      else:
+       b=str(j%n)+b
+      j=j//n
+    print('b',b)
+    a+=b
+    i+=1
 
-  def  conversion_representation(target,to,result):
-    # print('-'*50)
-    # print('target',target)
-    # print('to',to)
-    # print('result',result)
-    if target//to == 0:
-      result.appendleft(target%to)
-      return result
-    else:
-      result.appendleft(target%to)
-      return  conversion_representation(target//to,to,result)
-
-  # print( conversion_representation(100,8,deque()))
-  num_list_len=t*m
-  temp=[]
-  for num in range(num_list_len):
-    temp+=conversion_representation(num,n,deque())
-
-  # print('temp',temp)
-  for idx in range(p-1,num_list_len,m):
-    if temp[idx] > 9 and temp[idx] < 16:
-      answer += chr(55+temp[idx])
-    else:
-      answer+=str(temp[idx])
-
+  print('a[p-1::m]',a[p-1::m])
+  answer=a[p-1::m][:t]
   return answer
 
 # print(solution(2,	4,	2,	1))
-# print(solution( 16,	16,	2,	1))
-print(solution(  16,	16,	2,	2	))
+print(solution( 16,	16,	2,	1))
+# print(solution(  16,	16,	2,	2	))
 # 2	4	2	1	"0111"
 # 16	16	2	1	"02468ACE11111111"
 # 16	16	2	2	"13579BDF01234567"
