63def9a67ddb2f87229277fbcd4bf6038c685813
recursive-0 <maliethy@gmail.com>
2024-12-17 16:29:56 +0900

    방문 길이 다른 풀이2

--- programmers/python/level2/49994.py
+++ programmers/python/level2/49994.py
@@ -1,22 +1,33 @@
 def solution(dirs):
-  s=set()
-  d={'U':(0,1), 'D':(0,-1), 'R':(1,0), 'L':(-1,0)}
-  x,y=0,0
-  for dir in dirs:
-    nx, ny = x+d[dir][0] + y+d[dir][1]
-    if -5 <= nx <= 5 and -5 <= ny <= 5:
-      s.add((x,y,nx,ny))
-      s.add((nx,ny,x,y))
-      x,y=nx,ny
+  point = [0,0]
+  answer = set()
 
-  return len(s)//2
+  for _dirs in dirs:
+    print(_dirs)
+    if _dirs == 'U' and point[1]!=5:
+      point[1]+=1
+      nn = (point[0], point[1]-.5)
+    elif _dirs == 'D' and point[1]!=-5:
+      point[1]+=-1
+      nn = (point[0], point[1]+.5)
+    elif _dirs == 'R' and point[0]!=5:
+      point[0]+=1
+      nn = (point[0]-.5, point[1])
+    elif _dirs == 'L' and point[0]!=-5:
+      point[0]+=-1
+      nn = (point[0]+.5, point[1])
+    else: pass
+    print(nn)
+    answer.add(nn)
 
-# print(solution("ULURRDLLU"))
+  return len(answer)
+
+print(solution("ULURRDLLU"))
 # print(solution("LULLLLLLU"))
 # print(solution("ULDRULDRURDL"))
 # print(solution("UUUUUURDLUU"))
 # print(solution("DDDDDDLURDD"))
-print(solution("RLRL"))
+# print(solution("RLRL"))
 # "ULURRDLLU"	7
 # "LULLLLLLU"	7
 # "ULDRULDRURDL"	7
