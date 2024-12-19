0ad2aff443aff9cc3f7158a6d0f14ab2ff06cfe1
recursive-0 <maliethy@gmail.com>
2024-12-19 10:06:26 +0900

    [3차] 압축 다른 풀이1

--- programmers/python/level2/17684.py
+++ programmers/python/level2/17684.py
@@ -1,43 +1,18 @@
 def solution(msg):
   answer = []
-  init={chr(idx+64):idx for idx in range(1,27)}
-  # print(chr(65), ord('A'))
-  idx=0
-  while idx-1 < len(msg):
-    # print('---idx',idx)
-    # print('---w',msg[idx])
-    if idx+1 == len(msg):
-      # print("last letter")
-      answer.append(init[msg[idx]])
-      break
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  init={k:v for (k,v) in zip(alphabet, range(1,27))}
 
-    gap=1
-    # print('while msg[idx:next]', msg[idx:idx+gap+1])
-    # update gap
-    # idx+gap+1: default is 2
-    # check w exist in init
-    while msg[idx:idx+gap+1] in init.keys():
-      if idx+gap+1 == len(msg)+1:
-        # print("last letter with gap")
+  while True:
+    if msg in init:
+      answer.append(init[msg])
+      break
+    for i in range(1,len(msg)+1):
+      if msg[0:i] not in init:
+        answer.append(init[msg[0:i-1]])
+        init[msg[0:i]] = len(init)+1
+        msg = msg[i-1:]
         break
-      # print('add to gap!')
-      gap+=1
-
-    # print('gap', gap)
-    next = idx+gap+1
-    if next == len(msg)+1:
-      answer.append(init[msg[idx:next]])
-      break
-
-    # add w's int
-    answer.append(init[msg[idx:next-1]])
-    # print('answer', answer)
-    # print('next msg[idx:next-1]', msg[idx:next])
-    # add new key at init
-    if msg[idx:next] not in init.keys():
-      init[str(msg[idx:next])] = len(init)+1
-    # print('init',init)
-    idx+=gap
 
   return answer
 
