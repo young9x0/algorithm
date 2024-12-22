c2f1883f9546e379639639a81247b87dd3f31921
recursive-0 <maliethy@gmail.com>
2024-12-22 19:38:42 +0900

    모음사전 다른 풀이1

--- programmers/python/level2/84512.py
+++ programmers/python/level2/84512.py
@@ -1,44 +1,11 @@
 def solution(word):
   word_type="AEIOU"
-  hist=[]
-  sort_dict=[]
+  answer=0
+  length= len(word_type)
+  for i,n in enumerate(word):
+    answer += (length**(length-i) -1)/(length-1) * word_type.index(n) + 1
+  return answer
 
-  def hist_log(prev_list):
-    # print('prev', prev_list)
-
-    tmp=[]
-    for p_type in prev_list:
-      for type in word_type:
-        n_word=p_type+type
-
-        # print('n_word', n_word)
-        tmp.append(n_word)
-
-    # print('tmp',tmp)
-    hist.append(tmp)
-    # print('hist',hist)
-    if len(hist) < len(word_type):
-      hist_log(hist[-1])
-
-  tmp=[]
-  for type in word_type:
-    # print('-'*50)
-    # print('type',type)
-
-    tmp.append(type)
-  hist.append(tmp)
-  hist_log(hist[-1])
-
-  # print(hist)
-  for list in hist:
-    for el in list:
-      sort_dict.append(el)
-
-  sort_dict.sort()
-  # print(sort_dict)
-  return sort_dict.index(word)+1
-
-# print(solution("CCC"))
 # print(solution("AAAAE"))
 # print(solution("AAAE"))
 # print(solution("AAE"))
