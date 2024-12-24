2dd7349ba380a590ccfe25f300ecc9720127edb9
recursive-0 <maliethy@gmail.com>
2024-12-24 14:49:37 +0900

    게임 맵 최단거리 정확성: 16.5 효율성: 7.5 합계: 24.1 / 100.0

--- programmers/python/level2/1844.py
+++ programmers/python/level2/1844.py
@@ -1,51 +1,52 @@
 def solution(maps):
   r_len=len(maps)
   c_len=len(maps[0])
-  hist=[]
+  block_type=['LU','UL']
 
-  def move(c_x,c_y,cnt):
+  def move(c_x, c_y, cnt, prev):
     # print('-'*50)
     # print('cnt',cnt, 'x,y',c_x,c_y)
+    # print('prev',prev)
     if c_x == c_len-1 and c_y == r_len-1:
       return cnt
 
-    hist.append((c_x,c_y))
+    # hist.append((c_x,c_y))
     # print('hist',hist)
 
-    prev=(c_x,c_y)
+    cur=""
     # down
-    if  c_x+1 < c_len and maps[c_x+1][c_y] == 1 and (c_x+1,c_y) not in hist:
-      # print('down')
+    if  c_x+1 < c_len and maps[c_x+1][c_y] == 1 and prev != 'U':
       c_x+=1
+      cur='D'
 
     # right
-    elif c_y+1 < r_len and maps[c_x][c_y+1] == 1 and (c_x,c_y+1) not in hist:
-      # print('right')
+    elif c_y+1 < r_len and maps[c_x][c_y+1] == 1 and prev != 'L':
       c_y+=1
+      cur='R'
 
     # up
-    elif c_x-1 > -1 and maps[c_x-1][c_y] == 1 and (c_x-1,c_y) not in hist:
-      # print('up')
+    elif c_x-1 > -1 and maps[c_x-1][c_y] == 1 and prev != 'D':
       c_x-=1
+      cur='U'
 
     # left
-    elif c_y-1 > -1 and maps[c_x][c_y-1] == 1 and (c_x,c_y-1) not in hist:
-      # print('left')
+    elif c_y-1 > -1 and maps[c_x][c_y-1] == 1 and prev != 'R':
       c_y-=1
+      cur='L'
 
-    # print('x,y',c_x,c_y)
-    if (c_x,c_y) == prev:
+    # print('x,y',c_x,c_y, 'prev+cur',prev+cur)
+    if prev+cur in block_type:
       # print('all block!')
       return -1
     else:
       # print('next')
-      return move(c_x,c_y,cnt+1)
+      return move(c_x,c_y,cnt+1,cur)
 
 
-  return move(0,0,1)
+  return move(0,0,1,"")
 
-print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
-# print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
+# print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
+print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
 
 # [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]	11
 # [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]	-1
