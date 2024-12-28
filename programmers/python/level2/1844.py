15b313fb978f12f698674854f6e28cc52b7f7e3e
recursive-0 <maliethy@gmail.com>
2024-12-28 17:02:45 +0900

    게임 맵 최단거리 다른 풀이3

--- programmers/python/level2/1844.py
+++ programmers/python/level2/1844.py
@@ -1,23 +1,26 @@
+from collections import deque
 def solution(maps):
-  stack=[(0,0,1)]
-  dx=[0,0,-1,1]
-  dy=[1,-1,0,0]
+  n,m= len(maps), len(maps[0])
 
-  while stack:
-    x,y,d=stack.pop(0)
-    for q in range(len(dx)):
-      ny=y+dy[q]
-      nx=x+dx[q]
-      if ny<0 or nx<0 or ny>= len(maps) or nx>=len(maps[0]):
-        continue
-      else:
-        if maps[ny][nx] == 1 or maps[ny][nx]>d+1:
-          maps[ny][nx]= d+1
-          if ny == len(maps) -1 and nx == len(maps[0])-1:
-            return d+1
+  dx=[-1,1,0,0]
+  dy=[0,0,-1,1]
+  visited=[[False for _ in range(m)] for _ in range(n)]
+  queue=deque([(0,0)])
 
-          stack.append((nx,ny,d+1))
+  while queue:
+    x,y=queue.popleft()
+    if x==n-1 and y==m-1:
+      return maps[x][y]
 
+    for i in range(len(dx)):
+      nx,ny=x+dx[i], y+dy[i]
+
+      if 0<=nx< n and 0<=ny<m:
+        if not visited[nx][ny] and maps[nx][ny] !=0:
+          queue.append((nx,ny))
+          visited[nx][ny]=True
+          maps[nx][ny]=maps[x][y]+1
+          
   return -1
 
 # print(solution([[1,1,1],[1,1,1],[1,0,0],[1,1,1]]))
