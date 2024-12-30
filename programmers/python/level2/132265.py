a11223bc31c4e2d7e935144c6fd06117c2b9e49e
recursive-0 <maliethy@gmail.com>
2024-12-30 17:41:36 +0900

    롤케이크 자르기 15%

--- programmers/python/level2/132265.py
+++ programmers/python/level2/132265.py
@@ -1,30 +1,39 @@
 def solution(topping):
-  cnt = {key:0 for key in topping}
-
-  for type in topping:
-    cnt[type]+=1
-
-  k_len=len(cnt.keys())
-  t_len=len(topping)
-
-  # print('cnt',cnt)
-  # print('k_len',k_len)
+  a_len=len(topping)
+  if a_len == 1:
+    return 0
 
   def check_slice_len(idx):
     # print('idx',topping[:idx], topping[idx:])
-    cnt1 = {key:0 for key in topping[:idx]}
-    cnt2 = {key:0 for key in topping[idx:]}
-    return len(cnt1.keys()) == len(cnt2.keys())
+    left_set_len= len(set(topping[:idx]))
+    right_set_len= len(set(topping[idx:]))
+    return left_set_len == right_set_len
+
+  t_set=set(topping)
+  t_set_len=len(t_set)
+  # print('half_cnt',half_cnt)
+
+  if t_set_len == 1:
+    return a_len-1
 
   answer=0
-  if k_len%2 == 0:
-    for idx in range(1,t_len):
-      if check_slice_len(idx):
-        answer+=1
+
+  half_idx_start= topping.index(list(t_set)[t_set_len//2])+1
+  # print('half_idx_start',half_idx_start)
+  for idx in range(half_idx_start,a_len):
+    # print('idx',idx)
+    if check_slice_len(idx):
+      # print('success')
+      answer+=1
+    else:
+      break
 
   return answer
 
 # print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
-print(solution([1, 2, 3, 1, 4]))
+# print(solution([1, 2, 3, 1, 4]))
+print(solution([1, 2,1, 1, 4]))
+# print(solution([1,1,1,1,1]))
+# print(solution([9997,10000,9998,10000,9998,9997]))
 # [1, 2, 1, 3, 1, 4, 1, 2]	2
 # [1, 2, 3, 1, 4]	0
