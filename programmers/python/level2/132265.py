36442971866b97eb92d8ce5c4fd7ce360a555222
recursive-0 <maliethy@gmail.com>
2024-12-31 15:23:41 +0900

    롤케이크 자르기 25%

--- programmers/python/level2/132265.py
+++ programmers/python/level2/132265.py
@@ -3,49 +3,36 @@ def solution(topping):
   if a_len == 1:
     return 0
 
+  t_set=set(topping)
+  t_set_len=len(t_set)
+  if t_set_len == 1:
+    return a_len-1
+
   def check_slice_len(idx):
     # print('idx',topping[:idx], topping[idx:])
     left_set_len= len(set(topping[:idx]))
     right_set_len= len(set(topping[idx:]))
     return left_set_len == right_set_len
 
-  t_set=set(topping)
-  t_set_len=len(t_set)
-  if t_set_len == 1:
-    return a_len-1
-
-
-  t_set_half_idx= t_set_len//2 + 1
-  # print(t_set,'t_set_half_idx', t_set_half_idx)
-  tmp=[]
-  for type in topping:
-    if type in tmp:
-      continue
-
-    if len(tmp)<t_set_half_idx:
-      tmp.append(type)
-
-  # print('tmp',tmp)
-
-  half_idx_start= len(topping)-1
-  while topping[half_idx_start] != tmp[-1]:
-    half_idx_start-=1
-
   answer=0
-  # print('half_idx_start',half_idx_start)
-  for idx in range(half_idx_start,a_len):
-    # print('idx',idx)
-    if check_slice_len(idx):
-      # print('success')
-      answer+=1
-    elif idx> half_idx_start:
-      break
+  l_idx= a_len//2
+  while check_slice_len(l_idx):
+        # print('left success', l_idx)
+        answer+=1
+        l_idx-=1
+
+  r_idx= a_len//2+1
+  while check_slice_len(r_idx):
+        # print('right success',r_idx)
+        answer+=1
+        r_idx+=1
 
   return answer
 
+# print(solution([1, 2, 1, 3, 2, 2, 3, 4]))
 # print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
 # print(solution([1, 2, 3, 1, 4]))
-# print(solution([1, 2,1, 1, 4]))
+# print(solution([1, 2, 1, 1, 4]))
 # print(solution([1,1,1,1,1]))
 print(solution([9997,10000,9998,10000,9998,9997]))
 # [1, 2, 1, 3, 1, 4, 1, 2]	2
