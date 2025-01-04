104461b88796c4b4df52cb712bb7e8049d1f4dd3
recursive-0 <maliethy@gmail.com>
2025-01-04 17:08:33 +0900

    k진수에서 소수 개수 구하기 91.3%

--- programmers/python/level2/154539.py
+++ programmers/python/level2/154539.py
@@ -1,26 +1,36 @@
 def solution(numbers):
   answer = []
   n_len=len(numbers)
+  idx=0
+  prev=0
 
-  for idx,number in enumerate(numbers):
-    # print(idx,'number',number)
-    # print('hist',hist)
+  while idx<n_len:
+    num=numbers[idx]
+    # print(idx,'num',num)
+    if num == prev:
+      # print('prev')
+      answer.append(answer[-1])
+      idx+=1
+      continue
 
     n_idx=idx+1
-    while n_idx<n_len and number >= numbers[n_idx]:
+
+    while n_idx<n_len and num >= numbers[n_idx]:
      n_idx+=1
 
     if n_idx < n_len:
       # print('numbers[n_idx]',numbers[n_idx])
       answer.append(numbers[n_idx])
-
     else:
       answer.append(-1)
 
+    prev=num
+    idx+=1
 
   return answer
 
+print(solution([2, 3, 3,3,3,3,3,3,3,3,3,3,3,3,3, 3]))
 # print(solution([2, 3, 3, 5]))
-print(solution([9, 1, 5, 3, 6, 2]))
+# print(solution([9, 1, 5, 3, 6, 2]))
 # [2, 3, 3, 5]	[3, 5, 5, -1]
 # [9, 1, 5, 3, 6, 2]	[-1, 5, 6, 6, -1, -1]
