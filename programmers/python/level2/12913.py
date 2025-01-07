0e94c6d4fb2d3c34a36be113a8c63834d37e53d0
recursive-0 <maliethy@gmail.com>
2025-01-07 17:36:45 +0900

    땅따먹기 다른 풀이

--- programmers/python/level2/12913.py
+++ programmers/python/level2/12913.py
@@ -1,42 +1,9 @@
 def solution(land):
-  max=0
-  N= len(land)
-  # print('N',N)
+  for row in range(1,len(land)):
+    for col in range(len(land[0])):
+      land[row][col]=max(land[row-1][:col]+land[row-1][col+1:])+land[row][col]
 
-  if N == 0:
-    return 0
-  def find_max_two(list):
-    return sorted(list, key=lambda x: x[1])[-2:]
-
-  def make_two_step_max(row_idx, result):
-    # print('-'*50)
-    if row_idx <0:
-      return result
-
-    next_tmp=[(idx,col) for idx,col in enumerate(land[row_idx])]
-    next_max_two=find_max_two(next_tmp)
-    # print('next_max_two',next_max_two)
-
-    plus_tmp=[]
-    for c_key, c_val in result:
-      for n_key, n_val in next_max_two:
-        if c_key != n_key:
-          plus_tmp.append((n_key, c_val+n_val))
-
-    # print('plus_tmp',plus_tmp)
-    result=find_max_two(plus_tmp)
-    # print('result', result)
-    return make_two_step_max(row_idx-1,result)
-
-  cur_tmp=[(idx,col) for idx,col in enumerate(land[N-1])]
-  cur_max_two=find_max_two(cur_tmp)
-  # print('cur_max_two',cur_max_two)
-  last= make_two_step_max(N-2, cur_max_two)
-
-  for key, val in last:
-    if max < val:
-      max=val
-  return max
+  return max(land[-1])
 
 print(solution([[1, 1, 3, 1], [2, 3, 2, 2], [1, 4, 1, 1]]))
 # print(solution([[6, 7, 1, 2], [2, 3, 10, 8], [1, 3, 9, 4], [7, 11, 4, 9]]))
