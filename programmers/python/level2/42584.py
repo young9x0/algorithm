8a13ebffb56f267fbf89f948094f62b207e18ca3
recursive-0 <maliethy@gmail.com>
2025-01-08 16:21:51 +0900

    주식가격 다른 풀이

--- programmers/python/level2/42584.py
+++ programmers/python/level2/42584.py
@@ -1,27 +1,16 @@
 def solution(prices):
-  p_len=len(prices)
-  answer = [idx for idx in range(p_len-1,-1,-1)]
-  # print('answer',answer)
-  stack=[]
+  stack = []
+  answer = [0] * len(prices)
+  for i in range(len(prices)):
+    if stack != []:
+      while stack != [] and stack[-1][1] > prices[i]:
+        past, _ = stack.pop()
+        answer[past] = i - past
+    stack.append([i, prices[i]])
 
-  for idx in range(p_len):
-    # print('-'*50)
-    # print('idx',idx,prices[idx])
-    # print('stack',stack)
-
-    if idx == p_len-1:
-      while stack:
-        tmp=stack.pop()
-        answer[tmp[0]]=idx-tmp[0]
-      break
-    else:
-      while stack and stack[-1][1] > prices[idx]:
-        # print('stack[-1], prices[idx]', stack[-1], prices[idx])
-        tmp=stack.pop()
-        answer[tmp[0]]=idx-tmp[0]
-
-    stack.append((idx,prices[idx]))
-
+  print('stack',stack)
+  for i, s in stack:
+    answer[i] = len(prices) - 1 - i
   return answer
 
 print(solution([1, 2, 3, 2, 3]))
