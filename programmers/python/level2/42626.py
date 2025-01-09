9f917418a974f13ae3d92b0fe32f1eaeaf596a46
recursive-0 <maliethy@gmail.com>
2025-01-09 18:19:08 +0900

    더 맵게 정확성: 83.9 효율성: 0.0

--- programmers/python/level2/42626.py
+++ programmers/python/level2/42626.py
@@ -4,15 +4,32 @@ def solution(scoville, K):
 
   answer = 0
   temp=sorted(scoville)
-  while temp[0] < K and len(temp)>1:
-    # print('temp',temp)
-    temp=[mix_food(temp[0],temp[1])] + temp[2:]
-    # print('new temp',temp)
-    answer+=1
-    temp=sorted(temp)
 
+  while len(temp)>1 and temp[0]<K:
+    # print('=temp',temp)
+    mixed= mix_food(temp[0],temp[1])
+    # print('mixed', mixed)
+    answer+=1
+    if len(temp)>2:
+      temp=temp[2:]
+      # print('temp',temp)
+      for idx in range(len(temp)):
+        if temp[idx] < mixed:
+          # print('mixed is bigger')
+          if idx == len(temp)-1:
+            temp.append(mixed)
+          continue
+        else:
+          # print('insert mixed')
+          temp=temp[:idx]+[mixed]+temp[idx:]
+          break
+    else:
+      temp=[mixed]
+
+  if temp[0] < K:
+    return -1
   return answer
 
-print(solution([1, 2, 3, 9, 10, 12],	7))
-# print(solution([ 9, 10, 12,1, 2, 3,],	7))
+# print(solution([1, 2, 3, 9, 10, 12],	7))
+print(solution([1,2,3,5,6,7,8],	117))
 # [1, 2, 3, 9, 10, 12]	7	2
