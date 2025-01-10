89f500143b0d2fa6687d80633f12a410235e6d31
recursive-0 <maliethy@gmail.com>
2025-01-10 15:07:52 +0900

    더 맵게 정확성: 83.9 효율성: 16.1 합계: 100.0 / 100.0

--- programmers/python/level2/42626.py
+++ programmers/python/level2/42626.py
@@ -1,35 +1,32 @@
+# https://www.daleseo.com/python-heapq/
+from heapq import heappop,heappush, heapify
 def solution(scoville, K):
   def mix_food(a,b):
     return a + b*2
 
   answer = 0
-  temp=sorted(scoville)
+  heapify(scoville)
+  # print('scoville',scoville)
 
-  while len(temp)>1 and temp[0]<K:
-    # print('=temp',temp)
-    mixed= mix_food(temp[0],temp[1])
-    # print('mixed', mixed)
+  while len(scoville) > 1 and scoville[0] < K:
+    # print('-'*50)
+    # print(scoville[0],'scoville',scoville)
+    a=heappop(scoville)
+    b=heappop(scoville)
+    # print('a,b',a,b)
+    mixed=mix_food(a,b)
     answer+=1
-    if len(temp)>2:
-      temp=temp[2:]
-      # print('temp',temp)
-      for idx in range(len(temp)):
-        if temp[idx] < mixed:
-          # print('mixed is bigger')
-          if idx == len(temp)-1:
-            temp.append(mixed)
-          continue
-        else:
-          # print('insert mixed')
-          temp=temp[:idx]+[mixed]+temp[idx:]
-          break
-    else:
-      temp=[mixed]
+    # print('mixed',mixed)
+    heappush(scoville,mixed)
 
-  if temp[0] < K:
+  if scoville[0] >= K:
+    return answer
+  else:
+
     return -1
-  return answer
+
 
 # print(solution([1, 2, 3, 9, 10, 12],	7))
-print(solution([1,2,3,5,6,7,8],	117))
+# print(solution([6,7,8,1,2,3,5],	118))
+print(solution([10,2,7,12,30,14,3,9],	100))
 # [1, 2, 3, 9, 10, 12]	7	2
