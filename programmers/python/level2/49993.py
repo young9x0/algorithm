e19f98fc86cf57bcad6d0383955234e24076bfaf
recursive-0 <maliethy@gmail.com>
2025-01-12 16:42:43 +0900

    스킬트리 정확성: 42.9

--- programmers/python/level2/49993.py
+++ programmers/python/level2/49993.py
@@ -6,24 +6,26 @@ def solution(skill, skill_trees):
     # print('tree',tree)
     t_prev=-1
     s_prev=-1
-
+    cnt=0
     for s_idx, s in enumerate(skill):
       if s in tree:
         t_idx=tree.index(s)
-        # print(t_prev,'t_idx',t_idx)
-        # print(s_idx,'s_prev',s_prev)
+        # print(t_idx,'t_prev', t_prev)
+        # print(s_idx,'s_prev', s_prev)
         if t_prev<t_idx and s_idx==s_prev+1:
           t_prev=t_idx
           s_prev=s_idx
         else:
           # print('break!!')
           break
+      else:
+        cnt+=1
 
-    if t_prev>0 and s_prev>0:
+    if s_prev>0 or cnt==len(skill):
       answer+=1
       # print('answer',answer)
 
   return answer
 
-print(solution("CBD",	["BACDE", "CBADF", "AECB", "BDA"]	))
+print(solution("CBD",	["BACDE", "CBADF", "AECB", "BDA",'UYIOP']	))
 # "CBD"	["BACDE", "CBADF", "AECB", "BDA"]	2
