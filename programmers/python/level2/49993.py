e69091046e09bcc24b840acc8debc025155d3b60
recursive-0 <maliethy@gmail.com>
2025-01-12 17:11:47 +0900

    스킬트리 다른 풀이

--- programmers/python/level2/49993.py
+++ programmers/python/level2/49993.py
@@ -2,33 +2,20 @@ def solution(skill, skill_trees):
   answer = 0
 
   for tree in skill_trees:
-    # print('-'*50)
-    # print('tree',tree)
-    t_prev=-1
-    s_prev=-1
-    cnt=0
-    flag=True
-    for s_idx, s in enumerate(skill):
-      if s in tree:
-        t_idx=tree.index(s)
-        # print(t_idx,'t_prev', t_prev)
-        # print(s_idx,'s_prev', s_prev)
-        if t_prev<t_idx and s_idx==s_prev+1:
-          t_prev=t_idx
-          s_prev=s_idx
-        else:
-          # print('break!!')
-          flag=False
-          break
-      else:
-        cnt+=1
+    print('-'*50)
+    skill_list=list(skill)
 
-    # print('cnt',cnt)
-    # print('s_prev',s_prev)
-    # print('t_idx',t_idx)
-    if flag and (s_prev>-1 or cnt==len(skill)):
+    for t in tree:
+      print('tree t',t)
+      if t in skill:
+        s=skill_list.pop(0)
+        print('skill s',s)
+        if t != s:
+          print('break!!')
+          break
+    else:
       answer+=1
-      # print('answer',answer)
+      print('answer',answer)
 
   return answer
 
