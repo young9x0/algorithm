c04da27c67f1a8d615bb86bb0acc4d2ba75d2434
recursive-0 <maliethy@gmail.com>
2025-01-13 16:00:45 +0900

    택배상자 다른 풀이

--- programmers/python/level2/131704.py
+++ programmers/python/level2/131704.py
@@ -1,41 +1,25 @@
 def solution(order):
-  answer = 0
   o_len=len(order)
   stack=[]
-  box_list=[i+1 for i in range(o_len)]
-  # print(box_list)
 
-  o_idx, b_idx = 0,0
-  while o_idx<o_len and b_idx<o_len+1:
-    # print('-'*50)
-    # print('order idx',o_idx, order[o_idx])
-    # if b_idx<o_len:
-      # print('box idx',b_idx, box_list[b_idx])
-
-    while stack and stack[-1] == order[o_idx]:
-      answer+=1
+  o_idx, box_num = 0,1
+  while box_num<o_len+1:
+    print('-'*50)
+    print('box_num',box_num)
+    stack.append(box_num)
+    while stack[-1] == order[o_idx]:
+      o_idx+=1
       stack.pop()
-      o_idx+=1
-      # print('box==stack[-1] answer', answer)
-      # print('stack pop',stack)
+      print('stack',stack)
 
-    if b_idx==o_len:
-      break
+      if len(stack) == 0:
+        break
 
-    if box_list[b_idx] == order[o_idx]:
-      answer+=1
-      o_idx+=1
-      # print('box_list[b_idx]==order answer', answer)
-    else:
-      stack.append(box_list[b_idx])
-      # print('stack append',stack)
+    box_num+=1
 
-    if  b_idx<o_len:
-      b_idx+=1
+  return o_idx
 
-  return answer
-
-# print(solution([4, 3, 1, 2, 5]))
-print(solution([5, 4, 3, 2, 1]))
+print(solution([4, 3, 1, 2, 5]))
+# print(solution([5, 4, 3, 2, 1]))
 # [4, 3, 1, 2, 5]	2
 # [5, 4, 3, 2, 1]	5
