d95be7e39e8cc73c760ff0cc7017263e9fde662a
recursive-0 <maliethy@gmail.com>
2025-01-14 08:47:14 +0900

    update temp.py

--- programmers/python/temp.py
+++ programmers/python/temp.py
@@ -1,9 +1,7 @@
-def solution(array, n):
+def solution(array):
   answer = 0
   return answer
 
-print(solution([1, 1, 2, 3, 4, 5],1))
-print(solution([0, 2, 3, 4],1))
-# [1, 2, 3, 3, 3, 4]	3
-# [1, 1, 2, 2]	-1
-# [1]	1
+print(solution([1, 1, 2, 3, 4, 5]))
+print(solution([0, 2, 3, 4]))
+
