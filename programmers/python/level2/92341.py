fbb9077e40799aac88e54de41489bbf0b79991c8
recursive-0 <maliethy@gmail.com>
2025-01-14 12:30:20 +0900

    주차 요금 계산 다른 풀이

--- programmers/python/level2/92341.py
+++ programmers/python/level2/92341.py
@@ -1,63 +1,44 @@
-import math
+from math import ceil
+from collections import defaultdict
 
-def solution(fees, records):
-  answer = []
-  default_time, default_fee, plus_time, plus_fee=fees
-  parking_dict={}
-  total_time_dict={}
+def to_min(time_str):
+  return int(time_str[:2])*60+int(time_str[3:])
+class Parking:
+  def __init__(self, fees):
+    self.fees=fees
+    self.in_flag=False
+    self.in_time=0
+    self.total=0
 
-  def cal_fee(parking_time):
-    # print('='*50)
-    total= default_fee
-
-    if parking_time > default_time:
-      # print('math.ceil(park_time/plus_time)',math.ceil((parking_time-default_time)/plus_time))
-      total+=math.ceil((parking_time-default_time)/plus_time) * plus_fee
-
-    return total
-
-  for record in records:
-    splited=record.split()
-    # print('-'*50)
-    # print('splited',splited)
-    time, car_num, status= splited
-    hours, minutes= time.split(':')
-    # print(hours, minutes)
-    start_min=int(hours)*60+ int(minutes)
-
-    if status == 'IN':
-      parking_dict[car_num] = start_min
-      if car_num not in total_time_dict:
-        total_time_dict[car_num] = 0
-      # print('parking_dict[car_num]',parking_dict[car_num])
+  def update(self, t, inout):
+    self.in_flag = True if inout=='IN' else False
+    if self.in_flag:
+      self.in_time = to_min(t)
     else:
-      park_time= start_min-parking_dict[car_num]
-      # print('park_time',park_time)
+      self.total+=(to_min(t)-self.in_time)
 
-      total_time_dict[car_num] += park_time
-      # print('total_time_dict[car_num]',total_time_dict[car_num])
-      parking_dict[car_num] = -1
+  def calc_fee(self):
+    if self.in_flag:
+      self.update('23:59','out')
+    default_t,default_f,add_t,add_f= self.fees
+    over_time=self.total-default_t
+    return default_f + ceil(over_time/add_t) * add_f if add_t >=0 else default_f
+def solution(fees, records):
+  records_dict=defaultdict(lambda:Parking(fees))
+  # print((records_dict))
 
-  # print('parking_dict',parking_dict)
-  if sum(parking_dict.values()) > -1*len(parking_dict.keys()):
-    for key, val in parking_dict.items():
-      if val>-1:
-        park_time= 23*60+59-val
-        # print('park_time',park_time)
+  for rcd in records:
+    t,car,inout = rcd.split()
+    records_dict[car].update(t,inout)
 
-        total_time_dict[key] += park_time
-        # print('total_time_dict[key]',total_time_dict[key])
+  test=Parking(["05:34 5961 IN", "06:00 0000 IN"])
+  print(test.in_flag)
+  return [v.calc_fee() for k,v in sorted(records_dict.items())]
 
-  total_tuple_list = sorted(total_time_dict.items(), key= lambda x: x[0])
-  # print('total_tuple_list', total_tuple_list)
-  for (key, val) in total_tuple_list:
-    answer.append(cal_fee(val))
-
-  return answer
 
 print(solution([180, 5000, 10, 600],	["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
-print(solution([120, 0, 60, 591],	["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]))
-print(solution([1, 461, 1, 10],	["00:00 1234 IN"]))
+# print(solution([120, 0, 60, 591],	["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]))
+# print(solution([1, 461, 1, 10],	["00:00 1234 IN"]))
 # [180, 5000, 10, 600]	["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]	[14600, 34400, 5000]
 # [120, 0, 60, 591]	["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]	[0, 591]
 # [1, 461, 1, 10]	["00:00 1234 IN"]	[14841]
