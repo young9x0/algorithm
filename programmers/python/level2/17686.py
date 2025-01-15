5abd9e682a028dbb534a610c3d40b085866434c7
recursive-0 <maliethy@gmail.com>
2025-01-15 21:11:31 +0900

    [3차] 파일명 정렬 다른 풀이2

--- programmers/python/level2/17686.py
+++ programmers/python/level2/17686.py
@@ -1,21 +1,41 @@
-import re
+def head_compare(data):
+  print('-'*50)
+  print('data',data)
+  route=0
+  answer=['','']
+
+  for i, v in enumerate(data):
+    if (route==0):
+      print('route=0')
+      if(v.isnumeric() == True):
+        answer[0] = data[:i].lower()
+        route=1
+        answer[1] += v
+        print('isnumeric answer',answer)
+    elif route==1:
+      print('route=1')
+      if(v.isnumeric() == True):
+        print('numeric')
+        answer[1]+=v
+      else:
+        print('str')
+        break
+
+  answer[1] = int(answer[1])
+  print('answer',answer)
+  return answer
 
 def solution(files):
-  a= sorted(files, key=lambda file: int(re.findall('\d+', file)[0]))
-  print('a',a)
-  b= sorted(a, key=lambda file: re.split('\d+', file.lower())[0])
-  for file in files:
-    print("re.split('\d+', file.lower())",re.split('\d+', file.lower()))
-  print('b',b)
+  files.sort(key=lambda x:head_compare(x))
 
-  return b
+  return files
 
-print(solution([ "img1.png", "IMG01.GIF", "imd2.JPG"]))
+# print(solution([ "img1.png", "IMG01.GIF", "imd2.JPG"]))
 # print(solution(["img12.png", "img10.png", "img02.png", "img1.png","img01.gif", "IMG01.GIF", "img2.JPG"]))
 # print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
 # print(solution(["F-5 Freedom F11ighter", "B-50 Super12fortress", "A-10 Thunder13bolt II", "F-14 Tom14cat"]))
 # print(solution(["F-5 Freedom F11ighter", "A-10 Super12fortress", "A-10 Super13bolt II", "F-1 Tom14cat"]))
-# print(solution(["b-001  5Freedom8", "b-10 Super26fortress", "B-009 Super13bolt II", "B-015Areedom"]))
+print(solution(["b-001  5Freedom8", "b-10 Super26fortress", "B-009 Super13bolt II", "B-015Areedom"]))
 
 # 입력: ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
 # 출력: ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
