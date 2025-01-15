d8be7ae03d2c4cb1216928771fb567c66608abb4
recursive-0 <maliethy@gmail.com>
2025-01-15 20:28:37 +0900

    [3차] 파일명 정렬 100%

--- programmers/python/level2/17686.py
+++ programmers/python/level2/17686.py
@@ -4,12 +4,12 @@ def solution(files):
   regex=re.compile("[0-9]+")
   temp=[]
 
-  for file in files:
+  for raw_idx, file in enumerate(files):
     splited=regex.search(file)
     # print(splited.span())
     start,end=splited.span()
     # print(start,end)
-    temp.append((file[:start],file[start:end],file[end:]))
+    temp.append((raw_idx,file[:start],file[start:end],file[end:]))
 
   # print("temp",temp)
   t_len=len(temp)
@@ -33,20 +33,24 @@ def solution(files):
       j=i+1
       continue
 
-    h_i, n_i, t_i =temp[i]
-    h_j, n_j, t_j =temp[j]
+    i_idx, h_i, n_i, t_i =temp[i]
+    j_idx, h_j, n_j, t_j =temp[j]
     h_i = h_i.lower()
     h_j = h_j.lower()
-    # print('h_i, n_i, t_i',h_i, n_i, t_i)
-    # print('h_j, n_j, t_j',h_j, n_j, t_j)
+    n_i = int(n_i)
+    n_j = int(n_j)
+    # print('h_i, n_i, t_i',i_idx, h_i, n_i, t_i)
+    # print('h_j, n_j, t_j',j_idx, h_j, n_j, t_j)
 
-    if h_i+n_i == h_j+n_j:
+    if h_i+str(n_i) == h_j+str(n_j):
       # print("h_i+n_i == h_j+n_j")
+      if i_idx > j_idx:
+        change_idx(i,j)
       j+=1
       continue
 
     elif h_i == h_j:
-      if int(n_i) <= int(n_j):
+      if n_i <= n_j:
         # print("n_i < n_j")
         j+=1
         continue
@@ -65,13 +69,14 @@ def solution(files):
 
   # print(temp)
 
-  return ["".join(w_tuple) for w_tuple in temp]
+  return ["".join(w_tuple[1:]) for w_tuple in temp]
 
-print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
-print(solution(["img12.png", "img10.png", "img02.png", "img1.png","img01.gif", "IMG01.GIF", "img2.JPG"]))
-print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
-print(solution(["F-5 Freedom F11ighter", "B-50 Super12fortress", "A-10 Thunder13bolt II", "F-14 Tom14cat"]))
+print(solution([ "img1.png", "IMG01.GIF", "imd2.JPG"]))
+# print(solution(["img12.png", "img10.png", "img02.png", "img1.png","img01.gif", "IMG01.GIF", "img2.JPG"]))
+# print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
+# print(solution(["F-5 Freedom F11ighter", "B-50 Super12fortress", "A-10 Thunder13bolt II", "F-14 Tom14cat"]))
 # print(solution(["F-5 Freedom F11ighter", "A-10 Super12fortress", "A-10 Super13bolt II", "F-1 Tom14cat"]))
+# print(solution(["b-001  5Freedom8", "b-10 Super26fortress", "B-009 Super13bolt II", "B-015Areedom"]))
 
 # 입력: ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
 # 출력: ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
