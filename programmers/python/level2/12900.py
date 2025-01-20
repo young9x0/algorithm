233e4aeb04dc5f20b04d183192a31e6eb8ebd527
recursive-0 <maliethy@gmail.com>
2025-01-20 10:45:13 +0900

    2 x n 타일링 다른 풀이2

--- programmers/python/level2/12900.py
+++ programmers/python/level2/12900.py
@@ -1,11 +1,11 @@
 def solution(n):
-  a,b=1,1
+  a,b=0,1
   for i in range(n):
     a,b = b,a+b
 
-  return a % 1000000007
+  return b % 1000000007
 
-# print(solution(4))
+print(solution(4))
 print(solution(5))
 print(solution(6))
 print(solution(7))
