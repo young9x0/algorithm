aa1e5773e3345e7769e8817c06f9bb157b79b919
recursive-0 <maliethy@gmail.com>
2025-01-22 19:45:34 +0900

    2개 이하로 다른 비트 81.8 시간초과

--- programmers/python/level2/77885.py
+++ programmers/python/level2/77885.py
@@ -1,42 +1,39 @@
 
 def solution(numbers):
   answer = []
-  def to_bit_idx(idx, target, result):
-    # print('target%2',target, target%2)
-    if target%2 == 1:
-      result.append((idx, 1))
-    else:
-      result.append((idx, 0))
 
-    if target <2:
-      return [idx+1, set(result)]
+  def compared(target, object, diff):
+    # print('target, object, diff',target, object, diff)
+    if target%2 != object%2:
+      # print('diff')
+      diff+=1
+
+    if target == 1 and object > 1:
+      # print('last target, remain object')
+      target = 0
+
+    if object == 1:
+      return diff
     else:
-      return to_bit_idx(idx+1, target//2, result)
+      return compared(target//2, object//2, diff)
+
 
   for num in numbers:
-    [t_idx, target_bit]= to_bit_idx(0,num,[])
-    min= num+(2**(len(target_bit) + 1))
+    min= 100000_00000_00000
     idx=1
-    # print('min',min)
-    # print(t_idx, 'target_bit',target_bit)
+
     while num+idx < min+1:
-      [n_idx,next_bit]=to_bit_idx(0,num+idx,[])
-      same=next_bit&target_bit
-      diff_cnt= n_idx-len(same)
+      gap = compared(num, num+idx,0)
 
-      # print(n_idx,'next_bit',next_bit, num+idx)
-      # print('same',same)
-      # print('diff_cnt',diff_cnt)
-
-      if 0<diff_cnt< 3 and min > num+idx:
+      if 0 <gap< 3 and min > num+idx:
         min = num+idx
-        # print('min',min)
+        # print('h min',min)
 
       idx+=1
 
     answer.append(min)
   return answer
 
-# print(solution([2,7]))
-print(solution([7]))
+print(solution([2,7]))
+# print(solution([7]))
 # [3,11]
