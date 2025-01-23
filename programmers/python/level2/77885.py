228995c91b6307dbcaa5f9a79dd1a9083adcbf1b
recursive-0 <maliethy@gmail.com>
2025-01-23 10:40:39 +0900

    2개 이하로 다른 비트 다른 풀이2

--- programmers/python/level2/77885.py
+++ programmers/python/level2/77885.py
@@ -1,20 +1,41 @@
-def solution(numbers):
-  answer = []
-  for num in numbers:
-    print('xor', num ^ (num+1))
-    # 7(0111) ^ 8(1000) = 15(1111)
-    print('xor', (num ^ (num+1)) >> 2)
-    # 15 // 4 = 3
-    answer.append((num ^ (num+1)) >> 2 + num + 1)
+def f(number):
+  bin_num = bin(number)[2:]
+  print('bin_num',bin(number),bin_num)
 
+  if '0' not in bin_num:
+    print("'10' + bin_num[1:]",'10' + bin_num[1:])
+    return int('10' + bin_num[1:], 2)
+
+  bin_num = list(bin_num)
+  for i in range(len(bin_num)):
+    print('bin_num[-i-1]',bin_num[-i-1])
+    if bin_num[-i-1] == '0':
+      bin_num[-i-1] = '1'
+      break
+
+    if i > 0:
+      bin_num[-i] = '0'
+
+    print('bin_num',bin_num)
+
+  return int(''.join(bin_num), 2)
+
+
+def solution(numbers):
+  answer = [f(number) for number in numbers]
   return answer
 
 # print(solution([2,7]))
-print(solution([2]))
+# print(solution([2]))
 # print(solution([7]))
-# print(solution([11]))
+print(solution([11]))
+# 14
 # print(solution([15]))
+# 16
 # print(solution([16]))
+# 17
 # print(solution([17]))
+# 18
 # print(solution([19]))
+# 21
 # [3,11]
