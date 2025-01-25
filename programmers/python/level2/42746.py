cb7dc1debb27f99a97421fad7765556c90ca63ae
recursive-0 <maliethy@gmail.com>
2025-01-25 18:07:09 +0900

    가장 큰 수  53.3%

--- programmers/python/level2/42746.py
+++ programmers/python/level2/42746.py
@@ -1,53 +1,65 @@
+from collections import defaultdict
+
 def solution(numbers):
-  i, j = 0, 1
-  tmp = [n for n in numbers]
-  n_len=len(numbers)
-  def change(i,j):
-    # print('change')
-    # tmp[i] < tmp[j]
-    temp = tmp[i]
-    tmp[i] = tmp[j]
-    tmp[j] = temp
+  num_dict=defaultdict(list)
+  for num in numbers:
+    n_str=str(num)
+    num_dict[n_str[0]].append(n_str)
 
-  def add_self(num, limit):
-    new = str(num)
-    while len(new) < limit:
-      new += str(num)
+  # print('num_dict',num_dict)
 
-    # print('add_self', new)
-    return int(new)
+  def check_permutation(tmp):
+    max=int("".join(tmp))
+    def make_permutation(temp, rest, limit):
+      nonlocal max
+      # print('-'*50)
+      # print('temp',temp)
+      if len(temp) == limit:
+        # print('f temp',temp)
+        res= int("".join(temp))
+        if res>max:
+          max=res
+          # print('change max',max)
+        return
 
-  while i < n_len-1:
-    # print('-'*50)
-    # print('tmp[i],tmp[j],',tmp[i],tmp[j])
-    i_len= len(str(tmp[i]))
-    j_len= len(str(tmp[j]))
-    # print('i_len,j_len',i_len,j_len)
-    if i_len == j_len:
-      if tmp[i] < tmp[j]:
-        change(i,j)
+      # print('rest',rest)
+      r_len=len(rest)
+      for idx in range(r_len):
+        t_copy = [t for t in temp]
+        r_copy = [t for t in rest]
+        r_copy.pop(idx)
+        t_copy.append(rest[idx])
+        make_permutation(t_copy, r_copy, limit)
+
+    t_len= len(tmp)
+    for i in range(t_len):
+      copy = [t for t in tmp]
+      # print('tmp[i]',tmp[i])
+      # print('='*50)
+      copy.pop(i)
+      make_permutation([tmp[i]], copy, t_len)
+
+    # print('max',max)
+    return max
+
+  # print(check_permutation(['36','3','30','31']))
+  s_keys=sorted(num_dict.keys(), reverse=True)
+
+  answer=''
+  for key in s_keys:
+    if len(num_dict[key]) == 1:
+      answer+=num_dict[key][0]
     else:
+      # print('='*50)
+      s_int=check_permutation(num_dict[key])
+      # print('s_int',s_int)
+      answer+=str(s_int)
 
-      if i_len > j_len:
-        new_num = add_self(tmp[j], i_len)
-        if tmp[i] < new_num:
-          change(i, j)
-      else:
-        new_num = add_self(tmp[i], j_len)
-        if new_num < tmp[j]:
-          change(i, j)
-
-    # print('tmp',tmp)
-    j+=1
-    if j == n_len:
-      i+=1
-      j = i+1
-
-  # print('tmp',tmp)
-  return "".join(map(str, tmp))
+  return answer
 
 # print(solution([6, 10, 2]))
-print(solution([3, 30, 34, 5, 9]))
+# print(solution([3, 30, 34, 5, 9]))
+print(solution([11,1,1111,2001,199181,2010,2080,3,18,199]))
 
 # [6, 10, 2]	"6210"
 # [3, 30, 34, 5, 9]	"9534330"
