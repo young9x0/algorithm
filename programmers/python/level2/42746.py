f2db6c6ebb2eb7c750f6834d7a619edf2898101d
recursive-0 <maliethy@gmail.com>
2025-01-26 16:17:21 +0900

    가장 큰 수  66.7%

--- programmers/python/level2/42746.py
+++ programmers/python/level2/42746.py
@@ -2,49 +2,43 @@ from collections import defaultdict
 
 def solution(numbers):
   num_dict=defaultdict(list)
+  is_all_zero=True
+
   for num in numbers:
+    if is_all_zero and num > 0:
+      is_all_zero=False
     n_str=str(num)
     num_dict[n_str[0]].append(n_str)
 
   # print('num_dict',num_dict)
-
+  if is_all_zero:
+    return '0'
   def check_permutation(tmp):
-    max=int("".join(tmp))
-    def make_permutation(temp, rest, limit):
-      nonlocal max
-      # print('-'*50)
-      # print('temp',temp)
-      if len(temp) == limit:
-        # print('f temp',temp)
-        res= int("".join(temp))
-        if res>max:
-          max=res
-          # print('change max',max)
-        return
+    s_tmp=sorted(tmp, reverse=True)
+    # print('= s_tmp',s_tmp)
+    s_len=len(s_tmp)
 
-      # print('rest',rest)
-      r_len=len(rest)
-      for idx in range(r_len):
-        t_copy = [t for t in temp]
-        r_copy = [t for t in rest]
-        r_copy.pop(idx)
-        t_copy.append(rest[idx])
-        make_permutation(t_copy, r_copy, limit)
+    i,j = 0,1
+    while i < s_len-1:
+        i_j = s_tmp[i]+s_tmp[j]
+        j_i = s_tmp[j]+s_tmp[i]
+        if i_j < j_i:
+          temp = s_tmp[i]
+          s_tmp[i] = s_tmp[j]
+          s_tmp[j] = temp
+        if j < s_len-1:
+          j+=1
+        else:
+          i+=1
+          j=i+1
 
-    t_len= len(tmp)
-    for i in range(t_len):
-      copy = [t for t in tmp]
-      # print('tmp[i]',tmp[i])
-      # print('='*50)
-      copy.pop(i)
-      make_permutation([tmp[i]], copy, t_len)
+    # print('s_tmp',s_tmp)
+    return "".join(s_tmp)
 
-    # print('max',max)
-    return max
+  # print(check_permutation(['19','1097','19918','1998','189','1','109']))
+  d_keys= num_dict.keys()
 
-  # print(check_permutation(['36','3','30','31']))
-  s_keys=sorted(num_dict.keys(), reverse=True)
-
+  s_keys=sorted(d_keys, reverse=True)
   answer=''
   for key in s_keys:
     if len(num_dict[key]) == 1:
@@ -60,6 +54,7 @@ def solution(numbers):
 # print(solution([6, 10, 2]))
 # print(solution([3, 30, 34, 5, 9]))
 print(solution([11,1,1111,2001,199181,2010,2080,3,18,199]))
+# print(solution([0,0,0]))
 
 # [6, 10, 2]	"6210"
 # [3, 30, 34, 5, 9]	"9534330"
