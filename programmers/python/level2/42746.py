9e96cb593c614c968602c328efc8080dc93d7c29
recursive-0 <maliethy@gmail.com>
2025-01-27 15:53:59 +0900

    가장 큰 수 다른 풀이2

--- programmers/python/level2/42746.py
+++ programmers/python/level2/42746.py
@@ -1,8 +1,20 @@
 
+from functools import cmp_to_key
+# cmp_to_key: 비교 함수를 키 함수로 변환합니다
+# https://docs.python.org/ko/3/library/functools.html
+# https://wikidocs.net/109303
+
+def comparator(a,b):
+  t1=a+b
+  t2=b+a
+  # print('(int(t1) > int(t2))',(int(t1) > int(t2)))
+  return (int(t1) > int(t2)) - (int(t1) < int(t2))
+
 def solution(numbers):
-  numbers=list(map(str,numbers))
-  numbers=sorted(numbers,key=lambda x: x*3, reverse=True)
-  return "0" if set(numbers) == 0 else "".join(numbers)
+  str_num = [str(num) for num in numbers]
+  sorted_num = sorted(str_num,key=cmp_to_key(comparator), reverse=True)
+  answer='0' if set(numbers) == 0 else "".join(sorted_num)
+  return answer
 
 # print(solution([6, 10, 2]))
 # print(solution([3, 30, 34, 5, 9]))
