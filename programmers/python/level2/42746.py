0b333d79d88b30563a955d5712f49faf2247f347
recursive-0 <maliethy@gmail.com>
2025-01-27 15:43:33 +0900

    가장 큰 수 다른 풀이

--- programmers/python/level2/42746.py
+++ programmers/python/level2/42746.py
@@ -1,59 +1,14 @@
-from collections import defaultdict
 
 def solution(numbers):
-  num_dict=defaultdict(list)
-  is_all_zero=True
-
-  for num in numbers:
-    if is_all_zero and num > 0:
-      is_all_zero=False
-    n_str=str(num)
-    num_dict[n_str[0]].append(n_str)
-
-  # print('num_dict',num_dict)
-  if is_all_zero:
-    return '0'
-  def check_permutation(tmp):
-    s_tmp=sorted(tmp, reverse=True)
-    # print('= s_tmp',s_tmp)
-    s_len=len(s_tmp)
-
-    i,j = 0,1
-    while i < s_len-1:
-        i_j = s_tmp[i]+s_tmp[j]
-        j_i = s_tmp[j]+s_tmp[i]
-        if i_j < j_i:
-          temp = s_tmp[i]
-          s_tmp[i] = s_tmp[j]
-          s_tmp[j] = temp
-        if j < s_len-1:
-          j+=1
-        else:
-          i+=1
-          j=i+1
-
-    # print('s_tmp',s_tmp)
-    return "".join(s_tmp)
-
-  # print(check_permutation(['19','1097','19918','1998','189','1','109']))
-  d_keys= num_dict.keys()
-
-  s_keys=sorted(d_keys, reverse=True)
-  answer=''
-  for key in s_keys:
-    if len(num_dict[key]) == 1:
-      answer+=num_dict[key][0]
-    else:
-      # print('='*50)
-      s_int=check_permutation(num_dict[key])
-      # print('s_int',s_int)
-      answer+=str(s_int)
-
-  return answer
+  numbers=list(map(str,numbers))
+  numbers=sorted(numbers,key=lambda x: x*3, reverse=True)
+  return "0" if set(numbers) == 0 else "".join(numbers)
 
 # print(solution([6, 10, 2]))
 # print(solution([3, 30, 34, 5, 9]))
-print(solution([11,1,1111,2001,199181,2010,2080,3,18,199]))
+# print(solution([11,1,1111,2001,199181,2010,2080,3,18,199]))
+# print(solution([3,209,88,411,15,600,4,0,48,41]))
+print(solution([1,0,10,100,1000,11,121,112,10009,123,102]))
 # print(solution([0,0,0]))
 
 # [6, 10, 2]	"6210"
