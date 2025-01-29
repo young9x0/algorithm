855d3c5b3e9ba9fa9ee8fde1396054388411b21a
recursive-0 <maliethy@gmail.com>
2025-01-29 19:19:15 +0900

    큰 수 만들기 다른 풀이

--- programmers/python/level2/42883.py
+++ programmers/python/level2/42883.py
@@ -1,30 +1,28 @@
 def solution(number, k):
-  num_set= set([num for num in number])
-  sorted_set=sorted(num_set,reverse=True)
-  # print('sorted_set',sorted_set)
-  set_idx_dict= {num:idx+1 for idx,num in enumerate(sorted_set)}
+  stack = [number[0]]
+  for num in number[1:]:
+    while stack and stack[-1]<num and k>0:
+      k-=1
+      stack.pop()
+    stack.append(num)
+  if k>0:
+    stack=stack[:-k]
 
-  del_idx_list=[]
-  for idx, num in enumerate(number):
-    del_idx= (len(number)-idx)* set_idx_dict[num]
-    del_idx_list.append((del_idx,idx))
-
-  # print('del_idx_list',del_idx_list)
-  sorted_del_list = sorted(del_idx_list,key=lambda x:x[0],reverse=True)
-  k_del_list =[idx for val, idx in sorted_del_list[:k]]
-  # print('k_del_list',k_del_list)
-
-  answer = ""
-  for idx,num in enumerate(number):
-    if idx not in k_del_list:
-      answer+=number[idx]
-
-  return answer
+  return "".join(stack)
 
 # print(solution("1924",	2))
 # print(solution("1231234",	3))
-print(solution("4177252841",	4))
+# print(solution("4177252841",	4))
+# print(solution("11111",	4))
+# print(solution("1080201",	4))
+# print(solution("1120074",	3))
+# print(solution("1010100",	5))
+# print(solution("8761639",	3))
+print(solution("1769481",	4))
 
 # "1924"	2	"94"
 # "1231234"	3	"3234"
 # "4177252841"	4	"775841"
+# "2635473892"	4	"673892"
+# "11111"	4	"1"
+# "1080201"	4	"8201"
