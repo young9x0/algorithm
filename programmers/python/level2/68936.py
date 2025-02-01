bf96d8c56ece57af05ffc32a95c13872babc3666
recursive-0 <maliethy@gmail.com>
2025-02-01 14:19:23 +0900

    쿼드압축 후 개수 세기 다른 풀이

--- programmers/python/level2/68936.py
+++ programmers/python/level2/68936.py
@@ -1,64 +1,29 @@
 def solution(arr):
-  def is_same(quad_arr):
-    first= quad_arr[0][0]
-    for row in quad_arr:
-      for num in row:
-        if first != num:
-          return (first,False)
+  answer=[0,0]
+  def check(size, x, y):
+    print('-'*50)
+    print('size',size)
+    print('y',y,'x',x)
 
-    return (first, True)
+    if size == 1:
+      answer[arr[y][x]] += 1
+      return
+    else:
+      first = arr[y][x]
 
-  def check_quad(a_len, quad, answer):
-    # print('='*50)
-    # print('a_len',a_len)
-    # print('quad',quad)
-    # print('answer',answer)
-    first, flag = is_same(quad)
-    if flag:
+      for dy in range(size):
+        for dx in range(size):
+          if first != arr[y+dy][x+dx]:
+            check(size//2, x,y)
+            check(size//2, x+size//2,y)
+            check(size//2, x,y+size//2)
+            check(size//2, x+size//2,y+size//2)
+            return
+
+      print('all same')
       answer[first]+=1
-      return answer
 
-    idx= a_len//2
-    # one two
-    # three four
-    check_list=[[] for i in range(4)]
-    for r_idx in range(a_len):
-      # print('-'*50)
-      # print('r_idx',r_idx)
-      l_row=[]
-      r_row=[]
-      for n_idx in range(a_len):
-        if n_idx < idx:
-          l_row.append(quad[r_idx][n_idx])
-
-        else:
-          r_row.append(quad[r_idx][n_idx])
-
-      # print('l_row',l_row)
-      # print('r_row',r_row)
-      if r_idx < idx:
-        check_list[0].append(l_row)
-        check_list[1].append(r_row)
-      else:
-        check_list[2].append(l_row)
-        check_list[3].append(r_row)
-
-    # print('check_list',check_list)
-
-    for c_list in check_list:
-      first, flag = is_same(c_list)
-      # print('*'*50)
-      # print('c_list',c_list)
-      # print('flag',flag)
-      # print('first',first)
-      if flag:
-        answer[first]+=1
-      else:
-        check_quad(idx, c_list, answer)
-
-  answer = [0,0]
-  check_quad(len(arr[0]), arr, answer)
-
+  check(len(arr),0,0)
   return answer
 
 # print(solution([
@@ -67,24 +32,24 @@ def solution(arr):
 #   [1,0,0,1],
 #   [1,1,1,1]]))
 
-print(solution([
-[1,1,1,1,1,1,1,1],
-[0,1,1,1,1,1,1,1],
-[0,0,0,0,1,1,1,1],
-[0,1,0,0,1,1,1,1],
-[0,0,0,0,0,0,1,1],
-[0,0,0,0,0,0,0,1],
-[0,0,0,0,1,0,0,1],
-[0,0,0,0,1,1,1,1]
-]))
+# print(solution([
+# [1,1,1,1,1,1,1,1],
+# [0,1,1,1,1,1,1,1],
+# [0,0,0,0,1,1,1,1],
+# [0,1,0,0,1,1,1,1],
+# [0,0,0,0,0,0,1,1],
+# [0,0,0,0,0,0,0,1],
+# [0,0,0,0,1,0,0,1],
+# [0,0,0,0,1,1,1,1]
+# ]))
 
 # print(solution([
 #   [1]
 #  ]))
 
-# print(solution([
-#   [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]
-# ]))
+print(solution([
+  [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]
+]))
 
 
 
