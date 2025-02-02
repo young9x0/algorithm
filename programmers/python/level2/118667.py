62b9c8cbb72209294522b2098f26259e9b1b6f6a
recursive-0 <maliethy@gmail.com>
2025-02-02 15:17:09 +0900

    두 큐 합 같게 만들기 다른 풀이

--- programmers/python/level2/118667.py
+++ programmers/python/level2/118667.py
@@ -1,39 +1,25 @@
 def solution(queue1, queue2):
-  q_full = queue1+queue2
-  # print('half', half)
-  if sum(q_full)%2 > 0:
-    return -1
-
-  s_idx, l_idx, cnt = 0, len(queue1), 0
-  half = sum(q_full) // 2
-  q1_sum=sum(queue1)
-
-  while q1_sum != half:
-    # print('-'*50)
-    # print('q_full[s_idx:l_idx]',q_full[s_idx:l_idx])
-    # print('s_idx',s_idx)
-    # print('l_idx',l_idx)
-    # print('cnt',cnt)
-
-    if (s_idx == l_idx) or (l_idx == len(q_full)):
+  indicator2=sum(queue1)-int(sum(queue1+queue2)/2)
+  answer=0
+  sub_list=(queue1+queue2)[::-1]
+  add_list=(queue2+queue1)[::-1]
+  print(add_list)
+  while indicator2!=0:
+    try:
+      if indicator2>0:
+        indicator2-=sub_list.pop()
+      else:
+        indicator2+=add_list.pop()
+    except:
       return -1
+    answer+=1
+  return answer
 
-    if q1_sum > half:
-      q1_sum-=q_full[s_idx]
-      s_idx+=1
-      cnt+=1
-    elif q1_sum < half:
-      q1_sum+=q_full[l_idx]
-      l_idx+=1
-      cnt+=1
-
-  return cnt
-
-# print(solution([3, 2, 7, 2],	[4, 6, 5, 1]))
+print(solution([3, 2, 7, 2],	[4, 6, 5, 1]))
 # print(solution([1, 2, 1, 2],	[1, 10, 1, 2]))
 # print(solution([1, 1],	[1, 5]))
 # print(solution([1,1,9,8],	[1,5,1]))
-print(solution([1,2,1,6],	[1,1,1,15]))
+# print(solution([1,2,1,6],	[1,1,1,15]))
 
 # [3, 2, 7, 2]	[4, 6, 5, 1]	2
 # [1, 2, 1, 2]	[1, 10, 1, 2]	7
