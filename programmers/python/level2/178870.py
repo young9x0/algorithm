dc4ca5b328351a63a23d0fb09da2fd283c8e30c9
recursive-0 <maliethy@gmail.com>
2025-02-06 18:35:02 +0900

    연속된 부분 수열의 합 67.6% 시간초과+실패1

--- programmers/python/level2/178870.py
+++ programmers/python/level2/178870.py
@@ -1,36 +1,36 @@
 def solution(sequence, k):
-  result = []
   s_len=len(sequence)
+  result = [0,s_len-1]
 
-  for start in range(s_len):
+  for start in range(s_len-1,0,-1):
+    # print('-'*50)
     # print('start',start)
     total,idx= 0,start
-    temp=[]
 
-    while idx < s_len:
+    while idx > -1 and start-(idx+1) <= result[1] - result[0]:
+      # print('- idx',idx, sequence[idx])
+
       if total < k:
         total+=sequence[idx]
-        temp.append(sequence[idx])
-        idx+=1
+        idx-=1
       else:
         break
 
+      # print('total', total)
       if total == k:
-        # print('temp',temp)
-        result.append(([start,idx-1], temp))
-        break
+        # print(' sequence[idx:start]',idx,start, sequence[idx:start])
+        # print(' sequence[idx+1:start+1]',idx+1,start+1, sequence[idx+1:start+1])
+        if start-idx > 0 and sequence[idx:start] == sequence[idx+1:start+1]:
+          # print('same prev')
+          break
+        else:
+          return [idx+1,start]
 
-  # print('result',result)
-  if len(result) == 1:
-    return result[0][0]
-  else:
-    sorted_res = sorted(result, key=lambda x: (x[0][1] - x[0][0], x[0][0]))
-
-  return sorted_res[0][0]
 
 # print(solution([1, 2, 3, 4, 5],	7))
 # print(solution([1, 1, 1, 2, 3, 4, 5],	5))
-print(solution([2, 2, 2, 2, 2],	6))
+# print(solution([2, 2, 2, 2, 2],	6))
+print(solution([1,2, 2, 2, 2, 2,3],	6))
 
 # [1, 2, 3, 4, 5]	7	[2, 3]
 # [1, 1, 1, 2, 3, 4, 5]	5	[6, 6]
