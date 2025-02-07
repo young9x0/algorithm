136f4e2dde1da76a61fcd6545ea3afe2fbafc1f8
recursive-0 <maliethy@gmail.com>
2025-02-07 15:30:14 +0900

    연속된 부분 수열의 합 100%

--- programmers/python/level2/178870.py
+++ programmers/python/level2/178870.py
@@ -1,41 +1,39 @@
+from collections import deque
 def solution(sequence, k):
   s_len=len(sequence)
-
-  start = s_len-1
-  total,idx= 0,start
-  while start >-1:
+  dq=deque([])
+  idx = s_len-1
+  total= 0
+  while idx >-1:
     # print('-'*50)
-    # print('start',start)
-
-    if idx > -1:
-      # print('- idx',idx, sequence[idx])
-
-      if total < k:
-        total+=sequence[idx]
-        idx-=1
-      else:
-        start-=1
-        total,idx= 0,start
-
-      # print('total', total)
-      if total == k:
-        # print(' sequence[idx:start]',idx,start, sequence[idx:start])
-        # print(' sequence[idx+1:start+1]',idx+1,start+1, sequence[idx+1:start+1])
-        if sequence[idx:start] == sequence[idx+1:start+1]:
-          # print('same prev')
-          start-=1
-          total,idx= 0,start
-        else:
-          return [idx+1,start]
+    # print('idx',idx)
+    if total < k:
+      total+=sequence[idx]
+      dq.append((idx,sequence[idx]))
+      idx-=1
     else:
-      start-=1
-      total,idx= 0,start
+      first=dq.popleft()
+      # print('first',first)
+      total-=first[1]
+
+    # print('dq',dq)
+    # print('total', total)
+    if total == k:
+      # print('sequence[idx], dq[0][1]',sequence[idx], dq[0][1])
+      if idx>-1 and sequence[idx] == dq[0][1]:
+        # print('plus prev is same total')
+        first=dq.popleft()
+        # print('first',first)
+        total-=first[1]
+      else:
+        # print('dq',dq)
+        return [idx+1,dq[0][0]]
 
 # print(solution([1, 2, 3, 4, 5],	7))
 # print(solution([1, 1, 1, 2, 3, 4, 5],	5))
 # print(solution([2, 2, 2, 2, 2],	6))
 # print(solution([1,2, 2, 2, 2, 2,3],	6))
-print(solution([1,2,2,3,4,5,5],	11))
+# print(solution([1,2,2,3,4,5,5],	11))
 
 # [1, 2, 3, 4, 5]	7	[2, 3]
 # [1, 1, 1, 2, 3, 4, 5]	5	[6, 6]
