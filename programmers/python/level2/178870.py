d4b5327851dec0b22e15e2dcf87f4c944d3e2f1c
recursive-0 <maliethy@gmail.com>
2025-02-07 14:54:11 +0900

    연속된 부분 수열의 합 76.5% 시간초과

--- programmers/python/level2/178870.py
+++ programmers/python/level2/178870.py
@@ -1,36 +1,41 @@
 def solution(sequence, k):
   s_len=len(sequence)
-  result = [0,s_len-1]
 
-  for start in range(s_len-1,0,-1):
+  start = s_len-1
+  total,idx= 0,start
+  while start >-1:
     # print('-'*50)
     # print('start',start)
-    total,idx= 0,start
 
-    while idx > -1 and start-(idx+1) <= result[1] - result[0]:
+    if idx > -1:
       # print('- idx',idx, sequence[idx])
 
       if total < k:
         total+=sequence[idx]
         idx-=1
       else:
-        break
+        start-=1
+        total,idx= 0,start
 
       # print('total', total)
       if total == k:
         # print(' sequence[idx:start]',idx,start, sequence[idx:start])
         # print(' sequence[idx+1:start+1]',idx+1,start+1, sequence[idx+1:start+1])
-        if start-idx > 0 and sequence[idx:start] == sequence[idx+1:start+1]:
+        if sequence[idx:start] == sequence[idx+1:start+1]:
           # print('same prev')
-          break
+          start-=1
+          total,idx= 0,start
         else:
           return [idx+1,start]
-
+    else:
+      start-=1
+      total,idx= 0,start
 
 # print(solution([1, 2, 3, 4, 5],	7))
 # print(solution([1, 1, 1, 2, 3, 4, 5],	5))
 # print(solution([2, 2, 2, 2, 2],	6))
-print(solution([1,2, 2, 2, 2, 2,3],	6))
+# print(solution([1,2, 2, 2, 2, 2,3],	6))
+print(solution([1,2,2,3,4,5,5],	11))
 
 # [1, 2, 3, 4, 5]	7	[2, 3]
 # [1, 1, 1, 2, 3, 4, 5]	5	[6, 6]
