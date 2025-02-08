09cf48c8bd35c5fb51c350942b1d6f5bdc80ed1e
recursive-0 <maliethy@gmail.com>
2025-02-08 16:20:53 +0900

    마법의 엘리베이터 다른 풀이

--- programmers/python/level2/148653.py
+++ programmers/python/level2/148653.py
@@ -1,28 +1,9 @@
 def solution(storey):
-  answer = 0
-  def by_decimal(floor):
-    nonlocal answer
-    # print('-'*50)
-    # print('floor',floor)
-    # print('answer',answer)
+  if storey < 10:
+    return min(storey, 11-storey)
 
-    if floor == 0:
-      return answer
-
-    # print('floor%10',floor%10)
-    if floor%10 > 5 or (floor%10 == 5  and floor>=10 and int(str(floor)[-2])%10 + 1> 5):
-      # print('up')
-      answer+=(10-floor%10)
-      floor+=(10-floor%10)
-    else:
-      # print('down')
-      answer+=floor%10
-      floor-=floor%10
-
-    return by_decimal(floor//10)
-
-  by_decimal(storey)
-  return answer
+  left=storey%10
+  return min(left+solution(storey//10), 10 - left + solution(storey//10 + 1))
 
 # print(solution(16))
 # print(solution(2554))
