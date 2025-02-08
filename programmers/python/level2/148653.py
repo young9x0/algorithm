1d2fd0c6cb61cb12e1a1a14f72994aac97163149
recursive-0 <maliethy@gmail.com>
2025-02-08 15:37:35 +0900

    마법의 엘리베이터 76.9%

---
+++ programmers/python/level2/148653.py
@@ -0,0 +1,31 @@
+def solution(storey):
+  answer = 0
+  def by_decimal(floor):
+    nonlocal answer
+    # print('-'*50)
+    # print('floor',floor)
+    # print('answer',answer)
+
+    if floor == 0:
+      return answer
+
+    # print('floor%10',floor%10)
+    if floor%10 > 5:
+      # print('up')
+      answer+=(10-floor%10)
+      floor+=(10-floor%10)
+    else:
+      # print('down')
+      answer+=floor%10
+      floor-=floor%10
+
+    return by_decimal(floor//10)
+
+  by_decimal(storey)
+  return answer
+
+# print(solution(16))
+print(solution(2554))
+
+# 16	6
+# 2554	16
