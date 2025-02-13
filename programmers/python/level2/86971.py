680d46bcbb0eadaa8ae3edd80c3ff19cb00f4243
young9x0 <maliethy@gmail.com>
2025-02-13 16:43:47 +0900

    전력망을 둘로 나누기 100%

--- programmers/python/level2/86971.py
+++ programmers/python/level2/86971.py
@@ -8,51 +8,50 @@ def solution(n,wires):
     w_dict[s].append(f)
 
   # print(w_dict)
-  w_sorted = sorted(w_dict.items(), key=lambda x: len(x[1]), reverse=True)
-  # print('w_sorted',w_sorted)
 
-  w_tree={item[0]:[] for item in w_sorted}
-  visited={item[0]:False for item in w_sorted}
-
-  for node, link in w_sorted:
-    # print('node',node, link)
+  def child_cnt(root,node, visited):
+    # print('-'*50)
+    # print('node:',node)
+    # print('visited:',visited)
     if visited[node] == False:
       visited[node] = True
 
-    for child in link:
-      if visited[child] == False:
-        visited[child] = True
-        w_tree[node].append(child)
+      for child in w_dict[node]:
+        # print('child_cnt child:',child)
+        if visited[child] == False:
+          c_dp[root] += 1
+          child_cnt(root,child,visited)
 
-  def child_cnt(key, node):
-    # print('-'*50)
-    # print('cnt:',c_dp[key])
-    # print('node:',node)
-    if len(w_tree[node]) == 0:
-      return
-    else:
-      for child in w_tree[node]:
-        c_dp[key] += 1
-        child_cnt(key, child)
+  result=[]
+  for node, val in w_dict.items():
+    # print('='*50)
+    # print('root', node)
+    # print('val', val)
+    if len(val)>1:
+      max=0
+      c_dp={key:1 for key in val}
+      visited={key:False for key in range(1,n+1)}
+      visited[node] = True
+      for child in val:
+        # print('= child', child)
+        child_cnt(child, child, visited)
+        # print('c_dp',c_dp)
+        if c_dp[child] > max:
+          max = c_dp[child]
+        # print('max',max)
+      result.append((n-max, max))
 
-  # print('w_tree',w_tree)
-  max=0
-  node, child_list = w_sorted[0]
-  # print('child_list',child_list)
-  c_dp={key:1 for key in child_list}
-  for child in child_list:
-    child_cnt(child, child)
-    # print('c_dp',c_dp)
-    if c_dp[child] > max:
-      max = c_dp[child]
+  # print('result',result)
+  min=n
+  for a,b in result:
+    if min > abs(a-b):
+      min = abs(a-b)
+  return min
 
-  # print('max',max)
-
-  return n-(max*2)
-
-# print(solution(9,	[[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
-# print(solution(4,	[[1,2],[2,3],[3,4]]))
+print(solution(9,	[[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
+print(solution(4,	[[1,2],[2,3],[3,4]]))
 print(solution(7,	[[1,2],[2,7],[3,7],[3,4],[4,5],[6,7]]))
+print(solution(12,	[[1,4],[2,4],[3,4],[4,5],[5,6],[6,7],[7,8],[8,9],[9,10],[10,11],[11,12]]))
 
 # 9	[[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]	3
 # 4	[[1,2],[2,3],[3,4]]	0
