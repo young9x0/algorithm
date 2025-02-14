d9c022f8152e30a5c12e4b014a100fb0664ae933
young9x0 <maliethy@gmail.com>
2025-02-14 16:32:47 +0900

    전력망을 둘로 나누기 다른 풀이3

--- programmers/python/level2/86971.py
+++ programmers/python/level2/86971.py
@@ -1,30 +1,54 @@
-def dfs(v, graph, visited):
-  visited[v] = True
+uf=[]
+def find(a):
+  global uf
+  print('find')
+  print('a',a)
+  print('uf[a]',uf[a])
+  uf.append(a)
+  if uf[a] < 0: return a
+  uf[a] = find(uf[a])
+  return uf[a]
+
+def merge(a,b):
+  global uf
+  pa=find(a)
+  pb=find(b)
   print('-'*50)
-  print('v', v)
-  print('graph', graph)
-  print('visited', visited)
-  return sum([1] + [dfs(u,graph,visited) for u in graph[v] if not visited[u]])
+  print('merge')
+  print('pa',pa)
+  print('pb',pb)
+  print('before uf',uf)
+
+  if pa == pb: return
+  uf[pa]+=uf[pb]
+  uf[pb] =pa
+  print('after uf',uf)
 
 def solution(n,wires):
-  graph = [[] for _ in range(n+1)]
-  for v,u in wires:
-    graph[v].append(u)
-    graph[u].append(v)
+  global uf
+  ans=int(1e9)
+  # 1000000000
+  print('ans',ans)
 
-  print('graph', graph)
-  ans=100
-  for i in range(n-1):
-    visited=[False for _ in range(n+1)]
-    v1,v2 =wires[i]
-    print('v1,v2', v1, v2)
-    visited[v2]=True
-    tmp= abs(dfs(v1,graph,visited) - dfs(v2,graph,visited))
-    ans=min(tmp, ans)
+  k=len(wires)
+
+  for i in range(k):
+    print('='*50)
+    uf=[-1 for _ in range(n+1)]
+    tmp=[wires[x] for x in range(k) if x != i]
+    print('1 uf',uf)
+    print('tmp',tmp)
+
+    for a,b in tmp: merge(a,b)
+    print('2 uf', uf)
+    v=[x for x in uf[1:] if x < 0]
+    print('v',v)
+    ans=min(ans, abs(v[0]-v[1]))
+
   return ans
 
-# print(solution(9,	[[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
-print(solution(4,	[[1,2],[2,3],[3,4]]))
+print(solution(9,	[[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))
+# print(solution(4,	[[1,2],[2,3],[3,4]]))
 # print(solution(7,	[[1,2],[2,7],[3,7],[3,4],[4,5],[6,7]]))
 # print(solution(12,	[[1,4],[2,4],[3,4],[4,5],[5,6],[6,7],[7,8],[8,9],[9,10],[10,11],[11,12]]))
 
