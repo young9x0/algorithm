8d9683aed7aa68f80bb1257ff15f7d61f5521f05
young9x0 <maliethy@gmail.com>
2025-02-16 17:16:58 +0900

    시소 짝궁 17.6% 모든 시간초과->실패

--- programmers/python/level2/152996.py
+++ programmers/python/level2/152996.py
@@ -1,72 +1,43 @@
-import math
-
 def solution(weights):
   answer = 0
-  w_set = set(weights)
-
-
-  def prime_factorization(num, el_list, idx, prev_div, limit):
-    # print('-'*50)
-    # print('num',num)
-    # print('el_list',el_list)
-    if num == 1:
-      return
-
-    for div in range(2,limit):
-      if num % div == 0:
-        if prev_div == div:
-          idx+=1
-        else:
-          idx=0
-
-        el_list.append((idx,div))
-        return prime_factorization(num//div, el_list, idx, div, limit)
-
-  pf_dict={weight:[] for weight in w_set}
-  for weight in w_set:
+  sorted_w_set = sorted(set(weights))
+  # print('sorted_w_set', sorted_w_set)
+  dup_dict={}
+  w_set_len=len(sorted_w_set)
+  pf_dict={weight:set() for weight in weights}
+  for weight in weights:
     # print('weight:', weight)
-    limit = math.ceil(math.sqrt(weight))
-    # print('limit',limit)
-    prime_factorization(weight, pf_dict[weight],0,0, limit)
-  # print('pf_dict',pf_dict)
-
-
-  comb=[]
-  for prev in weights:
-    for next in weights:
-      if prev < next:
-        if (prev,next) not in comb:
-          comb.append((prev,next))
-      elif prev> next:
-        if (next, prev) not in comb:
-          comb.append((next,prev))
-      else:
-        if weights.count(prev) > 1 and (prev, prev) not in comb:
-          comb.append((prev,prev))
-
-  # print('comb',comb)
-  base_diff=[2,3,4,6,12]
-  for fir, sec in comb:
-    # print('fir',fir)
-    # print('sec',sec)
-    if fir == sec:
-      answer += 1
-      continue
+    if len(pf_dict[weight]) == 0:
+      dup_dict[weight]=0
+      for dist in range(2,5):
+        pf_dict[weight].add(weight * dist)
     else:
-      f_set= set(pf_dict[fir])
-      s_set= set(pf_dict[sec])
-      fs_set = (f_set | s_set) - (f_set & s_set)
-      # print('fs_set',fs_set)
-      diff=1
-      for idx, val in fs_set:
-        diff *= val
+      dup_dict[weight] +=1
 
-      if diff in base_diff:
+  print('pf_dict',pf_dict)
+
+  for p_idx in range(w_set_len):
+    for n_idx in range(p_idx + 1, w_set_len):
+      prev = sorted_w_set[p_idx]
+      next = sorted_w_set[n_idx]
+      print('-'*50)
+      print('prev, next', prev, next)
+      print('pf_dict[prev] & pf_dict[next]',pf_dict[prev] & pf_dict[next])
+      if len(pf_dict[prev] & pf_dict[next])>0:
         answer += 1
 
+  print('dup_dict', dup_dict)
+  for val in dup_dict.values():
+    if val > 0:
+      answer += 1
   return answer
 
 # print(solution([100,180,360,100,270]))
-print(solution([300,600,200,270,100]))
+# print(solution([350,700,111,255,111,222,355,255]))
+# print(solution([1000,250,500,1000,1000,1000,300,400]))
+# print(solution([100,100,100,310,500,500,190,270]))
+# print(solution([101,202,303,404,505,606,202,303,404]))
+print(solution([150,300,350,525,225,125]))
 
 # [100,180,360,100,270]	4
+# 5
