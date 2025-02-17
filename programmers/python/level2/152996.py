f326f46f160097ec6de68e9039a68f9900950dc2
young9x0 <maliethy@gmail.com>
2025-02-17 12:14:03 +0900

    시소 짝궁 다른 풀이2

--- programmers/python/level2/152996.py
+++ programmers/python/level2/152996.py
@@ -1,26 +1,31 @@
-from itertools import combinations
-from collections import Counter
+from collections import defaultdict
 
 def solution(weights):
   answer = 0
-  w_counter=Counter(weights)
-  # print('w_counter', w_counter)
-  for prev, next in combinations(w_counter.keys(), 2):
-    prev_dist=[prev*dist for dist in range(2,5)]
-    next_dist=[next*dist for dist in range(2,5)]
-    if len(set(prev_dist)&set(next_dist))>0:
-      answer += w_counter[prev] * w_counter[next]
+  dist_dict=defaultdict(int)
+  w_dict=defaultdict(int)
 
-  for val in w_counter.values():
-    if val>1:
-      answer += sum([i for i in range(1,val)])
+  for weight in weights:
+    print('weight',weight)
+    tmp = dist_dict[weight*2] + dist_dict[weight*3] + dist_dict[weight*4]
+    print('tmp',tmp)
+    if tmp > 0 and weight in w_dict:
+      tmp -= 2*w_dict[weight]
+
+    for dist in range(2,5):
+      dist_dict[weight*dist] += 1
+
+    w_dict[weight]+=1
+    answer+=tmp
+    print('w_dict:',w_dict)
+    print('dist_dict:',dist_dict)
 
   return answer
 
 # print(solution([100,180,360,100,270]))
 # print(solution([350,700,111,255,111,222,355,255]))
 # print(solution([1000,250,500,1000,1000,1000,300,400]))
-print(solution([100,100,200,200,300]))
+print(solution([100,100,200,200,100]))
 
 
 # [100,180,360,100,270]	4
