e53466955499c43e8d6e92fcccfc34ed3d8617c2
young9x0 <maliethy@gmail.com>
2025-02-17 11:59:50 +0900

    시소 짝궁 다른 풀이

--- programmers/python/level2/152996.py
+++ programmers/python/level2/152996.py
@@ -1,38 +1,20 @@
+from itertools import combinations
+from collections import Counter
+
 def solution(weights):
   answer = 0
-  sorted_set_w = sorted(set(weights))
-  # print('sorted_set_w', sorted_set_w)
-  dup_dict={}
-  sorted_len=len(sorted_set_w)
-  pf_dict={weight:set() for weight in weights}
-  for weight in weights:
-    # print('weight:', weight)
-    if len(pf_dict[weight]) == 0:
-      dup_dict[weight]=1
-      for dist in range(2,5):
-        pf_dict[weight].add(weight * dist)
-    else:
-      dup_dict[weight]+=1
+  w_counter=Counter(weights)
+  # print('w_counter', w_counter)
+  for prev, next in combinations(w_counter.keys(), 2):
+    prev_dist=[prev*dist for dist in range(2,5)]
+    next_dist=[next*dist for dist in range(2,5)]
+    if len(set(prev_dist)&set(next_dist))>0:
+      answer += w_counter[prev] * w_counter[next]
 
-  # print('pf_dict',pf_dict)
-  # print('dup_dict',dup_dict)
-  for p_idx in range(sorted_len):
-    for n_idx in range(p_idx + 1, sorted_len):
-      prev = sorted_set_w[p_idx]
-      next = sorted_set_w[n_idx]
-      # print('-'*50)
-      # print('prev, next', prev, next)
-      # print('pf_dict[prev] & pf_dict[next]',pf_dict[prev] & pf_dict[next])
-      if (prev*4 >= next*2 and len(pf_dict[prev] & pf_dict[next])>0):
-        if dup_dict[prev] + dup_dict[next] == 2 :
-          answer += 1
-        else:
-          answer += dup_dict[prev]*dup_dict[next]
-
-
-  for val in dup_dict.values():
+  for val in w_counter.values():
     if val>1:
-      answer += ((val-1)*(val))//2
+      answer += sum([i for i in range(1,val)])
+
   return answer
 
 # print(solution([100,180,360,100,270]))
