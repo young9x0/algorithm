4f3da56fb8d26f8195d6ac699771b14b44d99dc1
young9x0 <maliethy@gmail.com>
2025-02-20 15:01:48 +0900

    124 나라의 숫자 다른 풀이

--- programmers/python/level2/12899.py
+++ programmers/python/level2/12899.py
@@ -1,35 +1,23 @@
 def solution(n):
-  def change_num(num, tmp):
-    # print('num', num)
-    # print('tmp', tmp)
+  num=['1','2','4']
+  answer=""
 
-    if num == 0:
-      return tmp
+  while n>0:
+    n-=1
+    answer = num[n%3] + answer
+    n//=3
 
-    cur = (num%3)
-    # print('cur',cur)
-    if cur == 0:
-      tmp = '4'+tmp
-      num-=3
-    elif cur == 1:
-      tmp = '1'+tmp
-    else:
-      tmp = '2'+tmp
-
-    return change_num(num//3, tmp)
-
-  answer = change_num(n, '')
   return answer
 
-# print(solution(1))
-# print(solution(2))
-# print(solution(3))
-# print(solution(4))
+print(solution(1))
+print(solution(2))
+print(solution(3))
+print(solution(4))
 
 print(solution(10))
-# print(solution(13))
-# print(solution(14))
-# print(solution(15))
+print(solution(13))
+print(solution(14))
+print(solution(15))
 
 # 1	1
 # 2	2
