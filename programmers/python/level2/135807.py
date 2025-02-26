9352398d2ca550f3617dd86a5aba0207b05766d5
young9x0 <maliethy@gmail.com>
2025-02-26 20:28:37 +0900

    숫자 카드 나누기 다른 풀이

--- programmers/python/level2/135807.py
+++ programmers/python/level2/135807.py
@@ -1,60 +1,26 @@
-import math
+from math import gcd
+from functools import reduce
 
 def solution(arrayA, arrayB):
-  def prime_factorization(limit):
-    arr = [limit]
-    # print('limit', limit)
-    for div in range(math.floor(math.sqrt(limit)),1,-1):
-      # print('div', div)
-      if limit % div == 0:
-        arr.append(div)
-        if limit != div**2:
-          arr.append(limit//div)
+  a_gcd = reduce(gcd, arrayA)
+  b_gcd = reduce(gcd, arrayB)
+  print('a_gcd', a_gcd)
+  print('b_gcd', b_gcd)
+  ans = 0
 
-    return sorted(arr, reverse=True)
+  if all(num % a_gcd  for num in arrayB):
+    print('a_gcd', a_gcd)
+    if ans < a_gcd:
+      ans = a_gcd
+  if all(num % b_gcd  for num in arrayA):
+    print('b_gcd', b_gcd)
+    if ans < b_gcd:
+      ans = b_gcd
 
-  def find_max(a_set, b_set, prev_max, div_arr):
-    t_max = prev_max
-    idx = 0
-    d_len=len(div_arr)
-
-    while idx < d_len and div_arr[idx] > t_max:
-      # print('div_arr[idx]', div_arr[idx])
-      is_div = True
-      for a_num in a_set:
-        if a_num % div_arr[idx] > 0:
-          is_div = False
-          break
-
-      if is_div:
-        is_not_div = True
-        for b_num in b_set:
-          if b_num % div_arr[idx] == 0:
-            is_not_div=False
-            break
-
-        if is_not_div and t_max<div_arr[idx]:
-          t_max = div_arr[idx]
-
-      idx += 1
-
-    return t_max
-
-  a_set=set(arrayA)
-  b_set=set(arrayB)
-
-  a_div_arr = prime_factorization(min(a_set))
-  # print('a_div_arr', a_div_arr)
-  a_max=find_max(a_set,b_set,0, a_div_arr)
-
-  b_div_arr = prime_factorization(min(b_set))
-  b_max=find_max(b_set,a_set,a_max, b_div_arr)
-
-  return b_max
-
-print(solution([10, 17],	[5, 20]))
-print(solution([10, 20],	[5, 17]))
-print(solution([14, 35, 119],	[18, 30, 102]))
+  return ans
+# print(solution([10, 17],	[5, 20]))
+# print(solution([10, 20],	[5, 17]))
+# print(solution([14, 35, 119],	[18, 30, 102]))
 print(solution(	[18, 30, 102],[14, 35, 119]))
 
 # [10, 17]	[5, 20]	0
