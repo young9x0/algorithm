fbc36ec49fb5cac6102aac934bc4e4b8a70c20b7
young9x0 <maliethy@gmail.com>
2025-02-26 18:17:35 +0900

    숫자 카드 나누기 75% 시간초과

--- programmers/python/level2/135807.py
+++ programmers/python/level2/135807.py
@@ -1,67 +1,38 @@
 def solution(arrayA, arrayB):
-  def prime_factorization(num):
-    factors=set()
-    for div in range(2, num):
-      if num % div == 0:
-        factors.add(div)
+  def prime_factorization(a_set, b_set):
+    t_max =0
+    a_min = min(a_set)
+    div=a_min
+    while div > t_max:
+      is_div = True
+      for a_num in a_set:
+        if a_num % div > 0:
+          is_div = False
+          break
 
-    return factors
+      if is_div:
+        is_not_div = True
+        for b_num in b_set:
+          if b_num%div == 0:
+            is_not_div=False
+            break
+        if is_not_div and t_max<div:
+          t_max=div
+      div-=1
 
-  a_gcd_list=[]
-  for num in set(arrayA):
-    factors=prime_factorization(num)
-    # print('num',num)
-    # print('factors',factors)
-    factors.add(num)
-    a_gcd_list.append(factors)
+    return t_max
 
-  # print('a_gcd_list',a_gcd_list)
-  a_common_set=a_gcd_list[0]
-  a_len=len(a_gcd_list)
-  for idx in range(1,a_len):
-    a_common_set=a_common_set&a_gcd_list[idx]
+  a_set=set(arrayA)
+  b_set=set(arrayB)
+  a_max=prime_factorization(a_set,b_set)
+  b_max=prime_factorization(b_set,a_set)
 
-  # print('a_common_set',a_common_set)
-  b_gcd_list=[]
-  for num in set(arrayB):
-    factors=prime_factorization(num)
-    # print('num',num)
-    # print('factors',factors)
-    factors.add(num)
-    b_gcd_list.append(factors)
+  return max(a_max, b_max)
 
-  # print('b_gcd_list',b_gcd_list)
-  b_common_set = b_gcd_list[0]
-  b_len = len(b_gcd_list)
-  for idx in range(1, b_len):
-    # print(' b_gcd_list[idx]',b_common_set &  b_gcd_list[idx])
-    b_common_set = b_common_set & b_gcd_list[idx]
-
-  for gcd in b_gcd_list:
-    a_common_set = a_common_set - gcd
-
-  for gcd in a_gcd_list:
-    b_common_set = b_common_set - gcd
-
-  # print('a_common_set', a_common_set)
-  # print('b_common_set', b_common_set)
-
-  answer=0
-  if len(a_common_set)>0:
-    a_max= max(a_common_set)
-    if answer< a_max:
-      answer= a_max
-
-  if len(b_common_set)>0:
-    b_max = max(b_common_set)
-    if answer < b_max:
-      answer = b_max
-
-  return answer
-
-print(solution([10, 17],	[5, 20]))
-print(solution([10, 20],	[5, 17]))
-print(solution([14, 35, 119],	[18, 30, 102]))
+# print(solution([10, 17],	[5, 20]))
+# print(solution([10, 20],	[5, 17]))
+# print(solution([14, 35, 119],	[18, 30, 102]))
+print(solution(	[18, 30, 102],[14, 35, 119]))
 
 # [10, 17]	[5, 20]	0
 # [10, 20]	[5, 17]	10
