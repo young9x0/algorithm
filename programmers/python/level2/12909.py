2cbc483e5a2b96e3038783b648c91a140aa70d8c
young9x0 <maliethy@gmail.com>
2025-09-28 17:25:34 +0900

    올바른 괄호 다시 풀기

--- programmers/python/level2/12909.py
+++ programmers/python/level2/12909.py
@@ -1,20 +1,27 @@
 def solution(s):
-  st = list()
-  for ch in s:
-    if ch =='(':
-      st.append(ch)
-    if ch == ')':
-      try:
-        st.pop()
-      except IndexError:
-        return False
-  print(st)
-  return len(st) == 0
+  s_list = list(s)
+  # print('s_list:', s_list)
+  stack=[]
 
-print(solution("()()"))
+  for s in s_list:
+    # print("s", s)
+    # print('stack',stack)
+    if len(stack) == 0:
+      stack.append(s)
+    else:
+      if stack[-1] + s == "()":
+        stack.pop()
+      else:
+        stack.append(s)
+
+  # print("stack", stack)
+  return True if len(stack) == 0 else False
+
+
+# print(solution("()()"))
 print(solution("(())()"))
 print(solution(")()("))
-print(solution("(()("))
+# print(solution("(()("))
 # print(solution("(()(()()()()()()()()"))
 # "()()"	true
 # "(())()"	true
