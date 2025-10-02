cc1ee22264b794b2735df5fe87920d9eb6105198
young9x0 <maliethy@gmail.com>
2025-10-02 15:23:57 +0900

    최솟값 만들기 다시 풀기

--- programmers/python/level2/12941.py
+++ programmers/python/level2/12941.py
@@ -1,9 +1,17 @@
 def solution(A,B):
-  return sum(A.pop( A.index(min(A))) * B.pop(B.index(max(B))) for i in range(len(A)))
+  A.sort()
+  B.sort(reverse=True)
+  sum=0
+  for a, b in zip(A, B):
+    sum+= a*b
 
-# print(solution([1, 4, 2],	[5, 4, 3]))
+  # print('sum', sum)
+  return sum
+
 # print(solution([1, 4, 2],	[5, 4, 4]))
+# print(solution([1, 4, 2],	[5, 4, 3]))
 print(solution([1,2],	[3,4]))
 # print(solution([2],	[3]))
 # [1, 4, 2]	[5, 4, 4]	29
+# [1, 4, 2],	[5, 4, 3] 25
 # [1,2]	[3,4]	10
