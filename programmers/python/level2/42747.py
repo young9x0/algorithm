86502851e1b053b7d43b9e76672154dc56df3b81
young9x0 <maliethy@gmail.com>
2025-10-05 00:10:35 +0900

    H-Index 다시 풀기, 다른 사람 풀이 확인

--- programmers/python/level2/42747.py
+++ programmers/python/level2/42747.py
@@ -1,17 +1,21 @@
-# H-Index란: https://ssuamje.tistory.com/47
+# H-Index란: https://ssuamje.tistory.com/47, 이 학자가 작성한 논문은 평균적으로 h 이상 인용되고 있다.
 def solution(citations):
-  sorted_citations= sorted(citations, reverse=True)
-  c_len=len(sorted_citations)
-  for idx in range(c_len):
-    if sorted_citations[idx] <= idx:
-      return idx
-  return c_len
+  cSort= sorted(citations)
+  n = len(citations)
+  # print('cSort', cSort)
+  for idx in range(n):
+    print('n-idx',n-idx)
+    if cSort[idx] >= n-idx:
+      return cSort[idx]
+
+  return 0
+
 
 # print(solution([3, 0, 6, 1, 5]))
 # print(solution([0]))
 # print(solution([1]))
 # print(solution([10000]))
-# print(solution([88,89]))
-print(solution([46, 328, 8344, 164, 1]))
-# print(solution([0,1,2,3,3,3,3,3,3,4,10,20,30,40]))
-# [3, 0, 6, 1, 5]	3	3
+print(solution([5, 5, 6, 5, 5, 6]))
+
+# [3, 0, 6, 1, 5]	3
+# [5, 5, 6, 5, 5, 6] 5
