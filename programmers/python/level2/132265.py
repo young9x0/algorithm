8c87ff822a796fdef5b05254eb8a9c3f69bb5ecc
young9x0 <maliethy@gmail.com>
2025-10-05 00:11:10 +0900

    롤케이크 자르기 다시 풀기, 10%

--- programmers/python/level2/132265.py
+++ programmers/python/level2/132265.py
@@ -1,46 +1,28 @@
 def solution(topping):
-  print('topping',topping)
+  t_len=len(topping)
+  # print('t_len', t_len)
+  idx = 1
+  cnt=0
+  while(idx < t_len):
+    # print('= idx', idx)
+    # print('left:',set(topping[:idx]))
+    # print('right:',set(topping[idx:]))
 
-  l, r = 0, len(topping)
-  idx1 = 0
-  while l <= r:
-    m = (l + r) // 2
-    print('l',l,', r',r, 'm',m)
-    left = len(set(topping[:m]))
-    right = len(set(topping[m:]))
-    print('left',(topping[:m]), 'right',(topping[m:]))
-    if left < right:
-      l = m + 1
-    elif left >= right:
-      idx1 = m
-      r = m - 1
-      print('idx1',idx1)
+    if len(set(topping[:idx])) == len(set(topping[idx:])):
+      cnt+=1
+      # print('cnt', cnt)
+    idx+=1
 
-  print('-'*50)
-  l, r = 0, len(topping)
-  idx2 = 0
-  while l <= r:
-    m = (l + r) // 2
-    print('l',l,', r',r, 'm',m)
-    left = len(set(topping[:m]))
-    right = len(set(topping[m:]))
-    print('left',(topping[:m]), 'right',(topping[m:]))
-    if left <= right:
-      idx2 = m
-      l = m + 1
-      print('idx2',idx2)
-    elif left > right:
-      r = m - 1
-
-  return max(idx2 - idx1 + 1, 0)
+  return cnt
 
 
+# print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
 # print(solution([2, 2, 2, 2, 1, 1, 2, 3, 2]))
 # print(solution([1, 2, 3, 3, 2, 2, 2, 2, 2, 1]))
-# print(solution([1, 2, 1, 3, 1, 4, 1, 2]))
-print(solution([1, 2, 3, 1, 4]))
+# print(solution([1, 2, 2, 2, 2, 2, 3, 3, 2, 1]))
+# print(solution([1, 2, 3, 1, 4]))
 # print(solution([1, 2, 1, 1, 4]))
-# print(solution([1,1,1,1,1]))
+print(solution([1,1,1,1,1]))
 # print(solution([7,1,8,1,8,7]))
 # print(solution([2,4,3,2,4,3,2,8]))
 # print(solution([3,2,2,1,1,2,2,3,2,1]))
